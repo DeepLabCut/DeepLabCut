17beacde3a837803ec34d9f6118e11237c4cb910
Mackenzie Mathis <mathis@rowland.harvard.edu>
2018-06-19 18:04:20 -0400

    added variable for colormap

--- Evaluation-Tools/Step2_AnalysisofResults.py
+++ Evaluation-Tools/Step2_AnalysisofResults.py
@@ -25,7 +25,7 @@ sys.path.append(os.path.join(subfolder, "Generating_a_Training_Set"))
 
 import auxiliaryfunctions
 import pickle
-from myconfig import Task, date, scorer, Shuffles, TrainingFraction, snapshotindex, pcutoff, plotting
+from myconfig import Task, date, scorer, Shuffles, TrainingFraction, snapshotindex, pcutoff, plotting, colormap
 
 if plotting==True:
     import matplotlib
@@ -41,7 +41,7 @@ import pandas as pd
 ####################################################
 # https://stackoverflow.com/questions/14720331/how-to-generate-random-colors-in-matplotlib
 
-def get_cmap(n, name='hsv'):
+def get_cmap(n, name=colormap):
     '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
