a627e8fd984db450ef113e9a32018acb7cb9ac19
AlexEMG <amathis@fas.harvard.edu>
2018-10-30 22:09:11 -0400

    Speed update

--- Analysis-tools/AnalyzeVideos.py
+++ Analysis-tools/AnalyzeVideos.py
@@ -3,15 +3,23 @@ DeepLabCut Toolbox
 https://github.com/AlexEMG/DeepLabCut
 
 A Mathis, alexander.mathis@bethgelab.org
+R Warren, raw2163@cumc.columbia.edu
 M Mathis, mackenzie@post.harvard.edu
 
 This script analyzes videos based on a trained network (as specified in myconfig_analysis.py)
-
 You need tensorflow for evaluation. Run by:
 
 CUDA_VISIBLE_DEVICES=0 python3 AnalyzeVideos.py
 
+Updated to allow batchprocessing. See preprint on how to tune this optimally for your use-case:
+
+set batchsize in myconfig_analysis.py
+
+On the inference speed and video-compression robustness of DeepLabCut
+by Alexander Mathis and Rick Warren
+https://www.biorxiv.org/content/early/2018/10/30/457242
 """
+
 
 ####################################################
 # Dependencies
@@ -26,12 +34,13 @@ sys.path.append(subfolder + "pose-tensorflow")
 sys.path.append(subfolder + "Generating_a_Training_Set")
 
 from myconfig_analysis import videofolder, cropping, Task, date, \
-    trainingsFraction, resnet, snapshotindex, shuffle,x1, x2, y1, y2, videotype, storedata_as_csv
+    trainingsFraction, resnet, snapshotindex, shuffle,x1, x2, y1, y2, videotype, storedata_as_csv, batchsize
+
+print("Starting evaluation")
 
 # Deep-cut dependencies
 from config import load_config
 from nnet import predict
-from dataset.pose_dataset import data_to_input
 
 # Dependencies for video:
 import pickle
@@ -47,19 +56,6 @@ import pandas as pd
 import numpy as np
 import os
 from tqdm import tqdm
-
-
-def getpose(image, cfg, outputs, outall=False):
-    ''' Adapted from DeeperCut, see pose-tensorflow folder'''
-    image_batch = data_to_input(skimage.color.gray2rgb(image))
-    outputs_np = sess.run(outputs, feed_dict={inputs: image_batch})
-    scmap, locref = predict.extract_cnn_output(outputs_np, cfg)
-    pose = predict.argmax_pose_predict(scmap, locref, cfg.stride)
-    if outall:
-        return scmap, locref, pose
-    else:
-        return pose
-
 
 ####################################################
 # Loading data, and defining model folder
@@ -103,7 +99,7 @@ scorer = 'DeepCut' + "_resnet" + str(resnet) + "_" + Task + str(
 
 
 cfg['init_weights'] = os.path.join(modelfolder , 'train', Snapshots[snapshotindex])
-sess, inputs, outputs = predict.setup_pose_prediction(cfg)
+
 pdindex = pd.MultiIndex.from_product(
     [[scorer], cfg['all_joints_names'], ['x', 'y', 'likelihood']],
     names=['scorer', 'bodyparts', 'coords'])
@@ -112,68 +108,110 @@ pdindex = pd.MultiIndex.from_product(
 # Datafolder
 ##################################################
 
+#Auxiliary functions:
+def CrankVideo(cfg, sess, inputs, outputs,clip,nframes_approx,batchsize):
+    PredicteData = np.zeros((nframes_approx, 3 * len(cfg['all_joints_names'])))
+    batch_ind = 0 # keeps track of which image within a batch should be written to
+    batch_num = 0 # keeps track of which batch you are at
+    frames = np.empty((batchsize, nx, ny, 3), dtype='ubyte') # this keeps all frames in a batch
+    for index in tqdm(range(nframes_approx)):
+        image = img_as_ubyte(clip.reader.read_frame())
+        if index==int(nframes_approx-frame_buffer*2):
+            last_image = image
+        elif index>int(nframes_approx-frame_buffer*2):
+            if (image==last_image).all():
+                nframes = index
+                print("Detected frames: ", nframes)
+                if batch_ind>0:
+                    pose = predict.getposeNP(frames, cfg, sess, inputs, outputs) #process the whole batch (some frames might be from previous batch!)
+                    PredicteData[batch_num*batchsize:batch_num*batchsize+batch_ind, :] = pose[:batch_ind,:]
+                    
+                    
+                break
+            else:
+                last_image = image
+            
+        frames[batch_ind] = image
+        if batch_ind==batchsize-1:
+            pose = predict.getposeNP(frames,cfg, sess, inputs, outputs)
+            PredicteData[batch_num*batchsize:(batch_num+1)*batchsize, :] = pose
+            batch_ind = 0
+            batch_num += 1
+        else:
+            batch_ind+=1
+    return PredicteData,nframes
+
+def StoicVideo(cfg, sess, inputs, outputs,clip,nframes_approx):
+    PredicteData = np.zeros((nframes_approx, 3 * len(cfg['all_joints_names'])))
+    for index in tqdm(range(nframes_approx)):
+        image = img_as_ubyte(clip.reader.read_frame())
+        if index==int(nframes_approx-frame_buffer*2):
+            last_image = image
+        elif index>int(nframes_approx-frame_buffer*2):
+            if (image==last_image).all():
+                nframes = index
+                print("Detected frames: ", nframes)
+                break
+            else:
+                last_image = image
+        pose = predict.getpose(image, cfg, sess, inputs, outputs)
+        PredicteData[index, :] = pose.flatten()  # NOTE: thereby cfg['all_joints_names'] should be same order as bodyparts!
+    
+    return PredicteData,nframes
+
+frame_buffer=10
+
 # videofolder='../videos/' #where your folder with videos is.
-frame_buffer = 10
 
 os.chdir(videofolder)
-videos = np.sort([fn for fn in os.listdir(os.curdir) if (videotype in fn)])
-print("Starting ", videofolder, videos)
+videos = np.random.permutation([fn for fn in os.listdir(os.curdir) if (videotype in fn)])
+
 for video in videos:
-    dataname = video.split('.')[0] + scorer + '.h5'
+    vname=video.split('.')[0]
+    dataname = vname+ scorer +'.h5'
     try:
-        # Attempt to load data...
+        # Attempt to load data.
         pd.read_hdf(dataname)
         print("Video already analyzed!", dataname)
     except FileNotFoundError:
+        
+        cfg['batch_size']=batchsize
+        sess, inputs, outputs = predict.setup_pose_prediction(cfg)
+        
         print("Loading ", video)
+        start0=time.time()
+        
         clip = VideoFileClip(video)
         ny, nx = clip.size  # dimensions of frame (height, width)
         fps = clip.fps
-        #nframes = np.sum(1 for j in clip.iter_frames()) #this is slow (but accurate)
-        nframes_approx = int(np.ceil(clip.duration * clip.fps) + frame_buffer)
-        # this will overestimage number of frames (see https://github.com/AlexEMG/DeepLabCut/issues/9) This is especially a problem
-        # for high frame rates and long durations due to rounding errors (as Rich Warren found). Later we crop the result (line 187)
-        
         if cropping:
             clip = clip.crop(
                 y1=y1, y2=y2, x1=x1, x2=x2)  # one might want to adjust
 
+        nframes_approx = int(np.ceil(clip.duration * clip.fps) + frame_buffer)
         print("Duration of video [s]: ", clip.duration, ", recorded with ", fps,
               "fps!")
         print("Overall # of frames: ", nframes_approx,"with cropped frame dimensions: ", clip.size)
 
         start = time.time()
-        PredicteData = np.zeros((nframes_approx, 3 * len(cfg['all_joints_names'])))
         clip.reader.initialize()
         print("Starting to extract posture")
-        for index in tqdm(range(nframes_approx)):
-            #image = img_as_ubyte(clip.get_frame(index * 1. / fps))
-            image = img_as_ubyte(clip.reader.read_frame())
-            # Thanks to Rick Warren for the  following snipplet:
-            # if close to end of video, start checking whether two adjacent frames are identical
-            # this should only happen when moviepy has reached the final frame
-            # if two adjacent frames are identical, terminate the loop
-            if index==int(nframes_approx-frame_buffer*2):
-                last_image = image
-            elif index>int(nframes_approx-frame_buffer*2):
-                if (image==last_image).all():
-                    nframes = index
-                    print("Detected frames: ", nframes)
-                    break
-                else:
-                    last_image = image
-            pose = getpose(image, cfg, outputs)
-            PredicteData[index, :] = pose.flatten()  # NOTE: thereby cfg['all_joints_names'] should be same order as bodyparts!
+        if batchsize>1:
+            PredicteData,nframes=CrankVideo(cfg, sess, inputs, outputs,clip,nframes_approx,batchsize)
+        else:
+            PredicteData,nframes=StoicVideo(cfg, sess, inputs, outputs,clip,nframes_approx)
 
         stop = time.time()
 
         dictionary = {
-            "start": start,
+            "start-loading": start0,
+            "start-inference": start,
             "stop": stop,
             "run_duration": stop - start,
             "Scorer": scorer,
             "config file": cfg,
             "fps": fps,
+            "batch_size": batchsize,
             "frame_dimensions": (ny, nx),
             "nframes": nframes
         }
@@ -186,6 +224,7 @@ for video in videos:
         if storedata_as_csv:
             DataMachine.to_csv(video.split('.')[0] + scorer+'.csv')
         
-        with open(dataname.split('.')[0] + 'includingmetadata.pickle',
+        print("Evaluation took", round(stop-start,2), "seconds.")
+        with open(dataname.split('.h5')[0] + 'includingmetadata.pickle',
                   'wb') as f:
             pickle.dump(metadata, f, pickle.HIGHEST_PROTOCOL)
