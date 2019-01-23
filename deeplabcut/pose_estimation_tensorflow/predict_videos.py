7d9447659c4e6c0985f450ee7dbb695ed1c1c267
Mathis Lab <mathis@rowland.harvard.edu>
2019-01-23 11:27:05 -0500

    Update predict_videos.py

--- deeplabcut/pose_estimation_tensorflow/predict_videos.py
+++ deeplabcut/pose_estimation_tensorflow/predict_videos.py
@@ -160,7 +160,7 @@ def analyze_videos(config,videos,shuffle=1,trainingsetindex=0,videotype='avi',gp
         #looping over videos
         for video in Videos:
             AnalyzeVideo(video,DLCscorer,cfg,dlc_cfg,sess,inputs, outputs,pdindex,save_as_csv, destfolder)
-    
+
     os.chdir(str(start_path))
     print("The videos are analyzed. Now your research can truly start! \n You can create labeled videos with 'create_labeled_video'.")
     print("If the tracking is not satisfactory for some videos, consider expanding the training set. You can use the function 'extract_outlier_frames' to extract any outlier frames!")
