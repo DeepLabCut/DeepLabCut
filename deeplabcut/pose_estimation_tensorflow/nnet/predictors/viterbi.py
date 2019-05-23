Fixed back computation argmax call in viterbi plugin.

deeplabcut/pose_estimation_tensorflow/nnet/predictors/viterbi.py
@@ -12,8 +12,6 @@ from deeplabcut.pose_estimation_tensorflow.nnet.processing import Pose
 # For computations
 import numpy as np
 
-# TODO: Might change so that a 2D array of gaussian values is precomputed for speed...(In the constructor...)
-
 class Viterbi(Predictor):
     """
     A predictor that applies the Viterbi algorithm to frames in order to predict poses.
@@ -193,7 +191,7 @@ class Viterbi(Predictor):
         for bp in range(self._viterbi_frames.get_bodypart_count()):
             # Compute the max...
             table = self._viterbi_frames.get_prob_table(r_counter, bp)
-            y, x = np.unravel_index(np.argmax(table, shape=(height, width)))
+            y, x = np.unravel_index(np.argmax(table), shape=(height, width))
             prob = table[y, x]
             # Set the pose at this point....
             self._viterbi_frames.set_pose_at(r_counter, bp, x, y, poses)