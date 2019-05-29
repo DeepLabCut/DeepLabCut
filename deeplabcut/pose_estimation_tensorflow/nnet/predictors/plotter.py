236cfd265c122e6af72804373665f64b349c7c93
isaac <awesomeisaac2000@gmail.com>
2019-05-29 08:58:42 -0600

    Added plugin for creating probability frame videos...

--- deeplabcut/pose_estimation_tensorflow/nnet/predictors/plotter.py
+++ deeplabcut/pose_estimation_tensorflow/nnet/predictors/plotter.py
@@ -2,12 +2,17 @@
 # For types in methods
 from typing import Union, List
 
-# Plugin base class
 import tqdm
 
 from deeplabcut.pose_estimation_tensorflow.nnet.processing import Pose
 from deeplabcut.pose_estimation_tensorflow.nnet.processing import Predictor
 from deeplabcut.pose_estimation_tensorflow.nnet.processing import TrackingData
+
+# Used specifically by plugin...
+import math
+import numpy as np
+import io
+import cv2
 
 
 class PlotterArgMax(Predictor):
@@ -15,6 +20,14 @@ class PlotterArgMax(Predictor):
     Identical to singleargmax, but plots at most 100 probability frames to the user using matplotlib
     during processing...
     """
+
+    # Name of the video file to save to
+    VIDEO_NAME = "prob-dlc.mp4"
+    # Output codec to save using
+    OUTPUT_CODEC = cv2.VideoWriter_fourcc(*"X264")
+    # Frames per second to use for video
+    OUTPUT_FPS = 15
+
     def __init__(self, bodyparts: List[str], num_frames: int):
         super().__init__(bodyparts, num_frames)
         self._parts = bodyparts
@@ -23,34 +36,60 @@ class PlotterArgMax(Predictor):
         # Attempt to import matplotlib, if it fails set a flag to indicate so...
         try:
             from matplotlib import pyplot
-            self._has_plotter = True
+            self._has_plotter = pyplot
         except ImportError:
             print("Error: Unable to import matplotlib, fall back to just computing frames...")
-            self._has_plotter = False
+            self._has_plotter = None
 
         # Keeps track of how many frames
         self._current_frame = 0
-        self._last_shown_frame = 0
-        self._step = self._num_frames / 100
+        # Determines grid size of charts
+        self._grid_size = int(math.ceil(math.sqrt(len(self._parts))))
+        # Stores opencv video writer...
+        self._vid_writer = None
+
 
     def on_frames(self, scmap: TrackingData) -> Union[None, Pose]:
-        # If we managed to import numplotlib, and we have gone enough frames to move a step, display plots...
-        if(self._has_plotter and (self._current_frame - self._last_shown_frame >= self._step)):
-            from matplotlib import pyplot
+        # If we managed to import numplotlib, render plots and save them to a video...
+        if(self._has_plotter is not None):
+            pyplot = self._has_plotter
 
-            for bp in range(scmap.get_bodypart_count()):
-                pyplot.subplot(2, 2, bp + 1)
-                pyplot.title(f"Bodypart: {self._parts[bp]}, Frame: {self._current_frame}")
-                pyplot.pcolormesh(scmap.get_prob_table(0, bp))
+            for frame in range(scmap.get_frame_count()):
+                # Allocate buffer to store output of pyplot
+                buffer = io.BytesIO()
 
-            pyplot.show()
+                # Plot all probability maps
+                for bp in range(scmap.get_bodypart_count()):
+                    pyplot.subplot(self._grid_size, self._grid_size, bp + 1)
+                    pyplot.title(f"Bodypart: {self._parts[bp]}, Frame: {self._current_frame}")
+                    pyplot.pcolormesh(scmap.get_prob_table(frame, bp))
 
-        # Otherwise increment counter and return same selections as singleargmax plugin would....
-        self._current_frame += scmap.get_frame_count()
+                # Save chart to the buffer.
+                pyplot.savefig(buffer, format="png")
+                buffer.seek(0)
+
+                # Convert buffer cv2 image, then close the buffer
+                img = cv2.imdecode(np.frombuffer(buffer, dtype=np.uint8), cv2.IMREAD_COLOR)
+                buffer.close()
+
+                # If the video writer does not exist, create it now...
+                if (self._vid_writer is None):
+                    height, width, layers = img.shape
+                    self._vid_writer = cv2.VideoWriter(self.VIDEO_NAME, self.OUTPUT_CODEC, self.OUTPUT_FPS,
+                                                       (width, height))
+
+                # Write image to the video writer...
+                self._vid_writer.write(img)
+                self._current_frame += 1
+
+
+        # Return argmax values for each frame...
         return scmap.get_poses_for(scmap.get_max_scmap_points())
 
 
     def on_end(self, progress_bar: tqdm.tqdm) -> Union[None, Pose]:
+        # Release the video writer...
+        self._vid_writer.release()
         # We are done, return None...
         return None
 
