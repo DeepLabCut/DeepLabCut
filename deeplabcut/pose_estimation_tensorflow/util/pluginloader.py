d79ef40fe6ccf850bcf7034d546fd0c5bcb4fb70
isaac <awesomeisaac2000@gmail.com>
2019-06-06 10:58:42 -0600

    Plugin system updated to only load plugins from plugin folder. Also now plugins are properly dynamically reloaded when load_plugins is called a second time...

--- deeplabcut/pose_estimation_tensorflow/util/pluginloader.py
+++ deeplabcut/pose_estimation_tensorflow/util/pluginloader.py
@@ -46,7 +46,7 @@ def _load_modules(dirmodule: ModuleType, reload = True):
 # Generic type for method below
 T = TypeVar("T")
 
-def load_plugin_classes(plugin_dir: ModuleType, plugin_metaclass: Type[T], reload: bool = True) -> Set[Type[T]]:
+def load_plugin_classes(plugin_dir: ModuleType, plugin_metaclass: Type[T], do_reload: bool = True) -> Set[Type[T]]:
     """
     Loads all plugins, or classes, within the specified module folder that extend the provided metaclass type.
 
@@ -54,15 +54,45 @@ def load_plugin_classes(plugin_dir: ModuleType, plugin_metaclass: Type[T], reloa
                        using import...
     :param plugin_metaclass: The metaclass that all plugins extend. Please note this is the class type, not the
                              instance of the class, so if the base class is Foo just type Foo as this argument.
-    :param reload: Boolean, Determines if plugins should be reloaded if they already exist. Defaults to True.
+    :param do_reload: Boolean, Determines if plugins should be reloaded if they already exist. Defaults to True.
 
     :return: A list of class types that directly extend the provided base class and where found in the specified
              module folder.
     """
-    # Load the modules plugins could be in, so as to load the classes that might exist there into sys.modules.
-    _load_modules(plugin_dir, reload)
+    # Get absolute and relative package paths for this module...
+    path = list(iter(plugin_dir.__path__))[0]
+    rel_path = plugin_dir.__name__
 
-    # Return all subclasses of the plugin base class as a set so as to remove duplicates.
-    return set(plugin_metaclass.__subclasses__())
+    # Used to store the plugins that have been found
+    plugins: Set[Type[T]] = set()
+
+    # Iterate all modules in specified directory using pkgutil, importing them if they are not in sys.modules
+    for importer, package_name, ispkg in pkgutil.iter_modules([path]):
+        # Name to look for in sys.modules...
+        mod_name = rel_path + "." + package_name
+
+        # If the module name is not in system modules or the reload flag is set to true, perform a full load of the
+        # modules...
+        if((mod_name not in sys.modules) or do_reload):
+            sub_module = importer.find_module(package_name).load_module(package_name)
+            sys.modules[mod_name] = sub_module
+        else:
+            # Otherwise we just grab the module already loaded in sys.modules
+            sub_module = sys.modules[mod_name]
+
+        # Now we check if the module is a package, and if so, recursively call this method
+        if(ispkg):
+            plugins.union(load_plugin_classes(sub_module, plugin_metaclass, do_reload))
+        else:
+            # Otherwise we begin looking for plugin classes
+            for item in dir(sub_module):
+                field = getattr(sub_module, item)
+
+                # Checking if the field is a class, and if the field is a direct child of the plugin class
+                if(isinstance(field, type) and (plugin_metaclass in field.__bases__)):
+                    # It is a plugin, add it to the list...
+                    plugins.add(field)
+
+    return plugins
 
 
