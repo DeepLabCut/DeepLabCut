1d25d28d48d894e36916f3ccbe3b12fec7782950
Tom Vajtay <vajtayt@gmail.com>
2019-06-10 12:46:22 -0400

    Removed examples

--- examples/slurm_dlc_script.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ["DLClight"]="True"
-#os.environ["Colab"]="True"
-
-import deeplabcut as dlc
-
-config_path = 'home/tjv55/open_field-Tom-2019-04-09/config.yaml'
-
-dlc.train_network(config_path)
