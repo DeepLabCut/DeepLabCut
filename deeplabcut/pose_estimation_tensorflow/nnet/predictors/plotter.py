949f1692b7dce00e1b2ea5c79c5001b0d4371b49
isaac <awesomeisaac2000@gmail.com>
2019-11-11 14:05:59 -0700

    Added more settings to plotter predictor plugin, so as to support more plotting use cases.

--- deeplabcut/pose_estimation_tensorflow/nnet/predictors/plotter.py
+++ deeplabcut/pose_estimation_tensorflow/nnet/predictors/plotter.py
@@ -12,6 +12,7 @@ from deeplabcut.pose_estimation_tensorflow.nnet.processing import TrackingData
 import math
 import numpy as np
 from matplotlib import pyplot
+import matplotlib
 import cv2
 
 
@@ -46,16 +47,26 @@ class PlotterArgMax(Predictor):
         self.Z_SHRINK_F = settings["z_shrink_factor"]
         # The colormap to use while plotting...
         self.COLOR_MAP = settings["colormap"]
+        self.DPI = settings["dpi"]
+        self.AXIS_ON = settings["axis_on"]
+        # Miscellaneous arguments to pass to the figure and axis...
+        self.AXES_ARGS = settings["axes_args"] if (isinstance(settings["axes_args"], dict)) else {}
+        self.FIGURE_ARGS = settings["figure_args"] if (isinstance(settings["figure_args"], dict)) else {}
+        self.FIGURE_ARGS.update({"dpi": self.DPI})
 
         # Build the subplots...
         if(self.PROJECT_3D):
+            self.AXES_ARGS.update({'projection': '3d'})
             from mpl_toolkits.mplot3d import Axes3D
-            self._figure, self._axes = pyplot.subplots(self._grid_size, self._grid_size, subplot_kw={'projection': '3d'})
+            self._figure, self._axes = pyplot.subplots(self._grid_size, self._grid_size,
+                                                       subplot_kw=self.AXES_ARGS, **self.FIGURE_ARGS)
         else:
-            self._figure, self._axes = pyplot.subplots(self._grid_size, self._grid_size)
+            self._figure, self._axes = pyplot.subplots(self._grid_size, self._grid_size, subplot_kw=self.AXES_ARGS,
+                                                       **self.FIGURE_ARGS)
         # Hide all axis.....
-        for ax in self._axes.flat:
-            ax.axis("off")
+        if(self.AXIS_ON):
+            for ax in self._axes.flat:
+                ax.axis("off")
 
 
     def on_frames(self, scmap: TrackingData) -> Union[None, Pose]:
@@ -63,7 +74,8 @@ class PlotterArgMax(Predictor):
             # Plot all probability maps
             for bp, ax in zip(range(scmap.get_bodypart_count()), self._axes.flat):
                 ax.clear()
-                ax.axis("off")
+                if(self.AXIS_ON):
+                    ax.axis("off")
                 ax.set_title(f"Bodypart: {self._parts[bp]}, Frame: {self._current_frame}")
 
                 if(self.PROJECT_3D):
@@ -71,7 +83,8 @@ class PlotterArgMax(Predictor):
                     x, y = np.meshgrid(x, y)
                     z = np.log(scmap.get_prob_table(frame, bp)) if (self.LOG_SCALE) else scmap.get_prob_table(frame, bp)
                     ax.plot_surface(x, y, z, cmap=self.COLOR_MAP)
-                    ax.set_zlim(ax.get_zlim()[0], ax.get_zlim()[1] * self.Z_SHRINK_F)
+                    z_range = ax.get_zlim()[1] - ax.get_zlim()[0]
+                    ax.set_zlim(ax.get_zlim()[0], ax.get_zlim()[0] + (z_range * self.Z_SHRINK_F))
                 else:
                     ax.pcolormesh(np.log(scmap.get_prob_table(frame, bp)) if (self.LOG_SCALE) else
                                       scmap.get_prob_table(frame, bp), cmap=self.COLOR_MAP)
@@ -117,7 +130,14 @@ class PlotterArgMax(Predictor):
             ("3d_projection", "Boolean, determines if probability frames should be plotted in 3d.", False),
             ("colormap", "String, determines the underlying colormap to be passed to matplotlib while plotting the "
                          "mesh.", "Blues"),
-            ("z_shrink_factor", "Float, determines how much to shrink the z-axis if in 3D mode...", 5)
+            ("z_shrink_factor", "Float, determines how much to shrink the z-axis if in 3D mode...", 5),
+            ("dpi", "The dpi of the final video, the higher the dpi the more crisp...",
+             matplotlib.rcParams['figure.dpi']),
+            ("axis_on", "Boolean, determines if axis, or tick marks and grids of subplots are shown.", False),
+            ("axes_args", "A dictionary, miscellaneous arguments to pass to matplotlib axes when constructing them.",
+             None),
+            ("figure_args", "A dictionary, miscellaneous arguments to pass to matplotlib figure when constructing it.",
+             None)
         ]
 
     @staticmethod
