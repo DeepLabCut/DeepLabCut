901f89d69baa8fe13bc96f1a274c2e3ecba0798a
AlexEMG <amathis@fas.harvard.edu>
2019-12-11 19:32:57 -0500

    Added return value

--- deeplabcut/__main__.py
+++ deeplabcut/__main__.py
@@ -11,7 +11,7 @@ Licensed under GNU Lesser General Public License v3.0
 import os
 
 # if module is executed direcyly (i.e. `python -m deeplabcut.__init__`) launch straight into the GUI
-if os.environ.get('DLClight', default=False) == 'False':
+if os.environ.get("DLClight", default=False) == False:
     print("Starting GUI...")
     import deeplabcut
     deeplabcut.launch_dlc()
