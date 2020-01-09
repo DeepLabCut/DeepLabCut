34b49776471b3cf7d6ad9c34c01d314420106fd7
isaac <awesomeisaac2000@gmail.com>
2020-01-09 14:58:45 -0700

    Fixed bug where num_outputs was looked up before it was set...

--- deeplabcut/pose_estimation_tensorflow/predict_videos.py
+++ deeplabcut/pose_estimation_tensorflow/predict_videos.py
@@ -194,10 +194,6 @@ def analyze_videos(config, videos, videotype='avi', shuffle=1, trainingsetindex=
     #update batchsize (based on parameters in config.yaml)
     dlc_cfg['batch_size']=cfg['batch_size']
 
-    # Check and make sure that this predictor supports multi output if we are currently in that mode...
-    if((dlc_cfg["num_outputs"] > 1) and (not predictor_cls.supports_multi_output())):
-        raise NotImplementedError("The selected predictor plugin doesn't support multiple outputs!!!")
-
     # Name for scorer:
     DLCscorer = auxiliaryfunctions.GetScorerName(cfg,shuffle,trainFraction,trainingsiterations=trainingsiterations)
     
@@ -206,6 +202,10 @@ def analyze_videos(config, videos, videotype='avi', shuffle=1, trainingsetindex=
     # update number of outputs and adjust pandas indices
     dlc_cfg['num_outputs'] = cfg.get('num_outputs', 1)
     print('num_outputs = ', dlc_cfg['num_outputs'])
+
+    # Check and make sure that this predictor supports multi output if we are currently in that mode...
+    if((dlc_cfg["num_outputs"] > 1) and (not predictor_cls.supports_multi_output())):
+        raise NotImplementedError("The selected predictor plugin doesn't support multiple outputs!!!")
 
     # Set this up differently depending on the format...
     if(multi_output_format == "separate-bodyparts"):
