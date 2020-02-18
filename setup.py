de83d2508e9507d8a89c17f48ea32622d43ceb65
AlexEMG <amathis@fas.harvard.edu>
2020-02-18 17:17:43 -0500

    DLC 2.1.6.2 public

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deeplabcut",
-    version="2.1.6",
+    version="2.1.6.2",
     author="A. & M. Mathis Labs",
     author_email="alexander.mathis@bethgelab.org",
     description="Markerless pose-estimation of user-defined features with deep learning",
