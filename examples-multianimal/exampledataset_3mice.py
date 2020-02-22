e897f2e1e05be20247e8619ff86d29cda634443d
AlexEMG <amathis@fas.harvard.edu>
2020-02-21 23:04:57 -0500

    updated example scripts

--- examples-multianimal/exampledataset_3mice.py
+++ examples-multianimal/exampledataset_3mice.py
@@ -2,27 +2,15 @@ import os
 os.environ['DLClight']='True'
 import deeplabcut
 
-task='MontBlanc'
-scorer='Daniel'
-
-video=['/home/alex/Dropbox/InterestingCode/social_datasets/croppedNov18/montblanc.mov']
-
-#print("CREATING PROJECT")
-#path_config_file=deeplabcut.create_new_project(task,scorer,video,copy_videos=True,multianimal=True)
-
-#project already created!
-
-#path_config_file='/home/alex/Hacking/DLCreleases/DLCdev/examples-multianimal/MontBlanc-Daniel-2019-12-16/config.yaml'
-
-path_config_file='/media/alex/dropboxdisk/Dropbox/Collaborations/Cancer/DLCdev/examples-multianimal/MontBlanc-Daniel-2019-12-16/config.yaml'
-videopath='/media/alex/dropboxdisk/Dropbox/Collaborations/Cancer/DLCdev/examples-multianimal/MontBlanc-Daniel-2019-12-16/video'
+path_config_file='/media/alex/dropboxdisk/Dropbox/InterestingCode/social_datasets/croppedNov18/MultiMouse-Daniel-2019-12-16/config.yaml'
+videopath='/media/alex/dropboxdisk/Dropbox/InterestingCode/social_datasets/croppedNov18/MultiMouse-Daniel-2019-12-16/video'
 
 print("Plot labels...")
 #deeplabcut.check_labels(path_config_file)
-#deeplabcut.check_labels(path_config_file,draw_skeleton=True,visualizeindividuals=True)
+deeplabcut.check_labels(path_config_file,draw_skeleton=True,visualizeindividuals=True)
 #deeplabcut.check_labels(path_config_file,draw_skeleton=True,visualizeindividuals=False)
 
-print("Crop images...")
+#print("Crop images...")
 #deeplabcut.cropimagesandlabels(path_config_file,userfeedback=False)
 #then we uncommented the large-scale full frame data > it is not used for training!
 
@@ -39,8 +27,7 @@ saveiters=5000
 displayiters=500
 
 print("Creating multianimal training set...")
-deeplabcut.train_network(path_config_file, shuffle=shuffle,trainingsetindex=trainingsetindex,
-    saveiters=saveiters,displayiters=displayiters,max_snapshots_to_keep=21)
+deeplabcut.train_network(path_config_file, shuffle=shuffle,trainingsetindex=trainingsetindex,saveiters=saveiters,displayiters=displayiters)
 
 ################## Analyze video
 nmspath = 'deeplabcut/pose_estimation_tensorflow/lib/nms_cython'
