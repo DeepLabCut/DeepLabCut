35d6b6f30b837ab944a7cbfb43271c6da4e0cfbd
AlexEMG <amathis@fas.harvard.edu>
2020-02-24 00:48:08 -0500

    lib was missing (ommited)

---
+++ deeplabcut/pose_estimation_tensorflow/lib/inferenceutils.py
@@ -0,0 +1,190 @@
+"""
+DeepLabCut2.2 Toolbox (deeplabcut.org)
+© A. & M. Mathis Labs
+https://github.com/AlexEMG/DeepLabCut
+
+Please see AUTHORS for contributors.
+https://github.com/AlexEMG/DeepLabCut/blob/master/AUTHORS
+Licensed under GNU Lesser General Public License v3.0
+"""
+
+import numpy as np
+
+###################################
+#### auxiliaryfunctions
+###################################
+
+def distance(v,w):
+    return np.sqrt(np.sum((v-w)**2))
+
+def minmax(array,slack=10):
+    return np.nanmin(array)-slack,np.nanmax(array)+slack
+
+def individual2boundingbox(cfg,animals,X1=0):
+    boundingboxes=np.zeros((len(animals),5))*np.nan
+
+    for id,individual in enumerate(animals):
+        boundingboxes[id,0:4:2]=minmax(individual[::3]+X1,slack=cfg.boundingboxslack)
+        boundingboxes[id,1:4:2]=minmax(individual[1::3],slack=cfg.boundingboxslack)
+        boundingboxes[id,4]=np.nanmean(individual[2::3]) #average likelihood of all bpts!
+
+    return boundingboxes
+
+##########################################################
+#### conversion & greedy bodypart matching code
+##########################################################
+
+def convertdetectiondict2listoflist(dataimage,imname,BPTS,withid=False,evaluation=False):
+    ''' Arranges data into list of list of [(x, y, score, global index of detection)] (all detections per bodypart).
+    Also includes id if available. '''
+    if evaluation:
+        detectedcoordinates=dataimage['prediction']['coordinates'][0]
+        detectedlikelihood=dataimage['prediction']['confidence']
+    else:
+        detectedcoordinates=dataimage['coordinates'][0]
+        detectedlikelihood=dataimage['confidence']
+
+    all_detections=[]
+    detection_counter=0
+    for part in BPTS:
+        if withid: #(x, y, likelihood, identity)
+            detections_with_likelihood = list(zip(detectedcoordinates[part][:,0], detectedcoordinates[part][:,1],
+                                        detectedlikelihood[part].flatten(),np.argmax(dataimage['identity'][part],1)))
+        else: #(x, y, likelihood)
+            detections_with_likelihood = list(zip(detectedcoordinates[part][:,0], detectedcoordinates[part][:,1],detectedlikelihood[part].flatten()))
+        idx = range(detection_counter, detection_counter+ len(detections_with_likelihood))
+        all_detections.append([detections_with_likelihood[i] + (idx[i],) for i in range(len(idx))])
+        detection_counter += len(detections_with_likelihood)
+    return all_detections
+
+def matchconnectionsevaluation(cfg, dataimage, all_peaks, iBPTS, partaffinityfield_graph, PAF):
+    ''' PAF is a subset of the indices that should be used in the PAF graph '''
+    connection_all = []
+    special_k = []
+    for k in range(len(partaffinityfield_graph)):
+        cand_a = all_peaks[iBPTS[partaffinityfield_graph[k][0]]] # transfer orignal bpt index to the one in all_peaks!
+        cand_b = all_peaks[iBPTS[partaffinityfield_graph[k][1]]] #
+        n_a = len(cand_a)
+        n_b = len(cand_b)
+        if n_a != 0 and n_b != 0:
+            connection_candidate = []
+            for i in range(n_a):
+                for j in range(n_b):
+                    KK=PAF[k]
+                    d=distance(np.array(cand_a[i][:2]),np.array(cand_b[j][:2]))
+                    score_with_dist_prior=abs(dataimage['prediction']['costs'][KK][cfg.method][i,j])
+                    si=cand_a[i][2] #detectedlikelihood[partaffinityfield_graph[k][0]][i].flatten()[0]
+                    sj=cand_b[j][2] #detectedlikelihood[partaffinityfield_graph[k][1]][j].flatten()[0]
+                    if score_with_dist_prior>cfg.pafthreshold and d<cfg.distnormalization and d>=cfg.distnormalizationLOWER and si*sj>cfg.detectionthresholdsquare:
+                        connection_candidate.append([i, j, score_with_dist_prior,
+                                                     score_with_dist_prior + np.sqrt(si * sj)*cfg.addlikelihoods])
+
+            #sort candidate connections by score!
+            connection_candidate = sorted(connection_candidate, key=lambda x: x[2], reverse=True)
+            connection = np.zeros((0, 5))
+            for c in range(len(connection_candidate)):
+                i, j, s = connection_candidate[c][0:3]
+                if i not in connection[:, 3] and j not in connection[:, 4]:
+                    ii=int(cand_a[i][-1]) #global index!
+                    jj=int(cand_b[j][-1])
+                    connection = np.vstack([connection, [ii, jj, s, i, j]])
+                    if len(connection) >= min(n_a, n_b):
+                        break
+
+            connection_all.append(connection)
+        else:
+            special_k.append(k)
+            connection_all.append([])
+    return connection_all, special_k
+
+def matchconnections(cfg,dataimage,all_peaks,iBPTS,partaffinityfield_graph,PAF):
+    ''' PAF is a subset of indices should be used in the PAF graph '''
+    connection_all = []
+    special_k = []
+    for k in range(len(partaffinityfield_graph)):
+        cand_a = all_peaks[iBPTS[partaffinityfield_graph[k][0]]] # detectedcoordinates[partaffinityfield_graph[k][0]]
+        cand_b = all_peaks[iBPTS[partaffinityfield_graph[k][1]]] # detectedcoordinates[partaffinityfield_graph[k][1]]
+        n_a = len(cand_a)
+        n_b = len(cand_b)
+        if n_a != 0 and n_b != 0:
+            connection_candidate = []
+            for i in range(n_a):
+                for j in range(n_b):
+                    KK=PAF[k]
+                    d=distance(np.array(cand_a[i][:2]),np.array(cand_b[j][:2]))
+                    score_with_dist_prior=abs(dataimage['costs'][KK][cfg.method][i,j])
+                    si=cand_a[i][2] #detectedlikelihood[partaffinityfield_graph[k][0]][i].flatten()[0]
+                    sj=cand_b[j][2] #detectedlikelihood[partaffinityfield_graph[k][1]][j].flatten()[0]
+                    if score_with_dist_prior>cfg.pafthreshold and d<cfg.distnormalization and d>cfg.distnormalizationLOWER and si*sj>cfg.detectionthresholdsquare:
+                        connection_candidate.append([i, j, score_with_dist_prior,
+                                                     score_with_dist_prior + np.sqrt(si * sj)*cfg.addlikelihoods])
+
+            #sort by score!
+            connection_candidate = sorted(connection_candidate, key=lambda x: x[2], reverse=True)
+            connection = np.zeros((0, 5))
+            for c in range(len(connection_candidate)):
+                i, j, s = connection_candidate[c][0:3]
+                if i not in connection[:, 3] and j not in connection[:, 4]:
+                    ii=int(cand_a[i][-1]) #global index!
+                    jj=int(cand_b[j][-1])
+                    connection = np.vstack([connection, [ii, jj, s, i, j]])
+                    if len(connection) >= min(n_a, n_b):
+                        break
+
+            connection_all.append(connection)
+        else:
+            special_k.append(k)
+            connection_all.append([])
+    return connection_all, special_k
+
+def linkjoints2individuals(cfg,all_peaks,connection_all, special_k,partaffinityfield_graph,iBPTS,numjoints=18):
+    subset = np.empty((0, numjoints+2))
+    candidate = np.array([item for sublist in all_peaks for item in sublist])
+    for k in range(len(partaffinityfield_graph)):
+        if k not in special_k:
+            part_as = connection_all[k][:, 0] #indices for source of limb!
+            part_bs = connection_all[k][:, 1] #indices for target of limb (when all detections are enumerated)
+            index_a, index_b =iBPTS[partaffinityfield_graph[k][0]],iBPTS[partaffinityfield_graph[k][1]] #convert in order of bpts!
+            for i in range(len(connection_all[k])):  # looping over all connections for that limb
+                found = 0
+                subset_idx = [-1, -1]
+                for j in range(len(subset)):  # current number of individuals...
+                    if (subset[j][index_a] == part_as[i] or subset[j][index_b] == part_bs[i]) and found<2: #added found<2
+                        subset_idx[found] = j
+                        found += 1
+
+                if found == 1:
+                    j = subset_idx[0]
+                    if subset[j][index_b] != part_bs[i]:
+                        subset[j][index_b] = part_bs[i]
+                        subset[j][-1] += 1
+                        subset[j][-2] += candidate[part_bs[i].astype(int), 2] + connection_all[k][i][2]
+                elif found == 2:  # if found 2 and disjoint, merge them
+                    j1, j2 = subset_idx
+                    membership = ((subset[j1] >= 0).astype(int) + (subset[j2] >= 0).astype(int))[:-2]
+                    if len(np.nonzero(membership == 2)[0]) == 0:  # merge
+                        subset[j1][:-2] += (subset[j2][:-2] + 1)
+                        subset[j1][-2:] += subset[j2][-2:]
+                        subset[j1][-2] += connection_all[k][i][2]
+                        subset = np.delete(subset, j2, 0)
+                    else:  # as like found == 1
+                        subset[j1][index_b] = part_bs[i]
+                        subset[j1][-1] += 1
+                        subset[j1][-2] += candidate[part_bs[i].astype(int), 2] + connection_all[k][i][2]
+
+                # if find no partA in the subset, create a new subset
+                elif not found and k < numjoints:
+                    row = -1 * np.ones(numjoints+2)
+                    row[index_a] = part_as[i]
+                    row[index_b] = part_bs[i]
+                    row[-1] = 2
+                    row[-2] = sum(candidate[connection_all[k][i, :2].astype(int), 2]) + connection_all[k][i][2]
+                    subset = np.vstack([subset, row])
+
+    deleteIdx = [];
+    for i in range(len(subset)):  # delete animals with too few connected bodyparts or low average score
+        if subset[i][-1] < cfg.minimalnumberofconnections or subset[i][-2]/subset[i][-1] < cfg.averagescore:
+            deleteIdx.append(i)
+    subset = np.delete(subset, deleteIdx, axis=0)
+    #subset = sorted(subset, key=lambda x: x[-2], reverse=True)
+    return subset, candidate
---
+++ deeplabcut/pose_estimation_tensorflow/lib/trackingutils.py
@@ -0,0 +1,245 @@
+"""
+DeepLabCut2.2 Toolbox (deeplabcut.org)
+© A. & M. Mathis Labs
+https://github.com/AlexEMG/DeepLabCut
+
+Please see AUTHORS for contributors.
+https://github.com/AlexEMG/DeepLabCut/blob/master/AUTHORS
+Licensed under GNU Lesser General Public License v3.0
+
+Code adapted from:
+
+SORT: A Simple, Online and Realtime Tracker
+Copyright (C) 2016 Alex Bewley alex@dynamicdetection.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from __future__ import print_function
+from numba import jit
+import os.path
+import numpy as np
+from sklearn.utils.linear_assignment_ import linear_assignment # consider switching to faster variant!
+from filterpy.kalman import KalmanFilter
+
+@jit
+def iou(bb_test,bb_gt):
+  """
+  Computes IUO between two bboxes in the form [x1,y1,x2,y2]
+  """
+  xx1 = np.maximum(bb_test[0], bb_gt[0])
+  yy1 = np.maximum(bb_test[1], bb_gt[1])
+  xx2 = np.minimum(bb_test[2], bb_gt[2])
+  yy2 = np.minimum(bb_test[3], bb_gt[3])
+  w = np.maximum(0., xx2 - xx1)
+  h = np.maximum(0., yy2 - yy1)
+  wh = w * h
+  o = wh / ((bb_test[2]-bb_test[0])*(bb_test[3]-bb_test[1])
+    + (bb_gt[2]-bb_gt[0])*(bb_gt[3]-bb_gt[1]) - wh)
+  return(o)
+
+def convert_bbox_to_z(bbox):
+  """
+  Takes a bounding box in the form [x1,y1,x2,y2] and returns z in the form
+    [x,y,s,r] where x,y is the centre of the box and s is the scale/area and r is
+    the aspect ratio
+  """
+  w = bbox[2]-bbox[0]
+  h = bbox[3]-bbox[1]
+  x = bbox[0]+w/2.
+  y = bbox[1]+h/2.
+  s = w*h    #scale is just area
+  r = w/float(h)
+  return np.array([x,y,s,r]).reshape((4,1))
+
+def convert_x_to_bbox(x,score=None):
+  """
+  Takes a bounding box in the centre form [x,y,s,r] and returns it in the form
+    [x1,y1,x2,y2] where x1,y1 is the top left and x2,y2 is the bottom right
+  """
+  w = np.sqrt(x[2]*x[3])
+  h = x[2]/w
+  if(score==None):
+    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.]).reshape((1,4))
+  else:
+    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.,score]).reshape((1,5))
+
+
+class KalmanBoxTracker(object):
+  """
+  This class represents the internal state of individual tracked objects observed as bbox.
+  """
+  count = 0
+  def __init__(self,bbox):
+    """
+    Initialises a tracker using initial bounding box.
+    """
+    #define constant velocity model
+    self.kf = KalmanFilter(dim_x=7, dim_z=4)
+    self.kf.F = np.array([[1,0,0,0,1,0,0],[0,1,0,0,0,1,0],[0,0,1,0,0,0,1],[0,0,0,1,0,0,0],  [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]])
+    self.kf.H = np.array([[1,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,1,0,0,0,0],[0,0,0,1,0,0,0]])
+
+    self.kf.R[2:,2:] *= 10.
+    self.kf.P[4:,4:] *= 1000. #give high uncertainty to the unobservable initial velocities
+    self.kf.P *= 10.
+    self.kf.Q[-1,-1] *= 0.01
+    self.kf.Q[4:,4:] *= 0.01
+
+    self.kf.x[:4] = convert_bbox_to_z(bbox)
+    self.time_since_update = 0
+    self.id = KalmanBoxTracker.count
+    KalmanBoxTracker.count += 1
+    self.history = []
+    self.hits = 0
+    self.hit_streak = 0
+    self.age = 0
+
+  def update(self,bbox):
+    """
+    Updates the state vector with observed bbox.
+    """
+    self.time_since_update = 0
+    self.history = []
+    self.hits += 1
+    self.hit_streak += 1
+    self.kf.update(convert_bbox_to_z(bbox))
+
+  def predict(self):
+    """
+    Advances the state vector and returns the predicted bounding box estimate.
+    """
+    if((self.kf.x[6]+self.kf.x[2])<=0):
+      self.kf.x[6] *= 0.0
+    self.kf.predict()
+    self.age += 1
+    if(self.time_since_update>0):
+      self.hit_streak = 0
+    self.time_since_update += 1
+    self.history.append(convert_x_to_bbox(self.kf.x))
+    return self.history[-1]
+
+  def get_state(self):
+    """
+    Returns the current bounding box estimate.
+    """
+    return convert_x_to_bbox(self.kf.x)
+
+def associate_detections_to_trackers(detections,trackers,iou_threshold):
+  """
+  Assigns detections to tracked object (both represented as bounding boxes)
+
+  Returns 3 lists of matches, unmatched_detections and unmatched_trackers
+  """
+  if(len(trackers)==0):
+    return np.empty((0,2),dtype=int), np.arange(len(detections)), np.empty((0,5),dtype=int)
+  iou_matrix = np.zeros((len(detections),len(trackers)),dtype=np.float32)
+
+  for d,det in enumerate(detections):
+    for t,trk in enumerate(trackers):
+      iou_matrix[d,t] = iou(det,trk)
+  matched_indices = linear_assignment(-iou_matrix)
+
+  unmatched_detections = []
+  for d,det in enumerate(detections):
+    if(d not in matched_indices[:,0]):
+      unmatched_detections.append(d)
+  unmatched_trackers = []
+  for t,trk in enumerate(trackers):
+    if(t not in matched_indices[:,1]):
+      unmatched_trackers.append(t)
+
+  #filter out matched with low IOU
+  matches = []
+  for m in matched_indices:
+    if(iou_matrix[m[0],m[1]]<iou_threshold):
+      unmatched_detections.append(m[0])
+      unmatched_trackers.append(m[1])
+    else:
+      matches.append(m.reshape(1,2))
+  if(len(matches)==0):
+    matches = np.empty((0,2),dtype=int)
+  else:
+    matches = np.concatenate(matches,axis=0)
+
+  return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
+
+
+
+class Sort(object):
+  def __init__(self,cfg):
+    """
+    Sets key parameters for SORT
+    """
+    self.max_age = cfg.get('max_age', 1)
+    self.min_hits = cfg.get('min_hits', 3)
+    self.trackers = []
+    self.frame_count = 0
+    self.iou_threshold=cfg.get('iou_threshold',.3)
+
+  def update(self,dets):
+    """
+    Params:
+      dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
+    Requires: this method must be called once for each frame even with empty detections.
+    Returns the a similar array, where the last column is the object ID.
+
+    NOTE: The number of objects returned may differ from the number of detections provided.
+    """
+    self.frame_count += 1
+    #get predicted locations from existing trackers.
+    trks = np.zeros((len(self.trackers),5))
+
+    to_del = []
+    ret = []
+    for t,trk in enumerate(trks):
+      pos = self.trackers[t].predict()[0]
+      trk[:] = [pos[0], pos[1], pos[2], pos[3], 0]
+      if(np.any(np.isnan(pos))):
+        to_del.append(t)
+
+    trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
+    for t in reversed(to_del):
+      self.trackers.pop(t)
+    matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(dets,trks,self.iou_threshold)
+
+
+    #update matched trackers with assigned detections
+    animalindex=[]
+    for t,trk in enumerate(self.trackers):
+      if(t not in unmatched_trks):
+        d = matched[np.where(matched[:,1]==t)[0],0]
+        animalindex.append(d[0])
+        trk.update(dets[d,:][0]) #update coordinates
+      else:
+          animalindex.append('nix') #lost trk!
+
+    #create and initialise new trackers for unmatched detections
+    for i in unmatched_dets:
+        trk = KalmanBoxTracker(dets[i,:])
+        self.trackers.append(trk)
+        animalindex.append(i)
+
+    i = len(self.trackers)
+    for trk in reversed(self.trackers):
+        d = trk.get_state()[0]
+        if((trk.time_since_update < 1) and (trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits)):
+          ret.append(np.concatenate((d,[trk.id, int(animalindex[i-1])])).reshape(1,-1)) #for DLC we also return the original animalid
+          # +1 as MOT benchmark requires positive >> this is removed for DLC!
+        i -= 1
+        #remove dead tracklet
+        if(trk.time_since_update > self.max_age):
+          self.trackers.pop(i)
+
+    if(len(ret)>0):
+      return np.concatenate(ret)
+    return np.empty((0,5))
