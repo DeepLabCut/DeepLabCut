3e74d216e2fe7d127d2bb1fdac43407a60d0ded4
jeylau <30733203+jeylau@users.noreply.github.com>
2020-03-18 15:18:41 -0400

    Working example on the fish dataset.

--- crossval.py
+++ crossval.py
@@ -44,61 +44,53 @@ def convert_bbox_to_xywh(bbox, inplace=False):
     bbox[:, 3] = h
 
 
-config = '/Users/Jessy/Documents/PycharmProjects/dlcdev/datasets/silversideschooling-Valentina-2019-07-14/config.yaml'
+def compute_mot_metrics(inference_cfg, data, bboxes_ground_truth):
+    mot_tracker = trackingutils.Sort(inference_cfg)
+    all_jointnames = data['metadata']['all_joints_names']
+    numjoints = len(all_jointnames)
+    partaffinityfield_graph = data['metadata']['PAFgraph']
+    PAF = np.arange(len(partaffinityfield_graph))
+    partaffinityfield_graph = [partaffinityfield_graph[l] for l in PAF]
+    linkingpartaffinityfield_graph = partaffinityfield_graph
+    BPTS = iBPTS = range(numjoints)
+    ids = np.array(list(range(bboxes_ground_truth.shape[0])))
+    imnames = [fn for fn in list(data) if fn != 'metadata']
+    acc = mm.MOTAccumulator(auto_id=True)
+    for i, imname in enumerate(tqdm(imnames)):
+        animals = inferenceutils.assemble_individuals(inference_cfg, data[imname], numjoints, BPTS, iBPTS,
+                                                      PAF, partaffinityfield_graph, linkingpartaffinityfield_graph)
+        bb = inferenceutils.individual2boundingbox(inference_cfg, animals)
+        trackers = mot_tracker.update(bb)
+        bboxes_hyp = convert_bbox_to_xywh(trackers[:, :4])
+        bboxes_gt = bboxes_ground_truth[:, i, :4]
+        ids_gt = ids.copy()
+        empty = np.isnan(bboxes_gt).any(axis=1)
+        if empty.any():
+            bboxes_gt = bboxes_gt[~empty]
+            ids_gt = ids_gt[~empty]
+        dist = mm.distances.iou_matrix(bboxes_gt, bboxes_hyp, max_iou=inference_cfg['iou_threshold'])
+        acc.update(ids_gt, trackers[:, 4], dist)
+
+    mh = mm.metrics.create()
+    summary = mh.compute(acc, metrics=mm.metrics.motchallenge_metrics)
+    strsummary = mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names)
+    print(strsummary)
+    return acc, summary
+
+
 config_inference = '/Users/Jessy/Documents/PycharmProjects/dlcdev/datasets/silversideschooling-Valentina-2019-07-14/dlc-models/iteration-0/silversideschoolingJul14-trainset95shuffle1/test/inference_cfg.yaml'
-video = '/Users/Jessy/Documents/PycharmProjects/dlcdev/datasets/silversideschooling-Valentina-2019-07-14/videos/deeplc.menidia.school4.59rpm.S11.D.short.avi'
 ground_truth_file = '/Users/Jessy/Documents/PycharmProjects/dlcdev/datasets/silversideschooling-Valentina-2019-07-14/videos/deeplc.menidia.school4.59rpm.S11.D.shortDLC_resnet50_silversideschoolingJul14shuffle0_30000tracks.h5'
 full_data_file = '/Users/Jessy/Documents/PycharmProjects/dlcdev/datasets/silversideschooling-Valentina-2019-07-14/videos/deeplc.menidia.school4.59rpm.S11.D.shortDLC_resnet50_silversideschoolingJul14shuffle1_30000_full.pickle'
-
 inference_cfg = edict(deeplabcut.auxiliaryfunctions.read_plainconfig(config_inference))
-testing_cfg = inference_cfg.copy()
+testing_cfg = edict(inference_cfg.copy())
 ground_truth = pd.read_hdf(ground_truth_file)
 bboxes_ground_truth = reconstruct_all_bboxes(ground_truth, inference_cfg['boundingboxslack'], to_xywh=True)
-ids = np.array(list(range(bboxes_ground_truth.shape[0])))
 with open(full_data_file, 'rb') as file:
     data = pickle.load(file)
 
-# data_val = []
+accumulators = []
 # thresholds = np.linspace(0.1, 0.9, 9, endpoint=True)
-# inferencecfg.max_age=100
-# inferencecfg.min_hits=3
-# for threshold in thresholds:
-#     testing['iou_threshold'] = threshold
-# for min_hit in min_hits:
-#     testing['min_hits'] = min_hit
-#     deeplabcut.convert_detections2tracklets(config, [video], inferencecfg=testing)
-#     manager = TrackletManager(config, 0, 0)
-#     manager.load_tracklets_from_pickle(trackname)
-#     data_val.append(manager.xy.swapaxes(0, 1).reshape((manager.nframes, -1)))
-# for data in data_val:
-#     print(np.nanmean(data - ground_truth))
-
-
-mot_tracker = trackingutils.Sort(testing_cfg)
-all_jointnames=data['metadata']['all_joints_names']
-numjoints=len(all_jointnames)
-partaffinityfield_graph = data['metadata']['PAFgraph']
-PAF=np.arange(len(partaffinityfield_graph))
-partaffinityfield_graph=[partaffinityfield_graph[l] for l in PAF]
-linkingpartaffinityfield_graph=partaffinityfield_graph
-BPTS=iBPTS=range(numjoints)
-imnames = [fn for fn in list(data) if fn != 'metadata']
-acc = mm.MOTAccumulator(auto_id=True)
-for i, imname in enumerate(tqdm(imnames)):
-    animals = inferenceutils.assemble_individuals(inference_cfg, data[imname], numjoints, BPTS, iBPTS,
-                                                  PAF, partaffinityfield_graph, linkingpartaffinityfield_graph)
-    bb = inferenceutils.individual2boundingbox(inference_cfg, animals)
-    trackers = mot_tracker.update(bb)
-    bboxes_hyp = convert_bbox_to_xywh(trackers[:, :4])
-    bboxes_gt = bboxes_ground_truth[:, i, :4]
-    ids_gt = ids.copy()
-    empty = np.isnan(bboxes_gt).any(axis=1)
-    if empty.any():
-        bboxes_gt = bboxes_gt[~empty]
-        ids_gt = ids_gt[~empty]
-    dist = mm.distances.iou_matrix(bboxes_gt, bboxes_hyp, max_iou=inference_cfg['iou_threshold'])
-    acc.update(ids_gt, trackers[:, 4], dist)
-
-mh = mm.metrics.create()
-summary = mh.compute(acc, metrics=mm.metrics.motchallenge_metrics)
-strsummary = mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names)
+thresholds = [0.5]
+for threshold in thresholds:
+    testing_cfg['iou_threshold'] = threshold
+    accumulators.append(compute_mot_metrics(testing_cfg, data, bboxes_ground_truth))
