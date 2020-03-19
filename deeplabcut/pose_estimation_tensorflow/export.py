8505a42a23faa2a2c9b3ca1ed5e66aa782a90ca4
gkane <gkane@rowland.harvard.edu>
2020-03-19 15:12:33 -0400

    export writes and tars pose_cfg, ckpts, pbtxt, and pb

--- deeplabcut/pose_estimation_tensorflow/export.py
+++ deeplabcut/pose_estimation_tensorflow/export.py
@@ -11,22 +11,73 @@ Licensed under GNU Lesser General Public License v3.0
 
 import os
 import numpy as np
+import ruamel.yaml
+import glob
+import shutil
+import tarfile
+import tensorflow.compat.v1 as tf
+from tensorflow.python.tools import freeze_graph
+
 from deeplabcut.utils import auxiliaryfunctions
 from deeplabcut.pose_estimation_tensorflow.config import load_config
 from deeplabcut.pose_estimation_tensorflow.nnet import predict
-import tensorflow as tf
-
-tf_ver = [int(v) for v in tf.__version__.split('.')]
-if tf_ver[1] < 14:
-    from tensorflow.contrib.tensorrt import trt_convert as trt
-else:
-    from tensorflow.python.compiler.tensorrt import trt_convert as trt
 
 
-output = 'pose/locref_pred/block4/BiasAdd' # should it be ['concat_1'] or ['pose/part_pred/block4/BiasAdd']?
+def create_deploy_config_template():
+    '''
+
+    CURRENTLY NOT IMPLEMENTED
+
+    Creates a template for config.yaml file.
+    This specific order is preserved while saving as yaml file.
+    '''
+
+    yaml_str = '''\
+# Deploy config.yaml - info about project origin:
+    Task:
+    scorer:
+    date:
+    \n
+# Project path
+    project_path:
+    \n
+# Annotation data set configuration (and individual video cropping parameters)
+    video_sets:
+    bodyparts:
+    \n
+# Plotting configuration
+    skeleton:
+    skeleton_color:
+    \n
+    '''
+
+    ruamelFile = ruamel.yaml.YAML()
+    cfg_file = ruamelFile.load(yaml_str)
+    return cfg_file, ruamelFile
 
 
-def load_model(cfg, shuffle=1, trainingsetindex=0):
+def write_deploy_config(configname, cfg):
+    '''
+
+    CURRENTLY NOT IMPLEMENTED
+
+    Write structured config file.
+    '''
+
+    with open(configname, 'w') as cf:
+        ruamelFile = ruamel.yaml.YAML()
+        cfg_file, ruamelFile = create_deploy_config_template()
+        for key in cfg.keys():
+            cfg_file[key]=cfg[key]
+
+        # Adding default value for variable skeleton and skeleton_color for backward compatibility.
+        if not 'skeleton' in cfg.keys():
+            cfg_file['skeleton'] = []
+            cfg_file['skeleton_color'] = 'black'
+        ruamelFile.dump(cfg_file, cf)
+
+
+def load_model(cfg, shuffle=1, trainingsetindex=0, TFGPUinference=True):
     '''
 
     Loads a tensorflow session with a DLC model from the associated configuration
@@ -44,6 +95,9 @@ def load_model(cfg, shuffle=1, trainingsetindex=0):
     trainingsetindex : int. optional
         which training fraction to use, identified by its index
 
+    TFGPUinference : bool, optional
+        use tensorflow inference model? default = True
+
     Returns:
     --------
     sess : tensorflow session
@@ -51,7 +105,6 @@ def load_model(cfg, shuffle=1, trainingsetindex=0):
 
     checkpoint file path : string
         the path to the checkpoint file associated with the loaded model
-
     '''
 
     ########################
@@ -90,15 +143,26 @@ def load_model(cfg, shuffle=1, trainingsetindex=0):
     dlc_cfg['init_weights'] = os.path.join(model_folder, 'train', Snapshots[snapshotindex])
     trainingsiterations = (dlc_cfg['init_weights'].split(os.sep)[-1]).split('-')[-1]
     dlc_cfg['num_outputs'] = cfg.get('num_outputs', dlc_cfg.get('num_outputs', 1))
-    dlc_cfg['batch_size'] = cfg['batch_size']
+    #dlc_cfg['batch_size'] = cfg['batch_size']
+    dlc_cfg['batch_size'] = None
 
     # load network
-    sess, _, _ = predict.setup_pose_prediction(dlc_cfg)
+    if TFGPUinference:
+        sess, _, _ = predict.setup_GPUpose_prediction(dlc_cfg)
+        output = ['concat_1']
+    else:
+        sess, _, _ = predict.setup_pose_prediction(dlc_cfg)
+        if dlc_cfg['location_refinement']:
+            output = ['Sigmoid', 'pose/locref_pred/block4/BiasAdd']
+        else:
+            output = ['Sigmoid', 'pose/part_pred/block4/BiasAdd']
 
-    return sess, dlc_cfg['init_weights']
+    input = tf.get_default_graph().get_operations()[0].name
+
+    return sess, input, output, dlc_cfg
 
 
-def tf_to_pb(sess, checkpoint, dir=None):
+def tf_to_pb(sess, checkpoint, output, output_dir=None):
     '''
 
     Saves a frozen tensorflow graph (a protobuf file).
@@ -112,97 +176,72 @@ def tf_to_pb(sess, checkpoint, dir=None):
     checkpoint : string
         checkpoint of tensorflow model to be converted to protobuf (output will be <checkpoint>.pb)
 
+    output : list of strings
+        list of the names of output nodes (is returned by load_models)
+
+    output_dir : string, optional
+        path to the directory that exported models should be saved to.
+        If None, will export to the directory of the checkpoint file.
     '''
 
-    # export variables to constants
-    output_graph_def = tf.graph_util.convert_variables_to_constants(
-        sess,
-        tf.get_default_graph().as_graph_def(),
-        [output]
-    )
+    output_dir = os.path.expanduser(output_dir) if output_dir else os.path.dirname(checkpoint)
+    ckpt_base = os.path.basename(checkpoint)
 
-    # serialize and dump the output graph
-    if dir:
-        output_graph = os.path.normpath(dir + '/' + os.path.basename(checkpoint) + '.pb')
-    else:
-        output_graph = checkpoint + '.pb'
+    # save graph to pbtxt file
+    pbtxt_file = os.path.normpath(output_dir + '/' + ckpt_base + '.pbtxt')
+    tf.train.write_graph(sess.graph.as_graph_def(), '', pbtxt_file, as_text=True)
 
-    tf.gfile.GFile(output_graph, "wb").write(output_graph_def.SerializeToString())
+    # create frozen graph from pbtxt file
+    pb_file = os.path.normpath(output_dir + '/' + ckpt_base + '.pb')
+
+    freeze_graph.freeze_graph(input_graph=pbtxt_file,
+                              input_saver='',
+                              input_binary=False,
+                              input_checkpoint=checkpoint,
+                              output_node_names=",".join(output),
+                              restore_op_name="save/restore_all",
+                              filename_tensor_name="save/Const:0",
+                              output_graph=pb_file,
+                              clear_devices=True,
+                              initializer_nodes='')
 
 
-def pb_to_lite(checkpoint, dir=None):
+def export_model(cfg_path, iteration=None, shuffle=1, trainingsetindex=0, TFGPUinference=True, overwrite=False, make_tar=True):
     '''
 
-    converts a protobuf file to tensorflow lite format
+    Export DLC models for the model zoo or for live inference.
+    Saves the pose configuration, snapshot files, and frozen graph of the model to
+        a directory named exported-models within the project directory
 
-    Parameters:
+    Parameters
     -----------
-    checkpoint : string
-        the path to the checkpoint to be converted
 
+    cfg_path : string
+        path to the DLC Project config.yaml file
+
+    iteration : int, optional
+        the model iteration you wish to export.
+        If None, uses the iteration listed in the config file
+
+    shuffle : int, optional
+        the shuffle of the model to export. default = 1
+
+    trainingsetindex : int, optional
+        the index of the training fraction for the model you wish to export. default = 1
+
+    TFGPUinference : bool, optional
+        use the tensorflow inference model? Default = True
+        For inference using DeepLabCut-live, it is recommended to set TFGPIinference=False
+
+    overwrite : bool, optional
+        if the model you wish to export has already been exported, whether to overwrite. default = False
+
+    make_tar : bool, optional
+        Do you want to compress the exported directory to a tar file? Default = True
+        This is necessary to export to the model zoo, but not for live inference.
     '''
 
-    # model_dir = os.path.dirname(checkpoint)
-    # cfg = auxiliaryfunctions.read_config(os.path.normpath(model_dir+'/pose_cfg.yaml'))
-
-    pb_file = checkpoint+'.pb'
-    if not os.path.isfile(pb_file):
-        raise FileNotFoundError("The checkpoint has not been converted to a protobuf. You must first convert to protobuf (using tf_to_pb) before converting to tflite format.")
-
-    # if 'resnet' in cfg['net_type']:
-    #     input = 'Placeholder'
-    # elif 'mobilenet' in cfg['net_type']:
-    #     input = 'MobilenetV2/input'
-    # else:
-    #     raise Exception("Net type not found!")
-
-    input = 'Placeholder'
-    converter = tf.lite.TFLiteConverter.from_frozen_graph(pb_file, [input], [output], input_shapes={input : [None, 512, 512, 3]})
-    tflite_model = converter.convert()
-
-    if dir:
-        lite_file = os.path.normpath(dir + '/' + os.path.basename(checkpoint) + '.tflite')
-    else:
-        lite_file = checkpoint + '.tflite'
-    open(lite_file, 'wb').write(tflite_model)
-
-
-def pb_to_trt(checkpoint, precision_mode='FP16', dir=None):
-    '''
-
-    converts a protobuf file to tensorrt optimized graph
-
-    Parameters:
-    -----------
-    checkpoint : string
-        the path to the checkpoint to be converted
-
-    precision_mode : string, optional
-        precision mode for tensorrt optimized graph (FP16 by default, accepts FP32, INT8)
-
-    '''
-
-    pb_file = checkpoint+'.pb'
-    if not os.path.isfile(pb_file):
-        raise FileNotFoundError("The checkpoint has not been converted to a protobuf. You must first convert to protobuf (using tf_to_pb) before converting to tflite format.")
-
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(tf.gfile.GFile(pb_file, 'rb').read())
-
-    if tf_ver[1] >= 14:
-        converter = trt.TrtGraphConverter(input_graph_def=graph_def, nodes_blacklist=[output])
-        trt_graph = converter.convert()
-    else:
-        trt_graph = trt.create_inference_graph(input_graph_def=graph_def, outputs=[output], max_batch_size=64, precision_mode='FP16')
-
-    if dir:
-        trt_file = os.path.normpath(dir + '/' + os.path.basename(checkpoint) + '_trt.pb')
-    else:
-        trt_file = checkpoint + '_trt.pb'
-    tf.gfile.GFile(trt_file, "wb").write(trt_graph.SerializeToString())
-
-
-def export_models(cfg_path, iteration=None, shuffle=1, trainingsetindex=0, overwrite=False):
+    ### read config file
 
     try:
         cfg = auxiliaryfunctions.read_config(cfg_path)
@@ -213,22 +252,53 @@ def export_models(cfg_path, iteration=None, shuffle=1, trainingsetindex=0, overw
     cfg['iteration'] = iteration if iteration is not None else cfg['iteration']
     cfg['batch_size'] = cfg['batch_size'] if cfg['batch_size'] > 1 else 2
 
-    sess, ckpt = load_model(cfg, shuffle, trainingsetindex)
+    ### load model
 
-    # set up export directory
-    export_dir = os.path.normpath(cfg['project_path'] + '/exported_models')
+    sess, input, output, dlc_cfg = load_model(cfg, shuffle, trainingsetindex, TFGPUinference)
+    ckpt = dlc_cfg['init_weights']
+    model_dir = os.path.dirname(ckpt)
+
+    ### set up export directory
+
+    export_dir = os.path.normpath(cfg['project_path'] + '/' + 'exported-models')
     if not os.path.isdir(export_dir):
         os.mkdir(export_dir)
 
-    sub_dir_name = 'iteration-%d_shuffle-%d_trainfraction-%s' % (cfg['iteration'], shuffle, cfg['TrainingFraction'][trainingsetindex])
-    export_dir += '/' + sub_dir_name
+    sub_dir_name = 'DLC_%s_%s' % (cfg['Task'], dlc_cfg['net_type'])
+    full_export_dir = os.path.normpath(export_dir + '/' + sub_dir_name)
 
-    if os.path.isdir(export_dir):
+    if os.path.isdir(full_export_dir):
         if not overwrite:
-            raise FileExistsError("Export directory %s already exists. Terminating export..." % export_dir)
+            raise FileExistsError("Export directory %s already exists. Terminating export..." % full_export_dir)
     else:
-        os.mkdir(export_dir)
+        os.mkdir(full_export_dir)
 
-    tf_to_pb(sess, ckpt, dir=export_dir)
-    pb_to_lite(ckpt, dir=export_dir)
-    pb_to_trt(ckpt, dir=export_dir)
+    ### write pose config file
+
+    # sort dlc_cfg keys alphabetically, then save to pose_cfg.yaml in export directory
+    dlc_cfg = dict(dlc_cfg)
+    sorted_cfg = {}
+    for key, value in sorted(dlc_cfg.items()):
+        sorted_cfg[key] = value
+
+    pose_cfg_file = os.path.normpath(full_export_dir + '/pose_cfg.yaml')
+    ruamel_file = ruamel.yaml.YAML()
+    ruamel_file.dump(sorted_cfg, open(pose_cfg_file, 'w'))
+
+    ### copy checkpoint to export directory
+
+    ckpt_files = glob.glob(ckpt+'*')
+    ckpt_dest = [os.path.normpath(full_export_dir+'/'+os.path.basename(ckf)) for ckf in ckpt_files]
+    for ckf, ckd in zip(ckpt_files, ckpt_dest):
+        shutil.copy(ckf, ckd)
+
+    ### create pbtxt and pb files for checkpoint in export directory
+
+    tf_to_pb(sess, ckpt, output, output_dir=full_export_dir)
+
+    ### tar export directory
+
+    if make_tar:
+        tar_name = os.path.normpath(full_export_dir + '.tar.gz')
+        with tarfile.open(tar_name, 'w:gz') as tar:
+            tar.add(full_export_dir, arcname=os.path.basename(full_export_dir))
