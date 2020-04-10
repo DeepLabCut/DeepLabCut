94c8a90f4252c687b4e2079aaf9ae7e7485ab7eb
Mackenzie Mathis <mathis@rowland.harvard.edu>
2020-04-10 12:57:22 -0400

    fix to work on single animals.

--- deeplabcut/gui/create_videos.py
+++ deeplabcut/gui/create_videos.py
@@ -219,18 +219,43 @@ class Create_Labeled_Videos(wx.Panel):
             self.slow = True
         else:
             self.slow = False
-        if self.plot_idv.GetStringSelection() == "Yes":
-            color_by ='individual'
+
+        config_file = auxiliaryfunctions.read_config(self.config)
+        if config_file.get('multianimalproject', False):
+            if self.plot_idv.GetStringSelection() == "Yes":
+                color_by ='individual'
+            else:
+                color_by='bodypart'
+
+        if self.filter.GetStringSelection() == "Yes":
+            if len(self.bodyparts)==0:
+                self.bodyparts='all'
+
+                config_file = auxiliaryfunctions.read_config(self.config)
+                if config_file.get('multianimalproject', False):
+                    deeplabcut.create_labeled_video(self.config,self.filelist,self.videotype.GetValue(),shuffle=shuffle,
+                                                     trainingsetindex=trainingsetindex, save_frames=self.slow, draw_skeleton= self.draw,
+                                                     displayedbodyparts=self.bodyparts, trailpoints = self.trail_points.GetValue(),
+                                                     filtered=True, color_by=color_by)
+                else:
+                    deeplabcut.create_labeled_video(self.config,self.filelist,self.videotype.GetValue(),shuffle=shuffle,
+                                                     trainingsetindex=trainingsetindex, save_frames=self.slow, draw_skeleton= self.draw,
+                                                     displayedbodyparts=self.bodyparts, trailpoints = self.trail_points.GetValue(),
+                                                     filtered=True)
+
+        if len(self.bodyparts)==0:
+            self.bodyparts='all'
+        if config_file.get('multianimalproject', False):
+            deeplabcut.create_labeled_video(self.config,self.filelist,self.videotype.GetValue(),shuffle=shuffle,
+                                             trainingsetindex=trainingsetindex, save_frames=self.slow, draw_skeleton= self.draw,
+                                             displayedbodyparts=self.bodyparts, trailpoints = self.trail_points.GetValue(),
+                                             filtered=True, color_by=color_by)
         else:
-            color_by='bodypart'
-
-        if len(self.bodyparts) == 0:
-            self.bodyparts = 'all'
-
-        deeplabcut.create_labeled_video(self.config,self.filelist,self.videotype.GetValue(),shuffle=shuffle,
-                                         trainingsetindex=trainingsetindex, save_frames=self.slow, draw_skeleton=self.draw,
-                                         displayedbodyparts=self.bodyparts, trailpoints=self.trail_points.GetValue(),
-                                         filtered=filtered, color_by=color_by)
+            deeplabcut.create_labeled_video(self.config,self.filelist,self.videotype.GetValue(),shuffle=shuffle,
+                                             color_by='bodypart', trainingsetindex=trainingsetindex, save_frames=self.slow,
+                                             draw_skeleton= self.draw, displayedbodyparts=self.bodyparts,
+                                             trailpoints = self.trail_points.GetValue(),filtered=True)
+                                             
 
     def help_function(self,event):
 
