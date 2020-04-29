37dff446f1a74f75c52211d0ed589d303c30705a
AlexEMG <alexander@deeplabcut.org>
2020-04-28 21:53:04 -0400

    updated MB dataset and examplescript

--- examples-multianimal/exampledataset_birds.py
+++ examples-multianimal/exampledataset_birds.py
@@ -11,7 +11,6 @@ scorer='Daniel'
 
 #print("CREATING PROJECT")
 #path_config_file=deeplabcut.create_new_project(task,scorer,video,copy_videos=True,multianimal=True)
-
 basefolder='/media/alex/dropboxdisk/Dropbox/Collaborations/Cancer/DLCdev/examples-multianimal/MontBlanc-Daniel-2019-12-16'
 videotype='mov'
 
@@ -29,25 +28,24 @@ print("Plot labels...") #NEW:
 #deeplabcut.check_labels(path_config_file)
 #deeplabcut.check_labels(path_config_file,draw_skeleton=True,visualizeindividuals=True)
 #deeplabcut.check_labels(path_config_file,draw_skeleton=True,visualizeindividuals=False)
+'''
 
+'''
 print("Crop images...") #RECOMMENDED:
-#deeplabcut.cropimagesandlabels(path_config_file,userfeedback=False)
+deeplabcut.cropimagesandlabels(path_config_file,userfeedback=False)
 #then we uncommented the large-scale full frame data > it is not used for training!
-
-#cfg=deeplabcut.utils.auxiliaryfunctions.read_config(path_config_file)
-#deeplabcut.utils.auxfun_multianimal.getpafgraph(cfg)
 
 print("Plot labels...") #NEW:
 print("Creating multianimal training set...")
 deeplabcut.create_multianimaltraining_dataset(path_config_file,Shuffles=[shuffle])
 
-saveiters=5000
+saveiters=4000
 displayiters=500
-
+maxiters=12000
 print("Creating multianimal training set...")
 
 deeplabcut.train_network(path_config_file, shuffle=shuffle,trainingsetindex=trainingsetindex,
-    saveiters=saveiters,displayiters=displayiters,max_snapshots_to_keep=2,maxiters=20000)
+    saveiters=saveiters,displayiters=displayiters,max_snapshots_to_keep=2,maxiters=maxiters)
 
 trainposeconfigfile,testposeconfigfile,snapshotfolder=deeplabcut.return_train_network_path(path_config_file,shuffle=shuffle)
 cfg_dlc=deeplabcut.auxiliaryfunctions.read_plainconfig(testposeconfigfile)
@@ -59,8 +57,7 @@ deeplabcut.auxiliaryfunctions.write_plainconfig(testposeconfigfile,cfg_dlc)
 '''
 print("Evaluating network for shuffle ", shuffle)
 deeplabcut.evaluate_network(path_config_file,Shuffles=[shuffle],plotting=True)
-
-
+deeplabcut.evaluate_multianimal_crossvalidate(path_config_fil,Shuffles=[shuffle])
 
 ################## Analyze video #NEW:
 trainposeconfigfile,testposeconfigfile,snapshotfolder=deeplabcut.return_train_network_path(path_config_file,shuffle=shuffle)
@@ -71,12 +68,11 @@ edits = {'partaffinityfield_predict': True,
 cfg_dlc = deeplabcut.auxiliaryfunctions.edit_config(testposeconfigfile, edits)
 
 print("Starting inference for", shuffle)
-#deeplabcut.analyze_videos(path_config_file,[videopath],shuffle=shuffle,videotype=videotype)
+deeplabcut.analyze_videos(path_config_file,[videopath],shuffle=shuffle,videotype=videotype)
 #deeplabcut.analyze_videos(path_config_file,[videopath],shuffle=shuffle,videotype=videotype,c_engine=True)
 
-model='DLC_resnet50_MontBlancDec16shuffle0_20000'
-deeplabcut.create_video_with_all_detections(path_config_file, video[0], model)
+model='DLC_resnet50_MontBlancDec16shuffle'+str(shuffle)+'_'+str(maxiters)
+deeplabcut.create_video_with_all_detections(path_config_file, [video[0]], model)
 
 deeplabcut.convert_detections2tracklets(path_config_file,[videopath],shuffle=shuffle,videotype=videotype)
-
 deeplabcut.create_labeled_video(path_config_file,[videopath],shuffle=shuffle,videotype=videotype)
