9306c7aeeb47c8932ce994dd26187fb1a5fae735
AlexEMG <alexander@deeplabcut.org>
2020-05-12 15:46:24 -0400

    DLC b5

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,6 @@
 pip uninstall deeplabcut
 python3 setup.py sdist bdist_wheel
-pip install dist/deeplabcut-2.2b4-py3-none-any.whl
+pip install dist/deeplabcut-2.2b5-py3-none-any.whl
 
 ## This is just for me...
 #python3 examples-multianimal/exampledataset_birds.py
