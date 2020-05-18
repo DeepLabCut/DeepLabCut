37cb9ab435a992426f947430ba567ce8ebdcaaa9
Mackenzie Mathis <mathis@rowland.harvard.edu>
2020-05-18 17:22:19 -0400

    Update add.py

--- deeplabcut/create_project/add.py
+++ deeplabcut/create_project/add.py
@@ -1,9 +1,9 @@
 """
-DeepLabCut2.0 Toolbox (deeplabcut.org)
+DeepLabCut2.2 Toolbox (deeplabcut.org)
 © A. & M. Mathis Labs
 https://github.com/AlexEMG/DeepLabCut
-
 Please see AUTHORS for contributors.
+
 https://github.com/AlexEMG/DeepLabCut/blob/master/AUTHORS
 Licensed under GNU Lesser General Public License v3.0
 """
