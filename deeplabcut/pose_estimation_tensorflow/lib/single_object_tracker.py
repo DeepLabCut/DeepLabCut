aa549f47a253d1ebc763484b6d98898cce03e00e
Mackenzie Mathis <mathis@rowland.harvard.edu>
2020-06-13 13:02:43 -0400

    header change

--- deeplabcut/pose_estimation_tensorflow/lib/single_object_tracker.py
+++ deeplabcut/pose_estimation_tensorflow/lib/single_object_tracker.py
@@ -1,9 +1,11 @@
 """
 DeepLabCut 2.2 Toolbox (deeplabcut.org)
-© A. & M. Mathis Labs
-https://github.com/AlexEMG/DeepLabCut
+https://github.com/DeepLabCut/DeepLabCut
+
+© DeepLabCut authors
 Please see AUTHORS for contributors.
 https://github.com/AlexEMG/DeepLabCut/blob/master/AUTHORS
+
 Licensed under GNU Lesser General Public License v3.0
 
 This module was contributed by Tabet Ehsainieh - https://github.com/ehsainit
