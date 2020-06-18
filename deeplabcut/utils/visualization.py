ee81ed047e6d5fa52ec28281089bf2a605369a80
jeylau <30733203+jeylau@users.noreply.github.com>
2020-06-18 08:43:19 +0200

    Avoid drawing inexistent skeleton

--- deeplabcut/utils/visualization.py
+++ deeplabcut/utils/visualization.py
@@ -231,6 +231,7 @@ def make_labeled_images_from_dataframe(
     bodypart_names = bodyparts.unique()
     nbodyparts = len(bodypart_names)
     bodyparts = bodyparts[::2]
+    draw_skeleton = draw_skeleton and cfg['skeleton']  # Only draw if a skeleton is defined
 
     if color_by == "bodypart":
         map_ = bodyparts.map(dict(zip(bodypart_names, range(nbodyparts))))
