0743273c03dd8c867e36c6b4a20d4ba8d6d9a1b4
Anand <anand13394@gmail.com>
2020-08-29 20:28:42 +0530

    handled os.rename behaviour in windows environment

--- deeplabcut/generate_training_dataset/labeling_toolbox.py
+++ deeplabcut/generate_training_dataset/labeling_toolbox.py
@@ -902,13 +902,40 @@ class MainFrame(wx.Frame):
         """
 
         # Backup previous save
-        csv_path = os.path.join(self.dir, "CollectedData_" + self.scorer + ".csv") 
-        if os.path.exists( csv_path):
-        	os.rename( csv_path, csv_path.replace('.csv','.csv.backup'))
+        from sys import platform
 
+        csv_path = os.path.join(self.dir, "CollectedData_" + self.scorer + ".csv") 
         hdf_path = os.path.join(self.dir, "CollectedData_" + self.scorer + ".h5")
-        if os.path.exists( hdf_path):
-        	os.rename( hdf_path, hdf_path.replace('.h5','.h5.backup'))
+        csv_backup_path = csv_path.replace('.csv','.csv.backup')
+        hdf_backup_path = hdf_path.replace('.h5','.h5.backup')
+
+        if platform == 'linux' or platform == 'linux2':
+            if os.path.exists( csv_path):
+            	os.rename( csv_path, csv_backup_path)
+
+            if os.path.exists( hdf_path):
+            	os.rename( hdf_path, hdf_backup_path)
+        
+        elif platform == "win32":
+            if os.path.exists( csv_path):
+                if os.path.exists( csv_backup_path): #check if backupfile exists already
+                    os.remove( csv_backup_path) # requires double action as windows fails to rename file if exists already
+                    os.rename( csv_path, csv_backup_path)
+
+            if os.path.exists( hdf_path):
+                if os.path.exists( hdf_backup_path):
+                    os.remove( hdf_backup_path)
+                    os.rename( hdf_path, hdf_backup_path)
+        
+        elif platform == 'darwin':
+            try:
+                if os.path.exists( csv_path):
+                    os.rename( csv_path, csv_backup_path)
+
+                if os.path.exists( hdf_path):
+                    os.rename( hdf_path, hdf_backup_path)
+            except:
+                print(" Unexpected os.rename behaviour, try win32 approach")
 
         self.statusbar.SetStatusText("File saved")
         MainFrame.saveEachImage(self)
