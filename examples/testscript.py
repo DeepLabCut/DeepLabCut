b978d0cb3433c4b0870a5ba253fd608be4a8a752
AlexEMG <alexander@deeplabcut.org>
2021-01-06 23:48:27 +0100

    net comparison

--- examples/testscript.py
+++ examples/testscript.py
@@ -44,8 +44,8 @@ if __name__ == "__main__":
     dfolder = None
     net_type = "resnet_50"  #'mobilenet_v2_0.35' #'resnet_50'
 
-    # net_type='mobilenet_v2_0.35'
-    net_type='efficientnet-b0' #to -b6
+    net_type = "mobilenet_v2_0.35"
+    net_type = "efficientnet-b0"  # to -b6
 
     augmenter_type = "default"  # = imgaug!!
     augmenter_type2 = "scalecrop"
