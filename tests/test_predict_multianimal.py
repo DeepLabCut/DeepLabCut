8605e2483524126f6249172a9b5cd8fd44d8e1b7
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2021-05-20 18:44:12 +0200

    Fix tests

--- tests/test_predict_multianimal.py
+++ tests/test_predict_multianimal.py
@@ -18,32 +18,39 @@ def test_extract_detections(model_outputs, ground_truth_detections):
     inds_gt = np.concatenate(inds_gt).astype(int)
     pos_gt = np.concatenate(ground_truth_detections[0]["coordinates"][0])
     prob_gt = np.concatenate(ground_truth_detections[0]["confidence"])
-
     inds = predict_multianimal.find_local_peak_indices(scmaps, RADIUS, THRESHOLD)
-    pos = predict_multianimal.calc_peak_locations(locrefs, inds, STRIDE)
-    prob = tf.gather_nd(scmaps, inds)
     with tf.Session() as sess:
         inds = sess.run(inds)
-        pos = sess.run(pos)
-        prob = sess.run(prob)
+    pos = predict_multianimal.calc_peak_locations(locrefs, inds, STRIDE)
+    s, r, c, b = inds.T
+    prob = scmaps[s, r, c, b].reshape((-1, 1))
     idx = np.argsort(inds[:, -1], kind="mergesort")
     np.testing.assert_equal(inds[idx, 1:], inds_gt)
     np.testing.assert_almost_equal(pos[idx], pos_gt, decimal=3)
-    np.testing.assert_almost_equal(prob[idx], prob_gt.squeeze(), decimal=5)
+    np.testing.assert_almost_equal(prob[idx], prob_gt, decimal=5)
 
 
 def test_association_costs(model_outputs, ground_truth_detections):
     costs_gt = ground_truth_detections[0]["costs"]
-    aff_ref = costs_gt[0]["m1"]
-    lengths_ref = costs_gt[0]["distance"]
-    scmaps, _, pafs = model_outputs
     graph = [[i, j] for i in range(12) for j in range(i + 1, 12)]
-    inds_all = predict_multianimal.find_local_peak_indices(scmaps, RADIUS, THRESHOLD)
-    aff, lengths, *_ = predict_multianimal.compute_edge_costs(pafs, inds_all, graph)
-    aff = tf.reshape(aff[:aff_ref.size], aff_ref.shape)
-    lengths = tf.reshape(lengths[:lengths_ref.size], lengths_ref.shape)
-    with tf.compat.v1.Session() as sess:
-        aff = sess.run(aff)
-        lengths = sess.run(lengths)
-    np.testing.assert_equal(np.argmax(aff, axis=0), np.argmax(aff_ref, axis=0))
-    np.testing.assert_allclose(lengths, lengths_ref, atol=1.5)
+    preds = predict_multianimal.compute_peaks_and_costs(
+        *model_outputs,
+        graph=graph,
+        paf_inds=np.arange(len(graph)),
+        n_id_channels=0,
+        nms_radius=RADIUS,
+        min_confidence=THRESHOLD,
+        stride=STRIDE,
+    )[0]
+    assert all(k in preds for k in ("coordinates", "confidence", "costs"))
+    costs_pred = preds["costs"]
+    assert len(costs_pred) == len(costs_gt)
+    eq = [
+        np.array_equal(np.argmax(v["m1"], axis=0), np.argmax(costs_gt[k]["m1"], axis=0))
+        for k, v in costs_pred.items()
+    ]
+    assert sum(eq) == 60  # 6 arrays are unequal as cost computation was corrected
+    assert all(
+        np.allclose(v["distance"], costs_gt[k]["distance"], atol=1.5)
+        for k, v in costs_pred.items()
+    )
