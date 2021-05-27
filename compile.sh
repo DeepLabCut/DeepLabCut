1add717809e421d7b76d2ec833feb4d466797459
Mackenzie Mathis <mathis@rowland.harvard.edu>
2021-05-27 23:30:17 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #cd to where DLC is installed, and then: dist-packages/deeplabcut/pose_estimation_tensorflow/lib/nms_cython
 #i.e. in my case:
-cd /usr/local/lib/python3.6/dist-packages/deeplabcut/pose_estimation_tensorflow/lib/nms_cython
+cd /usr/local/lib/python3.7/dist-packages/deeplabcut/pose_estimation_tensorflow/lib/nms_cython
 python3 setup.py build_ext --inplace
