8a8b7306c67cf762e9db4301b9f8321f1333bf15
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2021-07-02 23:12:59 +0200

    Modest attempt to speed up target maps code

--- deeplabcut/pose_estimation_tensorflow/datasets/pose_multianimal_imgaug.py
+++ deeplabcut/pose_estimation_tensorflow/datasets/pose_multianimal_imgaug.py
@@ -17,11 +17,12 @@ import imageio
 import imgaug.augmenters as iaa
 import numpy as np
 from imgaug.augmentables import Keypoint, KeypointsOnImage
-from . import augmentation
-from .factory import PoseDatasetFactory
-from .pose_base import BasePoseDataset
-from .utils import DataItem, Batch
+from deeplabcut.pose_estimation_tensorflow.datasets import augmentation
+from deeplabcut.pose_estimation_tensorflow.datasets.factory import PoseDatasetFactory
+from deeplabcut.pose_estimation_tensorflow.datasets.pose_base import BasePoseDataset
+from deeplabcut.pose_estimation_tensorflow.datasets.utils import DataItem, Batch
 from deeplabcut.utils.auxfun_videos import imread
+from math import sqrt
 
 
 @PoseDatasetFactory.register("multi-animal-imgaug")
@@ -177,15 +178,15 @@ class MAImgaugPoseDataset(BasePoseDataset):
     def get_targetmaps_update(
         self, joint_ids, joints, data_items, sm_size, target_size
     ):
-        part_score_targets, part_score_weights, locref_targets, locref_masks = (
-            [],
-            [],
-            [],
-            [],
-        )
-        partaffinityfield_targets, partaffinityfield_masks = [], []
+        part_score_targets = []
+        part_score_weights = []
+        locref_targets = []
+        locref_masks = []
+        partaffinityfield_targets = []
+        partaffinityfield_masks = []
         for i in range(len(data_items)):
             # Approximating the scale
+            # FIXME I feel scale calculation is wrong
             scale = min(
                 target_size[0] / data_items[i].im_size[1],
                 target_size[1] / data_items[i].im_size[2],
@@ -209,7 +210,7 @@ class MAImgaugPoseDataset(BasePoseDataset):
                     partaffinityfield_target,
                     partaffinityfield_mask,
                 ) = self.compute_target_part_scoremap_numpy(
-                    joint_ids[i], [joints[i]], data_items[i], sm_size, scale
+                    joint_ids[i], joints[i], data_items[i], sm_size, scale
                 )
 
             part_score_targets.append(part_score_target)
@@ -277,12 +278,9 @@ class MAImgaugPoseDataset(BasePoseDataset):
             batch = {Batch.inputs: np.array(batch_images).astype(np.float64)}
             if self.has_gt:
                 targetmaps = self.get_targetmaps_update(
-                    joint_ids, batch_joints, data_items, sm_size, image_shape
+                    joint_ids, batch_joints, data_items, (sm_size[1], sm_size[0]), image_shape
                 )
                 batch.update(targetmaps)
-
-            # if returndata:
-            #        return batch_images,batch_joints,targetmaps
 
             batch = {key: np.array(data) for (key, data) in batch.items()}
             batch[Batch.data_item] = data_items
@@ -332,20 +330,19 @@ class MAImgaugPoseDataset(BasePoseDataset):
         self, joint_id, coords, data_item, size, scale
     ):
         stride = self.cfg["stride"]
+        half_stride = stride // 2
         dist_thresh = float(self.cfg["pos_dist_thresh"] * scale)
         num_idchannel = self.cfg.get("num_idchannel", 0)
 
         num_joints = self.cfg["num_joints"]
-        half_stride = stride / 2
 
-        scmap = np.zeros(np.concatenate([size, np.array([num_joints + num_idchannel])]))
-        locref_size = np.concatenate([size, np.array([num_joints * 2])])
-
+        scmap = np.zeros((*size, num_joints + num_idchannel))
+        locref_size = *size, num_joints * 2
         locref_map = np.zeros(locref_size)
         locref_scale = 1.0 / self.cfg["locref_stdev"]
         dist_thresh_sq = dist_thresh ** 2
 
-        partaffinityfield_shape = np.concatenate([size, np.array([self.cfg["num_limbs"] * 2])])
+        partaffinityfield_shape = *size, self.cfg["num_limbs"] * 2
         partaffinityfield_map = np.zeros(partaffinityfield_shape)
         if self.cfg["weigh_only_present_joints"]:
             partaffinityfield_mask = np.zeros(partaffinityfield_shape)
@@ -354,38 +351,33 @@ class MAImgaugPoseDataset(BasePoseDataset):
             partaffinityfield_mask = np.ones(partaffinityfield_shape)
             locref_mask = np.ones(locref_size)
 
-        width = size[1]
-        height = size[0]
+        height, width = size
         grid = np.mgrid[:height, :width].transpose((1, 2, 0))
+        xx = np.expand_dims(grid[..., 1], axis=2)
+        yy = np.expand_dims(grid[..., 0], axis=2)
 
-        # the animal id plays no role for scoremap + locref!
-        # so let's just loop over all bpts.
-        for k, j_id in enumerate(np.concatenate(joint_id)):
-            joint_pt = coords[0][k, :]
-            j_x = joint_pt[0].item()
-            j_x_sm = round((j_x - half_stride) / stride)
-            j_y = joint_pt[1].item()
-            j_y_sm = round((j_y - half_stride) / stride)
-
-            min_x = round(max(j_x_sm - dist_thresh - 1, 0))
-            max_x = round(min(j_x_sm + dist_thresh + 1, width - 1))
-            min_y = round(max(j_y_sm - dist_thresh - 1, 0))
-            max_y = round(min(j_y_sm + dist_thresh + 1, height - 1))
-            x = grid.copy()[:, :, 1]
-            y = grid.copy()[:, :, 0]
-            dx = j_x - x * stride - half_stride
-            dy = j_y - y * stride - half_stride
-            dist = dx ** 2 + dy ** 2
-            mask1 = dist <= dist_thresh_sq
-            mask2 = (x >= min_x) & (x <= max_x)
-            mask3 = (y >= min_y) & (y <= max_y)
-            mask = mask1 & mask2 & mask3
-            scmap[mask, j_id] = 1
+        # Produce score maps and location refinement fields
+        coords_sm = np.round((coords - half_stride) / stride).astype(int)
+        mins = np.round(np.maximum(coords_sm - dist_thresh - 1, 0)).astype(int)
+        maxs = np.round(
+            np.minimum(coords_sm + dist_thresh + 1, [width - 1, height - 1])
+        ).astype(int)
+        dx = coords[:, 0] - xx * stride - half_stride
+        dx_ = dx * locref_scale
+        dy = coords[:, 1] - yy * stride - half_stride
+        dy_ = dy * locref_scale
+        dist = dx ** 2 + dy ** 2
+        mask1 = dist <= dist_thresh_sq
+        mask2 = (xx >= mins[:, 0]) & (xx <= maxs[:, 0])
+        mask3 = (yy >= mins[:, 1]) & (yy <= maxs[:, 1])
+        mask = mask1 & mask2 & mask3
+        for n, ind in enumerate(np.concatenate(joint_id).tolist()):
+            mask_ = mask[..., n]
+            scmap[mask_, ind] = 1
             if self.cfg["weigh_only_present_joints"]:
-                locref_mask[mask, j_id * 2 + 0] = 1.0
-                locref_mask[mask, j_id * 2 + 1] = 1.0
-            locref_map[mask, j_id * 2 + 0] = (dx * locref_scale)[mask]
-            locref_map[mask, j_id * 2 + 1] = (dy * locref_scale)[mask]
+                locref_mask[mask_, [ind * 2 + 0, ind * 2 + 1]] = 1.0
+            locref_map[mask_, ind * 2 + 0] = dx_[mask_, n]
+            locref_map[mask_, ind * 2 + 1] = dy_[mask_, n]
 
         if num_idchannel > 0:
             coordinateoffset = 0
@@ -393,102 +385,67 @@ class MAImgaugPoseDataset(BasePoseDataset):
             idx = [i for i, id_ in enumerate(data_item.joints)
                    if id_ < num_idchannel]
             for person_id in idx:
-                joint_ids = joint_id[person_id].copy()
-                if len(joint_ids) > 1:
-                    for k, j_id in enumerate(joint_ids):
-                        joint_pt = coords[0][k + coordinateoffset, :]
-                        j_x = joint_pt[0].item()
-                        j_x_sm = round((j_x - half_stride) / stride)
-                        j_y = joint_pt[1].item()
-                        j_y_sm = round((j_y - half_stride) / stride)
-
-                        min_x = round(max(j_x_sm - dist_thresh - 1, 0))
-                        max_x = round(min(j_x_sm + dist_thresh + 1, width - 1))
-                        min_y = round(max(j_y_sm - dist_thresh - 1, 0))
-                        max_y = round(min(j_y_sm + dist_thresh + 1, height - 1))
-                        x = grid.copy()[:, :, 1]
-                        y = grid.copy()[:, :, 0]
-                        dx = j_x - x * stride - half_stride
-                        dy = j_y - y * stride - half_stride
-                        dist = dx ** 2 + dy ** 2
-                        mask1 = dist <= dist_thresh_sq
-                        mask2 = (x >= min_x) & (x <= max_x)
-                        mask3 = (y >= min_y) & (y <= max_y)
-                        mask = mask1 & mask2 & mask3
-                        scmap[mask, person_id + num_joints] = 1
+                joint_ids = joint_id[person_id]
+                if joint_ids.size > 1:  # FIXME Why > 1??
+                    inds = joint_ids + coordinateoffset
+                    mask_ = mask[..., inds].sum(axis=2)
+                    scmap[mask_, person_id + num_joints] = 1
                 coordinateoffset += len(joint_ids)
 
-        x = grid.copy()[:, :, 1]
-        y = grid.copy()[:, :, 0]
-
-        # if self.cfg.partaffinityfield_predict:
-        # print("hello",joint_id)
-        # print(np.concatenate(joint_id)) #this is all joint_ids for all individuals!
         coordinateoffset = 0  # the offset based on
+        y, x = np.rollaxis(grid * stride + half_stride, 2)
         for person_id in range(len(joint_id)):
-            # for k, joint_ids in enumerate(joint_id[person_id]):
-            joint_ids = joint_id[person_id].copy()
-            if len(joint_ids) > 1:  # otherwise there cannot be a joint!
-                # CONSIDER SMARTER SEARCHES here... (i.e. calculate the bpts beforehand?)
-                for l in range(self.cfg["num_limbs"]):
-                    bp1, bp2 = self.cfg["partaffinityfield_graph"][l]
-                    I1 = np.where(np.array(joint_ids) == bp1)[0]
-                    I2 = np.where(np.array(joint_ids) == bp2)[0]
-                    if (len(I1) > 0) * (len(I2) > 0):
-                        indbp1 = I1[0].item()
-                        indbp2 = I2[0].item()
-                        j_x = (coords[0][indbp1 + coordinateoffset, 0]).item()
-                        j_y = (coords[0][indbp1 + coordinateoffset, 1]).item()
+            joint_ids = joint_id[person_id].tolist()
+            if len(joint_ids) < 2:  # there is no possible edge
+                continue
+            for l, (bp1, bp2) in enumerate(self.cfg["partaffinityfield_graph"]):
+                try:
+                    ind1 = joint_ids.index(bp1)
+                except ValueError:
+                    continue
+                try:
+                    ind2 = joint_ids.index(bp2)
+                except ValueError:
+                    continue
+                j_x, j_y = coords[ind1 + coordinateoffset]
+                linkedj_x, linkedj_y = coords[ind2 + coordinateoffset]
+                dist = sqrt((linkedj_x - j_x) ** 2 + (linkedj_y - j_y) ** 2)
+                if dist > 0:
+                    Dx = (linkedj_x - j_x) / dist  # x-axis UNIT VECTOR
+                    Dy = (linkedj_y - j_y) / dist
+                    d1 = [
+                        Dx * j_x + Dy * j_y,
+                        Dx * linkedj_x + Dy * linkedj_y,
+                    ]  # in-line with direct axis
+                    d1lowerboundary = min(d1)
+                    d1upperboundary = max(d1)
+                    d2mid = j_y * Dx - j_x * Dy  # orthogonal direction
 
-                        linkedj_x = (coords[0][indbp2 + coordinateoffset, 0]).item()
-                        linkedj_y = (coords[0][indbp2 + coordinateoffset, 1]).item()
-
-                        dist = np.sqrt((linkedj_x - j_x) ** 2 + (linkedj_y - j_y) ** 2)
-                        if dist > 0:
-                            Dx = (linkedj_x - j_x) * 1.0 / dist  # x-axis UNIT VECTOR
-                            Dy = (linkedj_y - j_y) * 1.0 / dist
-
-                            d1 = [
-                                Dx * j_x + Dy * j_y,
-                                Dx * linkedj_x + Dy * linkedj_y,
-                            ]  # in-line with direct axis
-                            d1lowerboundary = min(d1)
-                            d1upperboundary = max(d1)
-                            d2mid = j_y * Dx - j_x * Dy  # orthogonal direction
-
-                            distance_along = Dx * (x * stride + half_stride) + Dy * (
-                                y * stride + half_stride
+                    distance_along = Dx * x + Dy * y
+                    distance_across = (
+                        (
+                            (
+                                y * Dx
+                                - x * Dy
                             )
-                            distance_across = (
-                                (
-                                    (
-                                        (y * stride + half_stride) * Dx
-                                        - (x * stride + half_stride) * Dy
-                                    )
-                                    - d2mid
-                                )
-                                * 1.0
-                                / self.cfg["pafwidth"]
-                                * scale
-                            )
+                            - d2mid
+                        )
+                        * 1.0
+                        / self.cfg["pafwidth"]
+                        * scale
+                    )
 
-                            mask1 = (distance_along >= d1lowerboundary) & (
-                                distance_along <= d1upperboundary
-                            )
-                            mask2 = np.abs(distance_across) <= 1
-                            # mask3 = ((x >= 0) & (x <= width-1))
-                            # mask4 = ((y >= 0) & (y <= height-1))
-                            mask = mask1 & mask2  # &mask3 &mask4
-                            if self.cfg["weigh_only_present_joints"]:
-                                partaffinityfield_mask[mask, l * 2 + 0] = 1.0
-                                partaffinityfield_mask[mask, l * 2 + 1] = 1.0
-
-                            partaffinityfield_map[mask, l * 2 + 0] = (
-                                Dx * (1 - abs(distance_across))
-                            )[mask]
-                            partaffinityfield_map[mask, l * 2 + 1] = (
-                                Dy * (1 - abs(distance_across))
-                            )[mask]
+                    mask1 = (distance_along >= d1lowerboundary) & (
+                        distance_along <= d1upperboundary
+                    )
+                    distance_across_abs = np.abs(distance_across)
+                    mask2 = distance_across_abs <= 1
+                    mask = mask1 & mask2
+                    temp = 1 - distance_across_abs[mask]
+                    if self.cfg["weigh_only_present_joints"]:
+                        partaffinityfield_mask[mask, [l * 2 + 0, l * 2 + 1]] = 1.0
+                    partaffinityfield_map[mask, l * 2 + 0] = Dx * temp
+                    partaffinityfield_map[mask, l * 2 + 1] = Dy * temp
 
             coordinateoffset += len(joint_ids)  # keeping track of the blocks
 
