1055f9771c5e2ca3e3bf3c4a8e74603e672f27a6
AnnaPaulish <apaulish99@gmail.com>
2021-12-02 14:36:56 +0100

    main_update

--- MainWindow.py
+++ MainWindow.py
@@ -1,23 +1,11 @@
-import sys
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QTabWidget
-from PyQt5.QtWidgets import QWidget, QLabel, QRadioButton, QFormLayout, QFileDialog
-from PyQt5.QtGui import QIcon
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QGridLayout, QFrame, QGraphicsDropShadowEffect, QMenu
+from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import QMenu, QLabel
+from PyQt5.QtWidgets import QVBoxLayout
 
-from PyQt5.QtWidgets import QHBoxLayout, QPushButton, QVBoxLayout, QSizePolicy, QStatusBar
-
-from PyQt5.QtWidgets import (
-    QCheckBox, QComboBox, QLineEdit,
-    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
-)
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui
 
-
-#from MainApp import *
 from CreateProject import *
 from OpenProject import *
 from extract_frames import *
