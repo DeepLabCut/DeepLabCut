56febd80c6b942a7f7ba785a03972ebf5ad15188
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2022-01-07 17:07:28 +0100

    Easily mine tracklets from the TrackletStitcher (#1634)
    * Add tracklet miner

--- deeplabcut/refine_training_dataset/stitch.py
+++ deeplabcut/refine_training_dataset/stitch.py
@@ -477,6 +477,13 @@ class TrackletStitcher:
             label: k for k, v in self._mapping.items() for label in v.values()
         }
 
+        # Store tracklets and corresponding negatives (those that overlap in time)
+        self._lu_overlap = defaultdict(list)
+        for tracklet1, tracklet2 in combinations(self, 2):
+            if tracklet2 in tracklet1:
+                self._lu_overlap[tracklet1].append(tracklet2)
+                self._lu_overlap[tracklet2].append(tracklet1)
+
     def __getitem__(self, item):
         return self.tracklets[item]
 
@@ -571,6 +578,32 @@ class TrackletStitcher:
                         max_gap = val
                     break
         return max_gap
+
+    def mine(self, n_samples):
+        p = np.asarray([t.likelihood for t in self])
+        p /= p.sum()
+        triplets = []
+        while len(triplets) != n_samples:
+            tracklet = np.random.choice(self, p=p)
+            overlapping_tracklets = self._lu_overlap[tracklet]
+            if not overlapping_tracklets:
+                continue
+            # Pick the closest (spatially) overlapping tracklet
+            ind_min = np.argmin([tracklet.distance_to(t) for t in overlapping_tracklets])
+            overlapping_tracklet = overlapping_tracklets[ind_min]
+            common_inds = set(tracklet.inds).intersection(overlapping_tracklet.inds)
+            ind_anchor = np.random.choice(list(common_inds))
+            anchor = tracklet.get_data_at(ind_anchor)[:, :2].astype(int)
+            neg = overlapping_tracklet.get_data_at(ind_anchor)[:, :2].astype(int)
+            ind_pos = np.random.choice(tracklet.inds[tracklet.inds != ind_anchor])
+            pos = tracklet.get_data_at(ind_pos)[:, :2].astype(int)
+            triplet = (
+                (anchor, ind_anchor),
+                (pos, ind_pos),
+                (neg, ind_anchor)
+            )
+            triplets.append(triplet)
+        return triplets
 
     def build_graph(
         self,
