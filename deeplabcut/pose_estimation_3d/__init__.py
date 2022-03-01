b6713458efb3d70505fb1a3805aa1cd3f18d21ba
intergalactic-mammoth <aristotelis.economides@gmail.com>
2022-03-01 15:55:40 +0100

    remove deeplabcut files from the GUI repo

--- deeplabcut/pose_estimation_3d/__init__.py
+++
@@ -1,3 +0,0 @@
-from deeplabcut.pose_estimation_3d.camera_calibration import *
-from deeplabcut.pose_estimation_3d.plotting3D import *
-from deeplabcut.pose_estimation_3d.triangulation import *
--- deeplabcut/pose_estimation_tensorflow/util/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Adapted from DeeperCut by Eldar Insafutdinov:
-https://github.com/eldar/pose-tensorflow
-"""
-
-from deeplabcut.pose_estimation_tensorflow.util.logging import *
