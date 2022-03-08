ff6bd872c35de47319ac5127096fa8b2d8abe66f
Alexander Mathis <mathisepfl@gmail.com>
2022-03-08 16:37:16 +0100

    False by default to be consistent with prior workflow

--- deeplabcut/create_project/add.py
+++ deeplabcut/create_project/add.py
@@ -9,7 +9,7 @@ Licensed under GNU Lesser General Public License v3.0
 """
 
 
-def add_new_videos(config, videos, copy_videos=False, coords=None, extract_frames=True):
+def add_new_videos(config, videos, copy_videos=False, coords=None, extract_frames=False):
     """
     Add new videos to the config file at any stage of the project.
 
