59898449849b3e18cf2dd1a119bbdfb8a74069f8
Alexander Mathis <mathisepfl@gmail.com>
2022-03-08 16:44:44 +0100

    filter video_list by the ones in the config file

--- deeplabcut/generate_training_dataset/frame_extraction.py
+++ deeplabcut/generate_training_dataset/frame_extraction.py
@@ -74,7 +74,7 @@ def extract_frames(
     slider_width=25,
     config3d=None,
     extracted_cam=0,
-    videos=None,
+    videos_list=None,
 ):
     """
     Extracts frames from the videos in the config.yaml file. Only the videos in the config.yaml will be used to select the frames.\n
@@ -143,8 +143,9 @@ def extract_frames(
         The index of the camera that already has extracted frames. This will match frame numbers to extract for all other cameras.
         This parameter is necessary if you wish to use epipolar lines in the labeling toolbox. Only use if mode = 'match' and config3d is provided.
 
-    videos: list, default: None
-            A list of the string containing full paths to videos to extract frames for. If this is left as None all videos in the config file will have frames extracted.
+    videos_list: list, default: None
+            A list of the string containing full paths to videos to extract frames for. If this is left as None all videos specified in the config file will have frames extracted. 
+            Otherwise one can select a subset by passing those paths. 
 
     Examples
     --------
@@ -210,8 +211,11 @@ def extract_frames(
             raise Exception(
                 "Perhaps consider extracting more, or a natural number of frames."
             )
-        if videos is None:
+        if videos_list is None:
             videos = cfg.get("video_sets_original") or cfg["video_sets"]
+        else: #filter video_list by the ones in the config file
+            videos = [v for v in cfg["video_sets"] if v in videos_list]
+            
         if opencv:
             from deeplabcut.utils.auxfun_videos import VideoReader
         else:
