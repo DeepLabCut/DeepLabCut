8d71e3b1a48c7f38929e336adcec9cbce7fd6499
Mackenzie Mathis <mathis@rowland.harvard.edu>
2022-04-09 17:39:42 +0200

    put back video type box

--- deeplabcut/gui/analyze_videos.py
+++ deeplabcut/gui/analyze_videos.py
@@ -103,8 +103,20 @@ class Analyze_videos(wx.Panel):
         self.shuffle = wx.SpinCtrl(self, value="1", min=0, max=100)
         shuffle_boxsizer.Add(self.shuffle, 1, wx.EXPAND | wx.TOP | wx.BOTTOM, 1)
 
+        videotype_text = wx.StaticBox(self, label="Specify the videotype")
+        videotype_text_boxsizer = wx.StaticBoxSizer(videotype_text, wx.VERTICAL)
+
+        videotypes = [".avi", ".mp4", ".mov"]
+        self.videotype = wx.ComboBox(self, choices=videotypes, style=wx.CB_READONLY)
+        self.videotype.SetValue(".avi")
+        videotype_text_boxsizer.Add(
+            self.videotype, 1, wx.EXPAND | wx.TOP | wx.BOTTOM, 1
+        )
+
+
         boxsizer.Add(self.hbox1, 0, wx.EXPAND | wx.TOP | wx.BOTTOM, 0)
         self.hbox1.Add(shuffle_boxsizer, 0, 0)
+        self.hbox1.Add(videotype_text_boxsizer, 0, 0)
 
         if self.cfg.get("multianimalproject", False):
             ntracks_text = wx.StaticBox(self, label="Number of animals")
@@ -448,6 +460,7 @@ class Analyze_videos(wx.Panel):
     def analyze_videos(self, event):
 
         shuffle = self.shuffle.GetValue()
+        video_type = self.videotype.GetValue(),
 
         if self.cfg.get("multianimalproject", False):
             print("DLC network loading and video analysis starting ... ")
@@ -479,6 +492,7 @@ class Analyze_videos(wx.Panel):
             scorername = deeplabcut.analyze_videos(
                 self.config,
                 self.filelist,
+                self.videotype.GetValue(),
                 shuffle=shuffle,
                 gputouse=None,
                 cropping=crop,
@@ -518,6 +532,7 @@ class Analyze_videos(wx.Panel):
             scorername = deeplabcut.analyze_videos(
                 self.config,
                 self.filelist,
+                self.videotype.GetValue(),
                 shuffle=shuffle,
                 gputouse=None,
                 save_as_csv=save_as_csv,
