75df30f0a0a600949d076780b55082fe80dbe305
intergalactic-mammoth <aristotelis.economides@gmail.com>
2022-04-12 12:05:37 +0200

    update train network

--- train_network.py
+++ train_network.py
@@ -11,8 +11,7 @@ from PySide2.QtGui import QIcon
 import deeplabcut
 from deeplabcut.utils import auxiliaryfunctions
 
-from widgets import ConfigEditor
-from components import EditYamlButton
+from components import EditYamlButton, _create_horizontal_layout, _create_label_widget
 
 
 class TrainNetwork(QWidget):
@@ -24,11 +23,13 @@ class TrainNetwork(QWidget):
         self.model_comparison = False
         self.pose_cfg_choice = False
         self.config = cfg
+        self.cfg = auxiliaryfunctions.read_config(self.config)
+
 
         self.main_layout = QtWidgets.QVBoxLayout(self)
         self.main_layout.setAlignment(Qt.AlignTop)
         self.main_layout.setSpacing(20)
-        self.main_layout.setContentsMargins(0, 20, 0, 20)
+        self.main_layout.setContentsMargins(0, 20, 10, 20)
         self.setLayout(self.main_layout)
 
         # use the default pose_cfg file for default values
@@ -52,75 +53,99 @@ class TrainNetwork(QWidget):
         separatorLine.setMidLineWidth(1)
 
         l1_step1 = QtWidgets.QLabel("DeepLabCut - Step 5. Train network")
+        l1_step1.setStyleSheet("font:bold")    
         l1_step1.setContentsMargins(20, 0, 0, 10)
 
         self.main_layout.addWidget(l1_step1)
         self.main_layout.addWidget(separatorLine)
 
-        layout_cfg = QtWidgets.QHBoxLayout()
-        layout_cfg.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        layout_cfg.setSpacing(20)
-        layout_cfg.setContentsMargins(20, 10, 300, 0)
-        cfg_text = QtWidgets.QLabel("Select the config file")
-        cfg_text.setContentsMargins(0, 0, 60, 0)
+        layout_config = _create_horizontal_layout()
+        self._generate_config_layout(layout_config)
+        self.main_layout.addLayout(layout_config)
 
-        self.cfg_line = QtWidgets.QLineEdit()
-        self.cfg_line.setMaximumWidth(800)
-        self.cfg_line.setMinimumWidth(600)
-        self.cfg_line.setMinimumHeight(30)
-        self.cfg_line.setText(self.config)
-        self.cfg_line.textChanged[str].connect(self.update_cfg)
-
-        browse_button = QtWidgets.QPushButton("Browse")
-        browse_button.setMaximumWidth(100)
-        browse_button.clicked.connect(self.browse_dir)
-
-        layout_cfg.addWidget(cfg_text)
-        layout_cfg.addWidget(self.cfg_line)
-        layout_cfg.addWidget(browse_button)
-
-        self.main_layout.addLayout(layout_cfg)
+        self.main_layout.addWidget(
+            _create_label_widget("Attributes", "font:bold")
+        )
+        tmp_layout = _create_horizontal_layout()
+        self._generate_layout_attributes(tmp_layout)
+        self.main_layout.addLayout(tmp_layout)
 
         self.layout_attributes = QtWidgets.QVBoxLayout()
         self.layout_attributes.setAlignment(Qt.AlignTop)
-        self.layout_attributes.setSpacing(20)
-        self.layout_attributes.setContentsMargins(0, 0, 40, 0)
-
-        label = QtWidgets.QLabel("Optional Attributes")
-        label.setContentsMargins(20, 20, 0, 10)
-        self.layout_attributes.addWidget(label)
-
-        self.layout_specify_edit = QtWidgets.QHBoxLayout()
-        self.layout_specify_edit.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-
-        self.layout_specify_edit.setSpacing(20)
-        self.layout_specify_edit.setContentsMargins(20, 0, 50, 20)
-
-        self._specify_shuffle()
-        self._specify_ts_index()
-        self._edit_file()
+        self.layout_attributes.setContentsMargins(0, 0, 10, 0)
 
         self.layout_display = QtWidgets.QHBoxLayout()
         self.layout_display.setAlignment(Qt.AlignLeft | Qt.AlignTop)
 
         self.layout_display.setSpacing(20)
-        self.layout_display.setContentsMargins(20, 0, 50, 0)
+        self.layout_display.setContentsMargins(0, 0, 20, 0)
 
         self._display()
         self._save()
         self._max()
         self._num_of_snapshots()
 
-        self.layout_attributes.addLayout(self.layout_specify_edit)
         self.layout_attributes.addLayout(self.layout_display)
 
-        self.ok_button = QtWidgets.QPushButton("Ok")
-        self.ok_button.setContentsMargins(0, 40, 40, 40)
-        self.ok_button.clicked.connect(self.train_network)
+        self.pose_cfg_path = os.path.join(
+            self.cfg["project_path"],
+            auxiliaryfunctions.GetModelFolder(
+                self.cfg["TrainingFraction"][int(self.trainingindex.value())],
+                int(self.shuffle.value()),
+                self.cfg,
+            ),
+            "train",
+            "pose_cfg.yaml",
+        )
+        self.edit_posecfg_btn = EditYamlButton(
+            "Edit pose_cfg.yaml", self.pose_cfg_path, parent=self)
 
+        self.ok_button = QtWidgets.QPushButton("Train Network")
+        self.ok_button.clicked.connect(self.train_network)
+        
+        self.layout_attributes.addWidget(self.edit_posecfg_btn, alignment=Qt.AlignRight)
         self.layout_attributes.addWidget(self.ok_button, alignment=Qt.AlignRight)
 
         self.main_layout.addLayout(self.layout_attributes)
+
+    def _generate_layout_attributes(self, layout):
+        # Shuffle
+        opt_text = QtWidgets.QLabel("Shuffle")
+        self.shuffle = QSpinBox()
+        self.shuffle.setMaximum(100)
+        self.shuffle.setValue(1)
+        self.shuffle.setMinimumHeight(30)
+
+        layout.addWidget(opt_text)
+        layout.addWidget(self.shuffle)
+
+        # Trainingset index
+        opt_text = QtWidgets.QLabel("Trainingset index")
+        self.trainingindex = QSpinBox()
+        self.trainingindex.setMaximum(100)
+        self.trainingindex.setValue(0)
+        self.trainingindex.setMinimumHeight(30)
+
+        layout.addWidget(opt_text)
+        layout.addWidget(self.trainingindex)
+
+    def _generate_config_layout(self, layout):
+        cfg_text = QtWidgets.QLabel("Active config file:")
+
+        self.cfg_line = QtWidgets.QLineEdit()
+        self.cfg_line.setMinimumHeight(30)
+        self.cfg_line.setText(self.config)
+        self.cfg_line.textChanged[str].connect(self.update_cfg)
+
+        browse_button = QtWidgets.QPushButton("Browse")
+        browse_button.setMaximumWidth(100)
+        browse_button.setMinimumHeight(30)
+        browse_button.clicked.connect(self.browse_dir)
+
+        layout.addWidget(cfg_text)
+        layout.addWidget(self.cfg_line)
+        layout.addWidget(browse_button)
+
 
     def update_cfg(self):
         text = self.proj_line.text()
@@ -135,61 +160,6 @@ class TrainNetwork(QWidget):
             return
         self.config = config[0]
         self.cfg_line.setText(self.config)
-
-    def _specify_shuffle(self):
-        l_opt = QtWidgets.QVBoxLayout()
-        l_opt.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        l_opt.setSpacing(20)
-        l_opt.setContentsMargins(20, 0, 0, 0)
-
-        opt_text = QtWidgets.QLabel("Specify the shuffle")
-        self.shuffles = QSpinBox()
-        self.shuffles.setValue(1)
-        self.shuffles.setMinimumWidth(400)
-        self.shuffles.setMinimumHeight(30)
-
-        l_opt.addWidget(opt_text)
-        l_opt.addWidget(self.shuffles)
-        self.layout_specify_edit.addLayout(l_opt)
-
-    def _specify_ts_index(self):
-        l_opt = QtWidgets.QVBoxLayout()
-        l_opt.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        l_opt.setSpacing(20)
-        l_opt.setContentsMargins(20, 0, 0, 0)
-
-        opt_text = QtWidgets.QLabel("Specify the trainingset index")
-        self.trainingindex = QSpinBox()
-        self.trainingindex.setValue(0)
-        self.trainingindex.setMinimumWidth(400)
-        self.trainingindex.setMinimumHeight(30)
-
-        l_opt.addWidget(opt_text)
-        l_opt.addWidget(self.trainingindex)
-        self.layout_specify_edit.addLayout(l_opt)
-
-    def _edit_file(self):
-        l_opt = QtWidgets.QVBoxLayout()
-        l_opt.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        l_opt.setSpacing(20)
-        l_opt.setContentsMargins(20, 0, 0, 0)
-
-        cfg = auxiliaryfunctions.read_config(self.config)
-        self.pose_cfg_path = os.path.join(
-            cfg["project_path"],
-            auxiliaryfunctions.GetModelFolder(
-                cfg["TrainingFraction"][int(self.trainingindex.value())],
-                int(self.shuffles.value()),
-                cfg,
-            ),
-            "train",
-            "pose_cfg.yaml",
-        )
-        self.edit_posecfg_btn = EditYamlButton(
-            "Edit pose_cfg.yaml", self.pose_cfg_path, parent=self)
-
-        l_opt.addWidget(self.edit_posecfg_btn)
-        self.layout_specify_edit.addLayout(l_opt)
 
     def _display(self):
         l_opt = QtWidgets.QVBoxLayout()
@@ -269,17 +239,12 @@ class TrainNetwork(QWidget):
         self.layout_display.addLayout(l_opt)
 
     def train_network(self):
-        #### children?
-        shuffle = int(self.shuffles.value())
-        ####
+
+        shuffle = int(self.shuffle.value())
         trainingsetindex = int(self.trainingindex.value())
-        ####
         max_snapshots_to_keep = int(self.snapshots.value())
-        ####
         displayiters = int(self.display_iters_spin.value())
-        ####
         saveiters = int(self.save_iters_spin.value())
-        ####
         maxiters = int(self.max_iters_spin.value())
 
         deeplabcut.train_network(
