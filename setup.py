18a035be9d515694d9550aecb4e63cb4301f87cd
AlexEMG <alexander@deeplabcut.org>
2022-04-12 15:45:56 +0200

    2.2.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deeplabcut",
-    version="2.2.1rc1",
+    version="2.2.1",
     author="A. & M. Mathis Labs",
     author_email="alexander@deeplabcut.org",
     description="Markerless pose-estimation of user-defined features with deep learning",
