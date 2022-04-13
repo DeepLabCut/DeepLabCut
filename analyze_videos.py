7b252c3ccf6abdb9054cdb39f5d36789ea50b2cd
intergalactic-mammoth <aristotelis.economides@gmail.com>
2022-04-13 09:21:44 +0200

    fix layout

--- analyze_videos.py
+++ analyze_videos.py
@@ -13,6 +13,7 @@ from PySide2.QtWidgets import (
 
 from widgets import ConfigEditor
 from components import (
+    _create_grid_layout,
     _create_label_widget,
     _create_horizontal_layout,
     _create_vertical_layout,
@@ -62,10 +63,21 @@ class AnalyzeVideos(QWidget):
         self.main_layout.addWidget(
             _create_label_widget(
                 "DeepLabCut - Step 7. Analyze Videos",
-                "font:bold; font-size:18px;",
-                (20, 20, 0, 10),
+                "font:bold;",
+                (10, 10, 0, 10),
             )
         )
+
+        self.separatorLine = QtWidgets.QFrame()
+        self.separatorLine.setFrameShape(QtWidgets.QFrame.HLine)
+        self.separatorLine.setFrameShadow(QtWidgets.QFrame.Raised)
+
+        self.separatorLine.setLineWidth(0)
+        self.separatorLine.setMidLineWidth(1)
+
+        self.main_layout.addWidget(self.separatorLine)
+        dummy_space = _create_label_widget("", margins=(0, 5, 0, 0))
+        self.main_layout.addWidget(dummy_space)
 
         layout_config = _create_horizontal_layout()
         self._generate_config_layout(layout_config)
@@ -76,40 +88,34 @@ class AnalyzeVideos(QWidget):
         self._generate_layout_video_analysis(self.layout_video_selection)
         self.main_layout.addLayout(self.layout_video_selection)
 
+        tmp_layout = _create_horizontal_layout()
+
         self.main_layout.addWidget(
             _create_label_widget("Analysis Attributes", "font:bold")
         )
-        self.layout_attributes = _create_horizontal_layout()
+        self.layout_attributes = _create_grid_layout()
         self._generate_layout_attributes(self.layout_attributes)
-        self.main_layout.addLayout(self.layout_attributes)
+        tmp_layout.addLayout(self.layout_attributes)
 
         # Single / Multi animal Only Layouts
         self.layout_single_animal = _create_horizontal_layout()
 
-        self.layout_multi_animal = _create_vertical_layout()
+        self.layout_multi_animal = _create_horizontal_layout()
+
 
         if self.cfg["multianimalproject"]:
-            self.main_layout.addWidget(
-                _create_label_widget("Multi-animal settings", "font:bold")
-            )
             self._generate_layout_multianimal_only_options(self.layout_multi_animal)
-            self.main_layout.addLayout(self.layout_multi_animal)
+            tmp_layout.addLayout(self.layout_multi_animal)
         else:
-            self.main_layout.addWidget(
-                _create_label_widget("Single-animal settings", "font:bold")
-            )
             self._generate_layout_single_animal(self.layout_single_animal)
-            self.main_layout.addLayout(self.layout_single_animal)
+            tmp_layout.addLayout(self.layout_single_animal)
 
-        self.main_layout.addWidget(_create_label_widget("Data Processing", "font:bold"))
-        self.layout_data_processing = _create_horizontal_layout()
-        self._generate_layout_data_processing(self.layout_data_processing)
-        self.main_layout.addLayout(self.layout_data_processing)
+        self.main_layout.addLayout(tmp_layout)
 
-        self.main_layout.addWidget(_create_label_widget("Visualization", "font:bold"))
-        self.layout_visualization = _create_horizontal_layout()
-        self._generate_layout_visualization(self.layout_visualization)
-        self.main_layout.addLayout(self.layout_visualization)
+        self.main_layout.addWidget(_create_label_widget("", "font:bold"))
+        self.layout_other_options = _create_vertical_layout()
+        self._generate_layout_other_options(self.layout_other_options)
+        self.main_layout.addLayout(self.layout_other_options)
 
         self.analyze_videos_btn = QtWidgets.QPushButton("Analyze Videos")
         self.analyze_videos_btn.clicked.connect(self.analyze_videos)
@@ -145,9 +151,24 @@ class AnalyzeVideos(QWidget):
 
         layout.addWidget(self.crop_bodyparts)
 
-    def _generate_layout_visualization(self, layout):
+    def _generate_layout_other_options(self, layout):
 
-        tmp_layout = QtWidgets.QVBoxLayout()
+        tmp_layout = _create_horizontal_layout(margins=(0, 0, 0, 0))
+
+        # Save results as csv
+        self.save_as_csv = QtWidgets.QCheckBox("Save result(s) as csv")
+        self.save_as_csv.setCheckState(Qt.Unchecked)
+        self.save_as_csv.stateChanged.connect(self.update_csv_choice)
+
+        tmp_layout.addWidget(self.save_as_csv)
+
+        # Filter predictions
+        self.filter_predictions = QtWidgets.QCheckBox("Filter predictions")
+        self.filter_predictions.setCheckState(Qt.Unchecked)
+        self.filter_predictions.stateChanged.connect(self.update_filter_choice)
+
+        tmp_layout.addWidget(self.filter_predictions)
+
 
         # Plot Trajectories
         self.plot_trajectories = QtWidgets.QCheckBox("Plot trajectories")
@@ -168,34 +189,17 @@ class AnalyzeVideos(QWidget):
 
         # Bodypart list
         self.bdpt_list_widget = QtWidgets.QListWidget()
-        # self.bdpt_list_widget.setMaximumWidth(400)
+        self.bdpt_list_widget.setMaximumWidth(500)
         self.bdpt_list_widget.addItems(self.all_bodyparts)
         self.bdpt_list_widget.setSelectionMode(
             QtWidgets.QAbstractItemView.MultiSelection
         )
-        self.bdpt_list_widget.selectAll()
+        # self.bdpt_list_widget.selectAll()
         self.bdpt_list_widget.setEnabled(False)
         self.bdpt_list_widget.itemSelectionChanged.connect(
             self.update_selected_bodyparts
         )
-        tmp_layout = QtWidgets.QVBoxLayout()
-        tmp_layout.addWidget(self.bdpt_list_widget)
-        layout.addLayout(tmp_layout)
-
-    def _generate_layout_data_processing(self, layout):
-        # Save results as csv
-        self.save_as_csv = QtWidgets.QCheckBox("Save result(s) as csv")
-        self.save_as_csv.setCheckState(Qt.Unchecked)
-        self.save_as_csv.stateChanged.connect(self.update_csv_choice)
-
-        layout.addWidget(self.save_as_csv)
-
-        # Filter predictions
-        self.filter_predictions = QtWidgets.QCheckBox("Filter predictions")
-        self.filter_predictions.setCheckState(Qt.Unchecked)
-        self.filter_predictions.stateChanged.connect(self.update_filter_choice)
-
-        layout.addWidget(self.filter_predictions)
+        layout.addWidget(self.bdpt_list_widget, Qt.AlignLeft)
 
     def _generate_layout_video_analysis(self, layout):
 
@@ -220,6 +224,10 @@ class AnalyzeVideos(QWidget):
         self.selected_videos_text = QtWidgets.QLabel("")
         layout.addWidget(self.selected_videos_text)
 
+        self.clear_videos = QtWidgets.QPushButton("Clear selection")
+        self.clear_videos.clicked.connect(self.clear_selected_videos)
+        layout.addWidget(self.clear_videos, alignment=Qt.AlignRight)
+
     def _generate_layout_attributes(self, layout):
         # Shuffle
         opt_text = QtWidgets.QLabel("Shuffle")
@@ -228,8 +236,8 @@ class AnalyzeVideos(QWidget):
         self.shuffle.setValue(1)
         self.shuffle.setMinimumHeight(30)
 
-        layout.addWidget(opt_text)
-        layout.addWidget(self.shuffle)
+        layout.addWidget(opt_text, 0, 0)
+        layout.addWidget(self.shuffle, 0, 1)
 
         # Trainingset index
         opt_text = QtWidgets.QLabel("Trainingset index")
@@ -238,19 +246,19 @@ class AnalyzeVideos(QWidget):
         self.trainingset.setValue(0)
         self.trainingset.setMinimumHeight(30)
 
-        layout.addWidget(opt_text)
-        layout.addWidget(self.trainingset)
+        layout.addWidget(opt_text, 1, 0)
+        layout.addWidget(self.trainingset, 1, 1)
 
         # Overwrite analysis files
         self.overwrite_tracks = QtWidgets.QCheckBox("Overwrite tracks")
         self.overwrite_tracks.setCheckState(Qt.Unchecked)
         self.overwrite_tracks.stateChanged.connect(self.update_overwrite_tracks)
 
-        layout.addWidget(self.overwrite_tracks)
+        layout.addWidget(self.overwrite_tracks, 0, 2)
 
     def _generate_layout_multianimal_only_options(self, layout):
-
-        tmp_layout = QtWidgets.QHBoxLayout()
+        
+        tmp_layout = QtWidgets.QGridLayout()
 
         opt_text = QtWidgets.QLabel("Tracking method")
         self.tracker_type_selector = QComboBox()
@@ -258,27 +266,27 @@ class AnalyzeVideos(QWidget):
         self.tracker_type_selector.addItems(["skeleton", "box", "ellipse"])
         self.tracker_type_selector.setCurrentText(self.tracker_method)
         self.tracker_type_selector.currentTextChanged.connect(self.update_tracker_type)
-        tmp_layout.addWidget(opt_text)
-        tmp_layout.addWidget(self.tracker_type_selector)
+        tmp_layout.addWidget(opt_text, 0, 0)
+        tmp_layout.addWidget(self.tracker_type_selector, 0, 1)
 
         opt_text = QtWidgets.QLabel("Number of animals in videos")
         self.num_animals_in_videos = QSpinBox()
         self.num_animals_in_videos.setValue(len(self.cfg.get("individuals", 1)))
         self.num_animals_in_videos.setMaximum(100)
         self.num_animals_in_videos.setMinimumHeight(30)
-        tmp_layout.addWidget(opt_text)
-        tmp_layout.addWidget(self.num_animals_in_videos)
+        tmp_layout.addWidget(opt_text, 1, 0)
+        tmp_layout.addWidget(self.num_animals_in_videos, 1, 1)
 
-        layout.addLayout(tmp_layout)
+        # layout.addLayout(tmp_layout)
 
-        tmp_layout = QtWidgets.QHBoxLayout()
+        # tmp_layout = QtWidgets.QGridLayout()
 
         self.calibrate_assembly_checkbox = QtWidgets.QCheckBox("Calibrate assembly")
         self.calibrate_assembly_checkbox.setCheckState(Qt.Unchecked)
         self.calibrate_assembly_checkbox.stateChanged.connect(
             self.update_calibrate_assembly
         )
-        tmp_layout.addWidget(self.calibrate_assembly_checkbox)
+        tmp_layout.addWidget(self.calibrate_assembly_checkbox, 0, 2)
 
         self.assemble_with_ID_only_checkbox = QtWidgets.QCheckBox(
             "Assemble with ID only"
@@ -287,7 +295,7 @@ class AnalyzeVideos(QWidget):
         self.assemble_with_ID_only_checkbox.stateChanged.connect(
             self.update_assemble_with_ID_only
         )
-        tmp_layout.addWidget(self.assemble_with_ID_only_checkbox)
+        tmp_layout.addWidget(self.assemble_with_ID_only_checkbox, 1, 2)
 
         self.use_transformer_tracking_checkbox = QtWidgets.QCheckBox(
             "Use tranformer tracking"
@@ -296,7 +304,7 @@ class AnalyzeVideos(QWidget):
         self.use_transformer_tracking_checkbox.stateChanged.connect(
             self.update_use_transformer_tracking
         )
-        tmp_layout.addWidget(self.use_transformer_tracking_checkbox)
+        tmp_layout.addWidget(self.use_transformer_tracking_checkbox, 0, 3)
 
         self.create_detections_video_checkbox = QtWidgets.QCheckBox(
             "Create video with all detections"
@@ -305,13 +313,13 @@ class AnalyzeVideos(QWidget):
         self.create_detections_video_checkbox.stateChanged.connect(
             self.update_create_video_detections
         )
-        tmp_layout.addWidget(self.create_detections_video_checkbox)
+        tmp_layout.addWidget(self.create_detections_video_checkbox, 1, 3)
 
         # Use ffprobe
         self.use_robustnframes = QtWidgets.QCheckBox("Robust frame reading")
         self.use_robustnframes.setCheckState(Qt.Unchecked)
         self.use_robustnframes.stateChanged.connect(self.update_robustnframes)
-        tmp_layout.addWidget(self.use_robustnframes)
+        tmp_layout.addWidget(self.use_robustnframes, 0, 4)
 
         layout.addLayout(tmp_layout)
 
@@ -462,6 +470,14 @@ class AnalyzeVideos(QWidget):
             self.select_video_button.adjustSize()
             self.selected_videos_text.adjustSize()
             self.logger.info(f"Videos selected to analyze:\n{self.filelist}")
+
+    def clear_selected_videos(self):
+        self.selected_videos_text.setText("")
+        self.select_video_button.setText("Select videos")
+        self.filelist.clear()
+        self.select_video_button.adjustSize()
+        self.selected_videos_text.adjustSize()
+        self.logger.info(f"Videos selected to analyze:\n{self.filelist}")
 
     def edit_config_file(self):
 
