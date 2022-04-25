a340d13cadd3391c6521dc0a37410693d4f7c146
intergalactic-mammoth <aristotelis.economides@gmail.com>
2022-04-25 19:00:50 +0200

    wip implement refine tracklets tab

--- refine_labels.py
+++ refine_labels.py
@@ -8,6 +8,15 @@ from PySide2.QtGui import QIcon
 from deeplabcut.utils import auxiliaryfunctions
 from pathlib import Path
 
+from components import (
+    DefaultTab,
+    VideoSelectionWidget,
+    _create_grid_layout,
+    _create_horizontal_layout,
+    _create_label_widget,
+)
+from widgets import ConfigEditor
+
 
 def refine_labels(config, multianimal=False):
     """
@@ -49,101 +58,198 @@ def refine_labels(config, multianimal=False):
     os.chdir(startpath)
 
 
-class RefineLabels(QWidget):
-    def __init__(self, parent, cfg):
-        super(RefineLabels, self).__init__(parent)
+class RefineLabels(DefaultTab):
+    # TODO: Add "run tracking" button + function
+    def __init__(self, root, parent, h1_description):
+        super(RefineLabels, self).__init__(root, parent, h1_description)
         # variable initilization
-        self.method = "automatic"
-        self.config = cfg
 
-        self.main_layout = QtWidgets.QVBoxLayout(self)
-        self.main_layout.setAlignment(Qt.AlignTop)
-        self.main_layout.setSpacing(20)
-        self.main_layout.setContentsMargins(0, 20, 40, 20)
-        self.setLayout(self.main_layout)
-
+        # TODO: rename this to private -> _set_page in all tabs
         self.set_page()
 
+    @property
+    def files(self):
+        self.video_selection_widget.files
+
+    @property
+    def inference_cfg_path(self):
+        return os.path.join(
+            self.root.cfg["project_path"],
+            auxiliaryfunctions.get_model_folder(
+                self.root.cfg["TrainingFraction"][0], #NOTE: trainingsetindex hardcoded!
+                int(self.root.shuffle_value),
+                self.root.cfg,
+            ),
+            "test",
+            "inference_cfg.yaml",
+        )
+
     def set_page(self):
-        separatorLine = QtWidgets.QFrame()
-        separatorLine.setFrameShape(QtWidgets.QFrame.HLine)
-        separatorLine.setFrameShadow(QtWidgets.QFrame.Raised)
 
-        separatorLine.setLineWidth(0)
-        separatorLine.setMidLineWidth(1)
+        # TODO: Multi video select.... have to change to single video!
+        self.main_layout.addWidget(_create_label_widget("Video Selection", "font:bold"))
+        self.video_selection_widget = VideoSelectionWidget(self.root, self)
+        self.main_layout.addWidget(self.video_selection_widget)
 
-        l1_step1 = QtWidgets.QLabel("DeepLabCut - Step 9. Refine labels")
-        l1_step1.setContentsMargins(20, 0, 0, 10)
+        self.main_layout.addWidget(_create_label_widget("Attributes", "font:bold"))
+        self.layout_attributes = _create_horizontal_layout()
+        self._generate_layout_attributes(self.layout_attributes)
+        self.main_layout.addLayout(self.layout_attributes)
 
-        self.main_layout.addWidget(l1_step1)
-        self.main_layout.addWidget(separatorLine)
+        self.container_layout = _create_horizontal_layout(margins=(0, 0, 0, 0))
 
-        layout_cfg = QtWidgets.QHBoxLayout()
-        layout_cfg.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        layout_cfg.setSpacing(20)
-        layout_cfg.setContentsMargins(20, 10, 300, 0)
-        cfg_text = QtWidgets.QLabel("Select the config file")
-        cfg_text.setContentsMargins(0, 0, 60, 0)
+        self.layout_refinement_settings = _create_grid_layout(margins=(20, 0, 0, 0))
+        self._generate_layout_refinement(self.layout_refinement_settings)
+        self.container_layout.addLayout(self.layout_refinement_settings)
 
-        self.cfg_line = QtWidgets.QLineEdit()
-        self.cfg_line.setMaximumWidth(800)
-        self.cfg_line.setMinimumWidth(600)
-        self.cfg_line.setMinimumHeight(30)
-        self.cfg_line.setText(self.config)
-        self.cfg_line.textChanged[str].connect(self.update_cfg)
+        self.layout_filtering_settings = _create_grid_layout(margins=(20, 0, 0, 0))
+        self._generate_layout_filtering(self.layout_filtering_settings)
+        self.container_layout.addLayout(self.layout_filtering_settings)
 
-        browse_button = QtWidgets.QPushButton("Browse")
-        browse_button.setMaximumWidth(100)
-        browse_button.clicked.connect(self.browse_dir)
+        self.main_layout.addLayout(self.container_layout)
 
-        layout_cfg.addWidget(cfg_text)
-        layout_cfg.addWidget(self.cfg_line)
-        layout_cfg.addWidget(browse_button)
+        self.edit_inferencecfg_btn = QtWidgets.QPushButton("Edit inference_cfg.yaml")
+        self.edit_inferencecfg_btn.setMinimumWidth(150)
+        self.edit_inferencecfg_btn.clicked.connect(self.open_inferencecfg_editor)
 
-        self.main_layout.addLayout(layout_cfg)
+        self.filter_tracks_button = QtWidgets.QPushButton("Filter tracks ( + .csv)")
+        self.filter_tracks_button.setMinimumWidth(150)
+        self.filter_tracks_button.clicked.connect(self.filter_tracks)
 
-        self.launch_button = QtWidgets.QPushButton("LAUNCH")
-        self.launch_button.setContentsMargins(0, 140, 100, 40)
+        self.launch_button = QtWidgets.QPushButton("Launch refinement GUI")
+        self.launch_button.setMinimumWidth(150)
         self.launch_button.clicked.connect(self.refine_labels)
 
         self.merge_button = QtWidgets.QPushButton("Merge dataset")
+        self.merge_button.setMinimumWidth(150)
         self.merge_button.clicked.connect(self.merge_dataset)
         self.merge_button.setEnabled(False)
 
-        self.main_layout.addWidget(self.merge_button, alignment=Qt.AlignRight)
+        self.main_layout.addWidget(self.edit_inferencecfg_btn, alignment=Qt.AlignRight)
         self.main_layout.addWidget(self.launch_button, alignment=Qt.AlignRight)
+        self.main_layout.addWidget(self.filter_tracks_button, alignment=Qt.AlignRight)
+        self.main_layout.addWidget(self.merge_button, alignment=Qt.AlignRight)
 
-    def update_cfg(self):
-        text = self.cfg_line.text()
-        self.config = text
-        self.cfg = auxiliaryfunctions.read_config(self.config)
+    def _generate_layout_attributes(self, layout):
+        # Shuffle
+        shuffle_text = QtWidgets.QLabel("Shuffle")
+        self.shuffle = QtWidgets.QSpinBox()
+        self.shuffle.setMaximum(100)
+        self.shuffle.setValue(self.root.shuffle_value)
+        self.shuffle.valueChanged.connect(self.root.update_shuffle)
 
+        # Num animals
+        num_animals_text = QtWidgets.QLabel("Number of animals in video")
+        self.num_animals_in_videos = QtWidgets.QSpinBox()
+        self.num_animals_in_videos.setValue(len(self.root.all_individuals))
+        self.num_animals_in_videos.setMaximum(100)
+        self.num_animals_in_videos.valueChanged.connect(self.log_num_animals)
 
-    def browse_dir(self):
-        cwd = self.config
-        config = QtWidgets.QFileDialog.getOpenFileName(
-            self, "Select a configuration file", cwd, "Config files (*.yaml)"
+        layout.addWidget(shuffle_text)
+        layout.addWidget(self.shuffle)
+        layout.addWidget(num_animals_text)
+        layout.addWidget(self.num_animals_in_videos)
+        
+    def _generate_layout_refinement(self, layout):
+
+        section_title = _create_label_widget(
+            "Refinement Settings", "font:bold", (0, 50, 0, 0)
         )
-        if not config[0]:
-            return
-        self.config = config[0]
-        self.cfg_line.setText(self.config)
+
+        # Min swap length
+        swap_length_label = QtWidgets.QLabel("Min swap length to highlight")
+        self.swap_length_widget = QtWidgets.QSpinBox()
+        self.swap_length_widget.setValue(2)
+        self.swap_length_widget.setMinimumWidth(150)
+        self.swap_length_widget.valueChanged.connect(self.log_swap_length)
+
+        # Max gap to fill
+        max_gap_label = QtWidgets.QLabel("Max gap of missing data to fill")
+        self.max_gap_widget = QtWidgets.QSpinBox()
+        self.max_gap_widget.setValue(5)
+        self.max_gap_widget.setMinimumWidth(150)
+        self.max_gap_widget.valueChanged.connect(self.log_max_gap)
+
+        # Trail length
+        trail_length_label = QtWidgets.QLabel("Visualization trail length")
+        self.trail_length_widget = QtWidgets.QSpinBox()
+        self.trail_length_widget.setValue(20)
+        self.trail_length_widget.setMinimumWidth(150)
+        self.trail_length_widget.valueChanged.connect(self.log_trail_length)
+
+        layout.addWidget(section_title, 0, 0)
+        layout.addWidget(swap_length_label, 1, 0)
+        layout.addWidget(self.swap_length_widget, 1, 1)
+        layout.addWidget(max_gap_label, 2, 0)
+        layout.addWidget(self.max_gap_widget, 2, 1)
+        layout.addWidget(trail_length_label, 3, 0)
+        layout.addWidget(self.trail_length_widget, 3, 1)
+
+    def _generate_layout_filtering(self, layout):
+
+        section_title = _create_label_widget("Filtering", "font:bold", (0, 50, 0, 0))
+
+        # Filter type
+        filter_label = QtWidgets.QLabel("Filter type")
+        self.filter_type_widget = QtWidgets.QComboBox()
+        self.filter_type_widget.setMinimumWidth(150)
+        options = ["median"]
+        self.filter_type_widget.addItems(options)
+        self.filter_type_widget.currentTextChanged.connect(self.log_filter_type)
+
+        # Filter window length
+        window_length_label = QtWidgets.QLabel("Window length")
+        self.window_length_widget = QtWidgets.QSpinBox()
+        self.window_length_widget.setValue(5)
+        self.window_length_widget.setMinimumWidth(150)
+        self.window_length_widget.valueChanged.connect(self.log_window_length)
+
+        layout.addWidget(section_title, 0, 0)
+        layout.addWidget(filter_label, 1, 0)
+        layout.addWidget(self.filter_type_widget, 1, 1)
+        layout.addWidget(window_length_label, 2, 0)
+        layout.addWidget(self.window_length_widget, 2, 1)
+
+    def log_swap_length(self, value):
+        self.root.logger.info(f"Swap length set to {value}")
+
+    def log_max_gap(self, value):
+        self.root.logger.info(f"Max gap size of missing data to fill set to {value}")
+
+    def log_trail_length(self, value):
+        self.root.logger.info(f"Visualization trail length set to {value}")
+
+    def log_filter_type(self, filter_type):
+        self.root.logger.info(f"Filter type set to {filter_type.upper()}")
+
+    def log_window_length(self, window_length):
+        self.root.logger.info(f"Window length set to {window_length}")
+
+    def log_num_animals(self, num_animals):
+        self.root.logger.info(f"Number of animals in video set to {num_animals}")
+
+    def open_inferencecfg_editor(self):
+        editor = ConfigEditor(self.inference_cfg_path)
+        editor.show()
+
+    def filter_tracks(self):
+        #TODO: 
+        raise NotImplementedError
 
     def merge_dataset(self):
+        # TODO: make sure this is correct
+
         msg = QtWidgets.QMessageBox()
         msg.setIcon(QtWidgets.QMessageBox.Warning)
         msg.setText(
-            "1. Make sure that you have refined all the labels before merging the dataset.\n\n2. If you merge the dataset, you need to re-create the training dataset before you start the training.\n\n3. Are you ready to merge the dataset?"
+            "Make sure that you have refined all the labels before merging the dataset.If you merge the dataset, you need to re-create the training dataset before you start the training. Are you ready to merge the dataset?"
         )
         msg.setWindowTitle("Warning")
-        msg.setMinimumWidth(1000)
-        self.logo_dir = os.path.dirname(os.path.realpath("logo.png")) + os.path.sep
-        self.logo = self.logo_dir + "/assets/logo.png"
-        msg.setWindowIcon(QIcon(self.logo))
+        msg.setWindowIcon(QtWidgets.QMessageBox.Warning)
         msg.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
         result = msg.exec_()
         if result == QMessageBox.Yes:
-            print("Yes")
+            pass
             # TODO: finish -->
             # notebook = self.GetParent()
             # notebook.SetSelection(4)
