9e7c50d508b69441c4ec7059175a4a12da2966eb
intergalactic-mammoth <aristotelis.economides@gmail.com>
2022-04-29 00:46:36 +0200

    global params file

---
+++ dlc_params.py
@@ -0,0 +1,23 @@
+class DLC_Params:
+    VIDEOTYPES = [
+        "avi",
+        "mp4",
+        "mov",
+    ]
+
+    NNETS = [
+        "dlcrnet_ms5",
+        "resnet_50",
+        "resnet_101",
+        "resnet_152",
+        "mobilenet_v2_1.0",
+        "mobilenet_v2_0.75",
+        "mobilenet_v2_0.5",
+        "mobilenet_v2_0.35",
+        "efficientnet-b0",
+        "efficientnet-b3",
+        "efficientnet-b6",
+    ]
+
+    IMAGE_AUGMENTERS = ["default", "tensorpack", "imgaug"]
+
