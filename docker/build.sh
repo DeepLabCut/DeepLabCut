a8cb1a3194a13e3f20c5afd3f466e2aa38248eeb
Steffen Schneider <steffen@bethgelab.org>
2022-08-04 17:33:29 +0200

    Add build matrix and build various images

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,6 @@
 # > docker/build.sh [build|test|push]
 
 set -e
-set -x
 
 DOCKER=${DOCKER:-'docker'}
 DOCKER_BUILD="$DOCKER build"
@@ -27,7 +26,11 @@ EOF
 }
 
 list_images() {
-    $DOCKER images | grep '^deeplabcut ' | sed -s 's/\s\s\+/\t/g' | cut -f1,2 -d$'\t' --output-delimiter ':' | grep core
+    $DOCKER images \
+    | grep '^deeplabcut ' \
+    | sed -s 's/\s\s\+/\t/g' \
+    | cut -f1,2 -d$'\t' --output-delimiter ':' \
+    | grep core
 }
 
 run_test() {
@@ -37,7 +40,7 @@ run_test() {
     kwargs=(
         -u $(id -u) --tmpfs /.local --tmpfs /.cache -w /app
         --env DLClight=True
-        -it deeplabcut:tmp 
+        -it deeplabcut:tmp
     )
 
     # Unit tests
@@ -51,17 +54,41 @@ run_test() {
      python3 testscript_cli.py
 }
 
+iterate_build_matrix() {
+    ## TODO(stes): Consider adding legacy versions for CUDA
+    ## if there is demand from users:
+    # 10.2-runtime-ubuntu18.04 \
+    # 11.1.1-runtime-ubuntu18.04 \
+    for cuda_version in \
+        11.4.0-runtime-ubuntu20.04 \
+        11.7.0-runtime-ubuntu20.04
+    do
+        for deeplabcut_version in \
+            2.2.0.2 \
+            2.2.1.1
+        do
+            for stage in base core gui jupyter; do
+                tag=${deeplabcut_version}-${stage}-cuda${cuda_version}
+                echo \
+                     --build-arg=CUDA_VERSION=${cuda_version} \
+                     --build-arg=DEEPLABCUT_VERSION=${deeplabcut_version} \
+                     "--tag=${BASENAME}:$tag" \
+                     -f "Dockerfile.${stage}" \.
+            done
+        done
+    done
+}
+
 for arg in "$@"; do
 case $1 in
     build)
-	cp -r examples ${DOCKERDIR}
-        for tag in base latest-core latest-gui latest-gui-jupyter; do
-            (
-            cd ${DOCKERDIR};
-            ${DOCKER_BUILD} -t ${BASENAME}:${tag} -f Dockerfile.${tag} .
-            )
-            echo $tag
-        done
+        cp -r examples ${DOCKERDIR}
+        (
+          cd ${DOCKERDIR}
+          iterate_build_matrix \
+          | tr '\n' '\0' \
+          | xargs -I@ -0 bash -c "echo Building @; $DOCKER build -q @"
+        )
     ;;
     test)
         for tag in latest-core; do
