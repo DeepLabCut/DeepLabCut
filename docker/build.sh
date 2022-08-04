16dd3489db047e0952122c80b57e4905f67fc2ff
Steffen Schneider <steffen@bethgelab.org>
2022-08-04 18:26:24 +0200

    Adapt dockerfiles

--- docker/build.sh
+++ docker/build.sh
@@ -3,11 +3,12 @@
 # > docker/build.sh [build|test|push]
 
 set -e
+set -x
 
-DOCKER=${DOCKER:-'docker'}
-DOCKER_BUILD="$DOCKER build"
-BASENAME=deeplabcut/deeplabcut
-DOCKERDIR=docker
+export DOCKER=${DOCKER:-'docker'}
+export DOCKER_BUILD="$DOCKER build"
+export BASENAME=deeplabcut/deeplabcut
+export DOCKERDIR=docker
 
 if [[ ! -d ./${DOCKERDIR} ]]; then
     >&2 echo Run from repository root. Current pwd is
@@ -34,17 +35,20 @@ list_images() {
 }
 
 run_test() {
+    echo $@
     test_image_id="$1"
     test_image_id=$(build_test_image $test_image_id)
 
     kwargs=(
         -u $(id -u) --tmpfs /.local --tmpfs /.cache -w /app
         --env DLClight=True
-        -it deeplabcut:tmp
+        deeplabcut:tmp
     )
 
     # Unit tests
-    $DOCKER run -v $(pwd)/tests:/app ${kwargs[@]} python3 -m pytest -q .
+    $DOCKER run -v $(pwd)/tests:/app ${kwargs[@]} python3 -m pytest -q . || return 1
+
+    return 0
 
     # Functional tests
     $DOCKER run \
@@ -53,27 +57,37 @@ run_test() {
      ${kwargs[@]} \
      python3 testscript_cli.py
 }
+export -f build_test_image
+export -f run_test
 
 iterate_build_matrix() {
     ## TODO(stes): Consider adding legacy versions for CUDA
     ## if there is demand from users:
     # 10.2-runtime-ubuntu18.04 \
     # 11.1.1-runtime-ubuntu18.04 \
+    mode=${1:-build}
     for cuda_version in \
         11.4.0-runtime-ubuntu20.04 \
         11.7.0-runtime-ubuntu20.04
     do
+            #2.2.0.2 \
         for deeplabcut_version in \
-            2.2.0.2 \
             2.2.1.1
         do
             for stage in base core gui jupyter; do
                 tag=${deeplabcut_version}-${stage}-cuda${cuda_version}
-                echo \
-                     --build-arg=CUDA_VERSION=${cuda_version} \
-                     --build-arg=DEEPLABCUT_VERSION=${deeplabcut_version} \
-                     "--tag=${BASENAME}:$tag" \
-                     -f "Dockerfile.${stage}" \.
+                case "$mode" in
+                    build)
+                    echo \
+                         --build-arg=CUDA_VERSION=${cuda_version} \
+                         --build-arg=DEEPLABCUT_VERSION=${deeplabcut_version} \
+                         "--tag=${BASENAME}:$tag" \
+                         -f "Dockerfile.${stage}" \.
+                    ;;
+                    *)
+                    echo ${BASENAME}:$tag
+                    ;;
+                esac
             done
         done
     done
@@ -87,13 +101,14 @@ case $1 in
           cd ${DOCKERDIR}
           iterate_build_matrix \
           | tr '\n' '\0' \
-          | xargs -I@ -0 bash -c "echo Building @; $DOCKER build -q @"
+          | xargs -I@ -0 bash -c "echo Building @; $DOCKER build @ || exit 255"
         )
     ;;
     test)
-        for tag in latest-core; do
-            run_test ${BASENAME}:${tag}
-        done
+        iterate_build_matrix test \
+        | grep '\-core\-' \
+        | tr '\n' '\0' \
+        | xargs -0 -I@ bash -c "run_test @ || exit 255"
     ;;
     push)
         for tag in base latest-core latest-gui latest-gui-jupyter; do
