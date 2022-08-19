5dbcc45f3ecb1197bfb71cb9a5766eb03d0f5421
yueoka <yueoka@users.noreply.github.com>
2022-08-19 20:40:05 +0900

    add validation for fps when fps*1000 overflow 65535(max of 16bit) (#1952)
    * add validation for fps when fps*1000 overflow 65535(max of 16bit)
    
    * add float validation for fps

--- deeplabcut/utils/make_labeled_video.py
+++ deeplabcut/utils/make_labeled_video.py
@@ -90,6 +90,9 @@ def CreateVideo(
         ny, nx = clip.height(), clip.width()
 
     fps = clip.fps()
+    if isinstance(fps, float):
+        if fps*1000 > 65535:
+            fps = round(fps)
     nframes = clip.nframes
     duration = nframes / fps
 
