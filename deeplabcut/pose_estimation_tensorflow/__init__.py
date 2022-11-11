162505738a23804a545de2218f324af220e4cbd1
shaokaiye <shaokaiyeah@gmail.com>
2022-11-11 10:50:32 +0100

    Fixed circular import

--- deeplabcut/pose_estimation_tensorflow/__init__.py
+++ deeplabcut/pose_estimation_tensorflow/__init__.py
@@ -21,4 +21,4 @@ from deeplabcut.pose_estimation_tensorflow.predict_videos import *
 from deeplabcut.pose_estimation_tensorflow.training import *
 from deeplabcut.pose_estimation_tensorflow.util import *
 from deeplabcut.pose_estimation_tensorflow.visualizemaps import *
-from deeplabcut.pose_estimation_tensorflow.predict_supermodel import video_inference_superanimal
+from deeplabcut.pose_estimation_tensorflow.predict_supermodel import *
