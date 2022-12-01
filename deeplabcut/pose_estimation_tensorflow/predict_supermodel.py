c42e2e0b8c0189509b69383c7fa03f2e82a4d5bb
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2022-12-01 15:10:20 +0100

    Cleanup and removal of bbox file

--- deeplabcut/pose_estimation_tensorflow/predict_supermodel.py
+++ deeplabcut/pose_estimation_tensorflow/predict_supermodel.py
@@ -1,11 +1,8 @@
-import argparse
 import os
 import os.path
 import pickle
-import re
 import time
 from pathlib import Path
-import yaml
 import cv2
 import imgaug.augmenters as iaa
 import numpy as np
@@ -13,167 +10,14 @@ import pandas as pd
 from skimage.util import img_as_ubyte
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
-import json
 from deeplabcut.pose_estimation_tensorflow.config import load_config
 from deeplabcut.pose_estimation_tensorflow.core import \
     predict as single_predict
 from deeplabcut.pose_estimation_tensorflow.core import \
     predict_multianimal as predict
-from deeplabcut.pose_estimation_tensorflow.lib import inferenceutils
-from deeplabcut.pose_estimation_tensorflow.lib import trackingutils
-from deeplabcut.utils import auxfun_multianimal
 from deeplabcut.utils import auxiliaryfunctions
 from deeplabcut.utils.auxfun_videos import VideoWriter
 import deeplabcut
-
-
-def extract_bbox_from_file(filename):
-
-    with open(filename, 'rb') as f:
-        json_obj = json.load(f)
-    bboxs = json_obj['bbox']
-
-    return bboxs
-
-
-def _topdown_reverse_transformation(preds, bbox, num_kpts):
-    ret = {}
-        
-    ret['coordinates'] = [[[]]* num_kpts]
-    ret['confidence'] = [[]] * num_kpts
-
-    for instance_id, pred in enumerate(preds):
-
-        if len(pred) == 0:
-            coordinate =  [[]] * num_kpts
-            confidence = [[]] * num_kpts
-        else:
-            coordinate = pred['coordinates'][0]
-            confidence = pred['confidence']        
-        
-        for kpt_id, coord_list in enumerate(coordinate):            
-            
-            if len(ret['coordinates'][0][kpt_id]) == 0:
-                ret['coordinates'][0][kpt_id] = [[]] * len(preds)
-                ret['confidence'][kpt_id] = [[]]* len(preds)
-                
-            if len(coord_list) == 0:                
-                ret['coordinates'][0][kpt_id][instance_id] = np.array([np.nan, np.nan])
-                ret['confidence'][kpt_id][instance_id] = np.array([np.nan])
-                continue
-            confidence_list = confidence[kpt_id]
-            max_idx = np.argmax(confidence_list)
-            # shifting the prediction
-            x1,y1 = bbox[instance_id][:2]
-            
-            max_pred = coordinate[kpt_id][max_idx] + np.array([x1, y1])
-
-            # only keep the max        
-            confidence[kpt_id] = confidence_list[max_idx]
-            coordinate[kpt_id] = max_pred
-
-        
-            ret['coordinates'][0][kpt_id][instance_id] = coordinate[kpt_id]
-            ret['confidence'][kpt_id][instance_id] = confidence[kpt_id]
-            
-    return ret
-
-
-
-def video_inference_topdown(
-        test_cfg,
-        sess,
-        inputs,
-        outputs,
-        cap,
-        nframes,
-        batchsize,
-        bboxs,
-        invert_color = False,
-
-):
-
-    strwidth = int(np.ceil(np.log10(nframes)))  # width for strings
-    batch_ind = 0  # keeps track of which image within a batch should be written to
-
-    nx, ny = cap.dimensions
-
-    pbar = tqdm(total=nframes)
-    counter = 0
-    inds = []
-    PredicteData = {}
-    # len(frames) -> (n_scale,)
-    # frames[0].shape - > (batchsize, h, w, 3)
-    num_kpts = len(test_cfg['all_joints_names'])
-    
-    while cap.video.isOpened():
-        # no crop needed
-        _frame = cap.read_frame()
-        if _frame is not None:
-            frame = img_as_ubyte(_frame)
-            h,w,_ = frame.shape 
-
-            bbox = bboxs[counter]
-            preds = []
-            _bbox = []
-            black_mean = None
-            for i in range(len(bbox)):
-
-                x1, y1, x2, y2 = bbox[i]
-
-                if x1<= 1.0 and x2<= 1.0 and y1<=1.0 and y2<=1.0:
-                    # normalized bounding box
-                    y1,y2 = int(h*y1), int(h*y2)
-                    x1,x2 = int(w*x1), int(w*x2)
-                    
-                _bbox.append([x1,y1,x2,y2])
-                    
-                cropped_frame = frame[y1:y2, x1:x2]
-                # hardcoded rule for cal database
-                if i == 1:
-                    cropped_frame = 255 - cropped_frame 
-                cropped_frame = np.array(cropped_frame)
-                cropped_frame = np.expand_dims(cropped_frame, axis = 0)
-                    # batch full, start true inferencing
-                D = predict.predict_batched_peaks_and_costs(
-                    test_cfg, cropped_frame, sess, inputs, outputs
-                )
-
-                if len(D) != 0:
-                    # stripping the batch dimension
-                    preds.append(D[0])
-                else:
-                    preds.append([])
-                    # only do this when animal is detected                                
-            preds = _topdown_reverse_transformation(preds, _bbox, num_kpts)
-                        
-            PredicteData["frame" + str(counter).zfill(strwidth)] = preds 
-        if counter>= nframes:
-            break
-        counter+=1
-        pbar.update(1)
-                                    
-    cap.close()
-    pbar.close()
-
-            
-    PredicteData["metadata"] = {
-        "nms radius": test_cfg.get("nmsradius", None),
-        "minimal confidence": test_cfg.get("minconfidence", None),
-        "sigma": test_cfg.get("sigma", 1),
-        "PAFgraph": test_cfg.get("partaffinityfield_graph",None),
-        "PAFinds": test_cfg.get(
-            "paf_best", np.arange(len(test_cfg["partaffinityfield_graph"]))
-        ),
-        "all_joints": [[i] for i in range(len(test_cfg["all_joints"]))],
-        "all_joints_names": [
-            test_cfg["all_joints_names"][i] for i in range(len(test_cfg["all_joints"]))
-        ],
-        "nframes": nframes,
-    }
-       
-    return PredicteData, nframes
-    
 
 
 # instead of having these in a lengthy function, I made this a separate function
@@ -181,7 +25,6 @@ def get_nuances(
     videos,
     test_cfg,
     superanimal_name,
-    bbox_file = '',
     videotype="avi",
     destfolder=None,
     batchsize=None,
@@ -191,18 +34,14 @@ def get_nuances(
     allow_growth=False,
     init_weights="",
     save_frames=False,
-    
+
 ):
 
     # Update number of output and batchsize
-    test_cfg["num_outputs"] = 1    
+    test_cfg["num_outputs"] = 1
 
     test_cfg['batch_size'] = batchsize
-    
-    if bbox_file !='':
-        # only supporting single batch topdown inference
-        test_cfg['batch_size'] = 1                        
-    
+
     if test_cfg["num_outputs"] > 1:
         if TFGPUinference:
             print(
@@ -333,7 +172,7 @@ def _average_multiple_scale_preds(preds, scale_list, num_kpts,  cos_dist_thresho
 
         for idx, ele in enumerate(ret_pred["coordinates"][0][kpt_id]):
             if dist[idx] < cos_dist_threshold or ret_pred["confidence"][kpt_id][idx] < confidence_threshold:
-                
+
                 filter_indices.append(idx)
 
         for idx, ele in enumerate(ret_pred["coordinates"][0][kpt_id]):
@@ -342,7 +181,7 @@ def _average_multiple_scale_preds(preds, scale_list, num_kpts,  cos_dist_thresho
                 ret_pred["confidence"][kpt_id][idx] = np.array([[np.nan]])
 
         if len(ret_pred['coordinates'][0][kpt_id]) != 0:
-                    
+
             ret_pred["coordinates"][0][kpt_id] = np.concatenate(
                 ret_pred["coordinates"][0][kpt_id], axis=0
             )
@@ -357,7 +196,7 @@ def _average_multiple_scale_preds(preds, scale_list, num_kpts,  cos_dist_thresho
             ret_pred["confidence"][kpt_id] = np.array(
                 [np.nanmedian(np.array(ret_pred["confidence"][kpt_id]), axis=0)]
             )
-        else:            
+        else:
             ret_pred["coordinates"][0][kpt_id] = np.array([[np.nan, np.nan]])
             ret_pred["confidence"][kpt_id] = np.array([[np.nan]])
     return ret_pred
@@ -389,9 +228,9 @@ def video_inference(
     # frames[0].shape - > (batchsize, h, w, 3)
     multi_scale_batched_frames = None
     frame_shapes = None
-    
+
     num_kpts = len(test_cfg['all_joints_names'])
-    
+
     while cap.video.isOpened():
         # no crop needed
         _frame = cap.read_frame()
@@ -521,9 +360,7 @@ def video_inference_superanimal(
     allow_growth=False,
     init_weights="",
     save_frames=False,
-    bbox_file = '',
-    customized_test_config = ''
-    
+    customized_test_config = '',
 ):
     """
     Makes prediction based on a super animal model. Note right now we only support single animal video inference
@@ -573,11 +410,8 @@ def video_inference_superanimal(
     init_weights: string, default empty
         The path to the tensorflow checkpoint of the super model. Make sure you don't include the suffix of the snapshot file.
 
-    bbox_file: string, default empty
-        The path to a json file that contains bounding box
-
     Examples
-    -------- 
+    --------
 
     Given a list of scales for spatial pyramid, i.e. [600, 700]
 
@@ -591,12 +425,11 @@ def video_inference_superanimal(
                                       videotype=videotype,
                                       init_weights = init_weights,
                                       scale_list = scale_list,
-                                      invert_color = False,   
-                                      bbox_file = bbox_file)
-    
+                                      invert_color = False)
+
     Note after calling video_inference, a config.yaml is created in your working directory
 
-    config_path = os.path.join(os.getcwd(), 'config.yaml') 
+    config_path = os.path.join(os.getcwd(), 'config.yaml')
 
     >>> deeplabcut.create_labeled_video(config_path,
                                 [video_path],
@@ -607,7 +440,7 @@ def video_inference_superanimal(
 
     """
 
-    
+
     dlc_root_path = os.sep.join(deeplabcut.__file__.split(os.sep)[:-1])
 
     name_dict = {'supertopview': 'supertopview.yaml',
@@ -625,10 +458,10 @@ def video_inference_superanimal(
     test_cfg["partaffinityfield_graph"] = []
     test_cfg["partaffinityfield_predict"] = False
 
-    
+
     if init_weights!='':
         test_cfg['init_weights'] = init_weights
-    
+
     setting = get_nuances(
         videos,
         test_cfg,
@@ -642,13 +475,12 @@ def video_inference_superanimal(
         allow_growth=allow_growth,
         init_weights=init_weights,
         save_frames=save_frames,
-        bbox_file = bbox_file
     )
 
     #test_cfg = setting["test_cfg"]
     # get dlc root path
 
-    
+
     videos = setting["videos"]
     destfolder = setting["destfolder"]
     DLCscorer = setting["DLCscorer"]
@@ -670,10 +502,6 @@ def video_inference_superanimal(
             )
             scale_list = [h]
 
-        if bbox_file!='':
-            bboxs = extract_bbox_from_file(bbox_file)        
-            
-        
         videofolder = str(Path(video).parents[0])
         if destfolder is None:
             destfolder = videofolder
@@ -733,33 +561,17 @@ def video_inference_superanimal(
 
             # extra data
             print("before inference")
-            if bbox_file == '':
-                PredicteData, nframes = video_inference(
-                    test_cfg,
-                    sess,
-                    inputs,
-                    outputs,
-                    vid,
-                    nframes,
-                    int(test_cfg["batch_size"]),
-                    invert_color=invert_color,
-                    scale_list=scale_list,
-                )
-
-            else:
-                PredicteData, nframes = video_inference_topdown(
-                    test_cfg,
-                    sess,
-                    inputs,
-                    outputs,
-                    vid,
-                    nframes,
-                    int(test_cfg["batch_size"]),
-                    bboxs,                    
-                    invert_color = invert_color,
-
-                )
-                
+            PredicteData, nframes = video_inference(
+                test_cfg,
+                sess,
+                inputs,
+                outputs,
+                vid,
+                nframes,
+                int(test_cfg["batch_size"]),
+                invert_color=invert_color,
+                scale_list=scale_list,
+            )
 
             stop = time.time()
 
@@ -795,18 +607,14 @@ def video_inference_superanimal(
 
             keypoint_names = test_cfg["all_joints_names"]
 
-            if bbox_file !='':
-                num_individuals = len(bboxs[0])
-                individuals = [f'individual{id}' for id in range(num_individuals)]
-            
-            product = [ [scorer], keypoint_names, xyz_labs] if bbox_file == "" else [ [scorer], individuals, keypoint_names, xyz_labs]
-            names = ['scorer', 'bodyparts', 'coords'] if bbox_file == "" else ['scorer', 'individuals', 'bodyparts', 'coords']
-            
-            columnindex = pd.MultiIndex.from_product(product,names = names)                                                                  
+            product = [[scorer], keypoint_names, xyz_labs]
+            names = ['scorer', 'bodyparts', 'coords']
+
+            columnindex = pd.MultiIndex.from_product(product,names = names)
             imagenames = [k for k in  PredicteData.keys() if k !='metadata']
 
             data = np.zeros((len(imagenames), len(columnindex))) * np.nan
-                                    
+
             df = pd.DataFrame(data, columns=columnindex, index=imagenames)
 
 
@@ -823,20 +631,8 @@ def video_inference_superanimal(
                 for kpt_id, kpt_name in enumerate(keypoint_names):
 
                     confidence = PredicteData[imagename]["confidence"]
-
-                    if bbox_file == '':
-                    
-                        df.loc[imagename][scorer,  kpt_name, 'x'] = keypoints[kpt_id][0][0]
-                        df.loc[imagename][scorer,  kpt_name, 'y'] = keypoints[kpt_id][0][1]
-                        df.loc[imagename][scorer,  kpt_name, 'likelihood'] = confidence[kpt_id]
-
-                    else:
-                        for individual_id in range(len(individuals)):
-                            
-                            
-                            df.loc[imagename][scorer, f'individual{individual_id}',  kpt_name, 'x'] = keypoints[kpt_id][individual_id][0]
-                            df.loc[imagename][scorer, f'individual{individual_id}', kpt_name, 'y' ] = keypoints[kpt_id][individual_id][1]
-                            df.loc[imagename][scorer, f'individual{individual_id}',  kpt_name, 'likelihood'] = confidence[kpt_id][individual_id]
-                    
+                    df.loc[imagename][scorer,  kpt_name, 'x'] = keypoints[kpt_id][0][0]
+                    df.loc[imagename][scorer,  kpt_name, 'y'] = keypoints[kpt_id][0][1]
+                    df.loc[imagename][scorer,  kpt_name, 'likelihood'] = confidence[kpt_id]
 
             df.to_hdf(dataname, "df_with_missing", format="table", mode="w")
