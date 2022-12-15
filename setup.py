8de3260ae923d7fe88e2f041ef84127c99729f30
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2022-12-15 08:47:36 +0100

    Add dlclibrary to setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/DeepLabCut/DeepLabCut",
     install_requires=[
+        "dlclibrary",
         "filterpy>=1.4.4",
         "ruamel.yaml>=0.15.0",
         "imgaug>=0.4.0",
