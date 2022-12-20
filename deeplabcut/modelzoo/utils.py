ddcd6e5d817646cb6f1edaa460fa72045d631e8a
shaokaiye <shaokaiyeah@gmail.com>
2022-12-20 21:08:10 +0000

    WIP fixing previous commit

--- deeplabcut/modelzoo/utils.py
+++ deeplabcut/modelzoo/utils.py
@@ -2,6 +2,7 @@ import json
 import os
 
 def parse_available_supermodels():
+    import deeplabcut
     dlc_path = deeplabcut.utils.auxiliaryfunctions.get_deeplabcut_path()
     json_path = os.path.join(
         dlc_path, "modelzoo", "models.json"
