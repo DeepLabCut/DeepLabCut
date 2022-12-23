66947197b16c132179b1dd0ddb81218a7e1fe463
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2022-12-23 19:26:08 +0100

    Backwards comp

--- deeplabcut/create_project/modelzoo.py
+++ deeplabcut/create_project/modelzoo.py
@@ -21,6 +21,8 @@ from dlclibrary.dlcmodelzoo.modelzoo_download import (
     MODELOPTIONS,
 )
 
+Modeloptions = MODELOPTIONS  # backwards compatability for COLAB NOTEBOOK
+
 
 def MakeTrain_pose_yaml(itemstochange, saveasconfigfile, defaultconfigfile):
     raw = open(defaultconfigfile).read()
