61d794294426b95a5ab83d36bb29e740b8f0d956
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2023-01-20 09:03:01 +0100

    Fix multiprocessing RuntimeError

--- examples/testscript_superanimal_adaptation.py
+++ examples/testscript_superanimal_adaptation.py
@@ -1,47 +1,39 @@
 """
 Test script for super animal adaptation
 """
-import os, subprocess, deeplabcut
-from pathlib import Path
-import pandas as pd
-import numpy as np
-
-basepath = os.path.dirname(os.path.realpath(__file__))
-videoname = "m3v1mp4"
-video =  os.path.join(
-    basepath, "openfield-Pranav-2018-10-30", "videos", videoname + ".mp4"
-)
-
-print ('adaptation training for superanimal_topviewmouse')
+import deeplabcut
+import os
 
 
-superanimal_name = 'superanimal_topviewmouse'
+if __name__ == "__main__":
+    basepath = os.path.dirname(os.path.realpath(__file__))
+    videoname = "m3v1mp4"
+    video = os.path.join(
+        basepath, "openfield-Pranav-2018-10-30", "videos", videoname + ".mp4"
+    )
 
-videotype = '.mp4'
+    print("adaptation training for superanimal_topviewmouse")
 
-scale_list = [200, 300, 400]
+    superanimal_name = "superanimal_topviewmouse"
+    videotype = ".mp4"
+    scale_list = [200, 300, 400]
+    deeplabcut.video_inference_superanimal(
+        [video],
+        superanimal_name,
+        videotype=".mp4",
+        video_adapt=True,
+        scale_list=scale_list,
+        pcutoff=0.1,
+    )
 
-deeplabcut.video_inference_superanimal(
-    [video],
-    superanimal_name,
-    videotype = '.mp4',
-    video_adapt = True,
-    scale_list = scale_list,
-    pcutoff = 0.1
-)
+    print("adaptation training for superanimal_quadruped")
 
-
-print ('adaptation training for superanimal_quadruped')
-
-superanimal_name = 'superanimal_quadruped'
-
-
-deeplabcut.video_inference_superanimal(
-    [video],
-    superanimal_name,
-    videotype = '.mp4',
-    video_adapt = True,
-    scale_list = scale_list,
-    pcutoff = 0.3
-)
-
+    superanimal_name = "superanimal_quadruped"
+    deeplabcut.video_inference_superanimal(
+        [video],
+        superanimal_name,
+        videotype=".mp4",
+        video_adapt=True,
+        scale_list=scale_list,
+        pcutoff=0.3,
+    )
--- examples/testscript_superanimal_inference.py
+++ examples/testscript_superanimal_inference.py
@@ -2,35 +2,34 @@
 Testscript for super animal inference
 
 """
-import os, subprocess, deeplabcut
-from pathlib import Path
-import pandas as pd
-import numpy as np
+import deeplabcut
+import os
 
-basepath = os.path.dirname(os.path.realpath(__file__))
-videoname = "reachingvideo1"
-video = [
-    os.path.join(
-        basepath, "Reaching-Mackenzie-2018-08-30", "videos", videoname + ".avi"
+
+if __name__ == "__main__":
+    basepath = os.path.dirname(os.path.realpath(__file__))
+    videoname = "reachingvideo1"
+    video = [
+        os.path.join(
+            basepath, "Reaching-Mackenzie-2018-08-30", "videos", videoname + ".avi"
+        )
+    ]
+
+    print("testing superanimal_topviewmouse")
+    superanimal_name = "superanimal_topviewmouse"
+    scale_list = [200, 300, 400]
+    deeplabcut.video_inference_superanimal(
+        video,
+        superanimal_name,
+        videotype=".avi",
+        scale_list=scale_list,
     )
-]
 
-
-print ('testing superanimal_topviewmouse')
-superanimal_name = 'superanimal_topviewmouse'
-scale_list = [200, 300, 400]
-deeplabcut.video_inference_superanimal(
-    video,
-    superanimal_name,
-    videotype = '.avi',
-    scale_list = scale_list,
-)
-
-print ('testing superanimal_topviewmouse')
-superanimal_name = 'superanimal_quadruped'
-deeplabcut.video_inference_superanimal(
-    video,
-    superanimal_name,
-    videotype = '.avi',
-    scale_list = scale_list,
-)
+    print("testing superanimal_topviewmouse")
+    superanimal_name = "superanimal_quadruped"
+    deeplabcut.video_inference_superanimal(
+        video,
+        superanimal_name,
+        videotype=".avi",
+        scale_list=scale_list,
+    )
