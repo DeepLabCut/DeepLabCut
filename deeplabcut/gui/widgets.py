c257fb52564c5a7c03ed71a2c6c18903fadbeaf2
n-poulsen <45132115+n-poulsen@users.noreply.github.com>
2023-05-05 19:13:51 +0200

    Fix rectangle selector (#2227)
    * Remove RectangleSelector drawtype for matplotlib >3.7 compatability
    * removed rectprops to use default value

--- deeplabcut/gui/widgets.py
+++ deeplabcut/gui/widgets.py
@@ -514,12 +514,10 @@ class FrameCropper(QtWidgets.QDialog):
         self.rs = RectangleSelector(
             self.ax,
             self.line_select_callback,
-            drawtype="box",
             minspanx=5,
             minspany=5,
             interactive=True,
             spancoords="pixels",
-            rectprops=dict(facecolor="red", edgecolor="black", alpha=0.3, fill=True),
         )
         self.show()
         self.fig.canvas.start_event_loop(timeout=-1)
