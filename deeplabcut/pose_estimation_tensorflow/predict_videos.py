b4048040ce2b730ef20bfcb0e2e3cd3ead30e814
Konrad <54865575+KonradDanielewski@users.noreply.github.com>
2023-07-19 15:01:46 +0200

    spelling and formatted string

--- deeplabcut/pose_estimation_tensorflow/predict_videos.py
+++ deeplabcut/pose_estimation_tensorflow/predict_videos.py
@@ -1821,7 +1821,8 @@ def convert_detections2tracklets(
                     ass.assemble()
                     ass.to_pickle(dataname.split(".h5")[0] + "_assemblies.pickle")
                 else:
-                    print(f"Loading assemblies from {dataname.split(".h5")[0] + "_assemblies.pickle"}")
+                    ass_filename = dataname.split(".h5")[0] + "_assemblies.pickle"
+                    print(f"Loading assemblies from {ass_filename}")
                     ass = pd.read_pickle(dataname.split(".h5")[0] + "_assemblies.pickle")
                 try:
                     data.close()
