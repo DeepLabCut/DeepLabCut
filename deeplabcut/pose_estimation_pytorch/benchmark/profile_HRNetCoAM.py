35a93a1e866486292c208b3aac0510ceda30f591
AlexEMG <alexander@deeplabcut.org>
2023-08-02 17:51:02 +0200

    adding benchmark

---
+++ deeplabcut/pose_estimation_pytorch/benchmark/profile_HRNetCoAM.py
@@ -0,0 +1,9 @@
+# Script for reproducing results in Zhou* & Stoffl* et al. for BUCTD with CoAM
+
+#path=datapath
+#results=resultspath or put numbers
+
+#train model
+
+# evaluate and
+# check if predicted is close to result
