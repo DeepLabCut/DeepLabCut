f8552c6a75dc57aaf899f65a7c76c94d615b16af
shaokaiyeah <shaokaiyeah@gmail.com>
2023-08-28 09:09:16 +0000

    Updated bash script

--- deeplabcut/superanimal_pytorch/image_inference.sh
+++ deeplabcut/superanimal_pytorch/image_inference.sh
@@ -3,5 +3,7 @@ pose_model_name='superquadruped_hrnetw32'
 det_checkpoint='/mnt/md0/shaokai/mmdetection/work_dirs/faster_rcnn_r50_fpn_1x_quadruped/epoch_80.pth'
 pose_checkpoint='/mnt/md0/shaokai/integration/DeepLabCut/deeplabcut/superanimal_pytorch/third_party/work_dirs/hrnet_w32_quadruped_256x256_splitD/latest.pth'
 image_path='/mnt/md0/shaokai/many_dogs.jpeg'
+device='cuda'
+out_path='res.json'
 
-python topdown_superanimal_img_inference.py $det_model_name $pose_model_name $det_checkpoint $pose_checkpoint $image_path
+python topdown_superanimal_img_inference.py $det_model_name $pose_model_name $det_checkpoint $pose_checkpoint $image_path --out $out_path --device $device
