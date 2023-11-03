e6823365db56d7bde421c2213461e78f55abea20
AlexEMG <alexander@deeplabcut.org>
2023-11-03 13:08:06 +0100

    release updates

--- examples/test.sh
+++ examples/test.sh
@@ -6,7 +6,7 @@ rm -r OUT
 cd ..
 pip uninstall deeplabcut
 python3 setup.py sdist bdist_wheel
-pip install dist/deeplabcut-2.3.7-py3-none-any.whl
+pip install dist/deeplabcut-2.3.8-py3-none-any.whl
 
 cd examples
 
