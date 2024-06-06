3bf862a2530219a6bd31fcb5667730b43b8c45bc
shaokai <shaokaiyeah@gmail.com>
2024-06-06 14:07:05 +0200

    Generalized data converter (#195)

---
+++ benchmark_superanimal/keypoint_space_conversion.py
@@ -0,0 +1,26 @@
+"""Script to convert a dataset for its keypoint space to match the SuperAnimal space"""
+from pathlib import Path
+
+from deeplabcut.modelzoo.generalized_data_converter.datasets import COCOPoseDataset
+from deeplabcut.utils.auxiliaryfunctions import get_deeplabcut_path
+
+
+def main():
+    src_proj_root = Path("/media/data/trimouse_coco_original_shuffle0")
+    conversion_table_path = (
+        Path(get_deeplabcut_path())
+        / "modelzoo"
+        / "conversion_tables"
+        / "conversion_table_topview.csv"
+    )
+    dataset = COCOPoseDataset(str(src_proj_root), "trimouse")
+    dataset.project_with_conversion_table(conversion_table_path)
+    dataset.materialize(
+        src_proj_root.with_name("trimouse_coco_superanimal_shuffle0_shallow_copy"),
+        deepcopy=False,
+        framework="coco",
+    )
+
+
+if __name__ == "__main__":
+    main()
