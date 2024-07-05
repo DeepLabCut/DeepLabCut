99bfa0db770c355d141722f4156b622363327cd7
Niels Poulsen <niels.poulsen@epfl.ch>
2024-07-05 11:45:46 +0200

    bug fix: no shuffle_change slot in main window

--- deeplabcut/gui/components.py
+++ deeplabcut/gui/components.py
@@ -201,12 +201,6 @@ class ShuffleSpinBox(QtWidgets.QSpinBox):
         self.setMaximum(10_000)
         self.setValue(self.root.shuffle_value)
         self.valueChanged.connect(self.root.update_shuffle)
-        self.root.shuffle_change.connect(self.update_shuffle)
-
-    @Slot(int)
-    def update_shuffle(self, new_shuffle: int):
-        if new_shuffle != self.value():
-            self.setValue(new_shuffle)
 
 
 class DefaultTab(QtWidgets.QWidget):
