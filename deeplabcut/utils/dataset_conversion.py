f3ff04dcdfe963e8433b96bc8c809d7e68cb04cf
Niels Poulsen <niels.poulsen@epfl.ch>
2024-09-13 14:57:51 +0200

    added skeleton for project conversion

---
+++ deeplabcut/utils/dataset_conversion.py
@@ -0,0 +1,21 @@
+#
+# DeepLabCut Toolbox (deeplabcut.org)
+# © A. & M.W. Mathis Labs
+# https://github.com/DeepLabCut/DeepLabCut
+#
+# Please see AUTHORS for contributors.
+# https://github.com/DeepLabCut/DeepLabCut/blob/master/AUTHORS
+#
+# Licensed under GNU Lesser General Public License v3.0
+#
+from pathlib import Path
+
+
+def convert_sleap_project(sleap_file: Path, output_dlc_path: Path) -> None:
+    """Converts a SLEAP project to a DeepLabCut project
+
+    Args:
+        sleap_file: Path to the SLEAP project file (``.pkg.slp``).
+        output_dlc_path: Path to the output DLC project directory
+    """
+    pass
