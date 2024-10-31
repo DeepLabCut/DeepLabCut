DeepLabCut/DeepLabCut

commit 051f5cdb2143f805713981585e8a060c14c924cd

Train: read gpus from config if not passed by param (#2744) (#2764)

deeplabcut/pose_estimation_pytorch/apis/train.py
@@ -105,6 +105,9 @@ def train(
         run_config["device"] = device
         device = utils.resolve_device(run_config)
 
+    if gpus is None:
+        gpus = run_config["runner"]["gpus"]
+
     if device == "mps" and task == Task.DETECT:
         device = "cpu"  # FIXME: Cannot train detectors on MPS
 

commit 50f8fd17953c80aee4634f82f04a377a89bdf23c

Fix bug introduced in PR #2764

deeplabcut/pose_estimation_pytorch/apis/train.py
@@ -106,7 +106,7 @@ def train(
         device = utils.resolve_device(run_config)
 
     if gpus is None:
-        gpus = run_config["runner"]["gpus"]
+        gpus = run_config["runner"].get("gpus")
 
     if device == "mps" and task == Task.DETECT:
         device = "cpu"  # FIXME: Cannot train detectors on MPS