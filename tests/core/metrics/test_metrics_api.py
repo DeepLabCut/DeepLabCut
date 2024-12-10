dd0fd7563bcb40a74779bd1dc5bf97d378b4d139
n-poulsen <45132115+n-poulsen@users.noreply.github.com>
2024-12-10 15:52:09 +0100

    PyTorch model evaluation - `per_keypoint_evaluation` and `comparisonbodyparts` (#2810)

--- tests/core/metrics/test_metrics_api.py
+++ tests/core/metrics/test_metrics_api.py
@@ -16,6 +16,17 @@ from numpy.testing import assert_almost_equal
 import deeplabcut.core.metrics as metrics
 
 
+def _get_gt_and_pred_with_constant_err(
+    num_idv: int, num_bpt: int, error: float
+) -> tuple[np.ndarray, np.ndarray]:
+    gt = np.arange(num_idv * num_bpt * 3).astype(float).reshape((num_idv, num_bpt, 3))
+    gt[..., 2] = 2
+    predictions = gt.copy()
+    predictions[..., 2] = 0.9
+    predictions[..., :2] += error
+    return gt, predictions
+
+
 def test_computing_metrics_with_no_predictions():
     gt = np.arange(5 * 6 * 3).astype(float).reshape((5, 6, 3))
     gt[..., 2] = 2
@@ -30,11 +41,7 @@ def test_computing_metrics_with_no_predictions():
 @pytest.mark.parametrize("error", [0.5, 1, 2])
 def test_computing_metrics_with_constant_error(error):
     # only works for small errors: otherwise another matching can be found
-    gt = np.arange(5 * 6 * 3).astype(float).reshape((5, 6, 3))
-    gt[..., 2] = 2
-    predictions = gt.copy()
-    predictions[..., 2] = 0.9
-    predictions[..., :2] += error
+    gt, predictions = _get_gt_and_pred_with_constant_err(5, 6, error)
     results = metrics.compute_metrics(
         ground_truth={"image": gt},
         predictions={"image": predictions},
@@ -43,6 +50,46 @@ def test_computing_metrics_with_constant_error(error):
     )
     assert_almost_equal(results["rmse"], np.sqrt(2) * error)
     assert_almost_equal(results["rmse_pcutoff"], np.sqrt(2) * error)
+
+
+@pytest.mark.parametrize("error", [0.5, 1, 2])
+def test_metrics_with_unique_with_constant_error(error):
+    # only works for small errors: otherwise another matching can be found
+    gt, predictions = _get_gt_and_pred_with_constant_err(5, 6, error)
+    gt_unique, pred_unique = _get_gt_and_pred_with_constant_err(1, 8, error)
+    results = metrics.compute_metrics(
+        ground_truth={"image": gt},
+        predictions={"image": predictions},
+        unique_bodypart_gt={"image": gt_unique},
+        unique_bodypart_poses={"image": pred_unique},
+    )
+    assert_almost_equal(results["rmse"], np.sqrt(2) * error)
+    assert_almost_equal(results["rmse_pcutoff"], np.sqrt(2) * error)
+
+
+@pytest.mark.parametrize("error", [0.5, 1, 2])
+def test_metrics_per_bpt_with_unique_with_constant_error(error):
+    # only works for small errors: otherwise another matching can be found
+    gt, predictions = _get_gt_and_pred_with_constant_err(5, 6, error)
+    gt_unique, pred_unique = _get_gt_and_pred_with_constant_err(1, 8, error)
+    results = metrics.compute_metrics(
+        ground_truth={"image": gt},
+        predictions={"image": predictions},
+        unique_bodypart_gt={"image": gt_unique},
+        unique_bodypart_poses={"image": pred_unique},
+        per_keypoint_rmse=True,
+    )
+    assert_almost_equal(results["rmse"], np.sqrt(2) * error)
+    assert_almost_equal(results["rmse_pcutoff"], np.sqrt(2) * error)
+
+    for bpt_idx in range(gt.shape[1]):
+        key = f"rmse_keypoint_{bpt_idx}"
+        assert key in results
+        assert_almost_equal(results[key], np.sqrt(2) * error)
+    for bpt_idx in range(gt_unique.shape[1]):
+        key = f"rmse_unique_keypoint_{bpt_idx}"
+        assert key in results
+        assert_almost_equal(results[key], np.sqrt(2) * error)
 
 
 @pytest.mark.parametrize("error", [0.5, 1, 2])
