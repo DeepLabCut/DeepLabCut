7ab54215b25c99779ad7027a22b7eb821ca30f2b
Niels Poulsen <niels.poulsen@epfl.ch>
2024-12-19 13:33:06 +0100

    improve api __init__ imports

--- deeplabcut/pose_estimation_pytorch/apis/__init__.py
+++ deeplabcut/pose_estimation_pytorch/apis/__init__.py
@@ -11,16 +11,30 @@
 
 from deeplabcut.pose_estimation_pytorch.apis.analyze_images import (
     analyze_images,
+    analyze_image_folder,
     superanimal_analyze_images,
 )
-from deeplabcut.pose_estimation_pytorch.apis.analyze_videos import analyze_videos
+from deeplabcut.pose_estimation_pytorch.apis.analyze_videos import (
+    analyze_videos,
+    video_inference,
+    VideoIterator,
+)
 from deeplabcut.pose_estimation_pytorch.apis.convert_detections_to_tracklets import (
     convert_detections2tracklets,
 )
-from deeplabcut.pose_estimation_pytorch.apis.evaluate import evaluate_network
+from deeplabcut.pose_estimation_pytorch.apis.evaluate import (
+    predict,
+    evaluate,
+    evaluate_network,
+)
 from deeplabcut.pose_estimation_pytorch.apis.export import export_model
 from deeplabcut.pose_estimation_pytorch.apis.train import train_network
 from deeplabcut.pose_estimation_pytorch.apis.visualization import (
     extract_maps,
     extract_save_all_maps,
+)
+from deeplabcut.pose_estimation_pytorch.apis.utils import (
+    build_predictions_dataframe,
+    get_detector_inference_runner,
+    get_pose_inference_runner,
 )
