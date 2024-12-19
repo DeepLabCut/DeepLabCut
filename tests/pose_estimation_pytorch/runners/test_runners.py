883a0165764464ac3715e9afd1453b3c37adc7ab
Niels Poulsen <niels.poulsen@epfl.ch>
2024-12-19 14:47:01 +0100

    fix failing CI test

--- tests/pose_estimation_pytorch/runners/test_runners.py
+++ tests/pose_estimation_pytorch/runners/test_runners.py
@@ -9,6 +9,7 @@
 # Licensed under GNU Lesser General Public License v3.0
 #
 import pickle
+from pathlib import Path
 from unittest.mock import Mock
 
 import numpy as np
@@ -19,7 +20,7 @@ import deeplabcut.pose_estimation_pytorch.runners as runners
 
 
 def test_load_snapshot_weights_only_error(tmpdir_factory):
-    snapshot_dir = tmpdir_factory.mktemp("snapshot-dir")
+    snapshot_dir = Path(tmpdir_factory.mktemp("snapshot-dir"))
     snapshot_path = snapshot_dir / "snapshot.pt"
     torch.save(dict(content=np.zeros(10)), str(snapshot_path))
 
