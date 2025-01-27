0b3206508cc551a2082c632e8b8264c3b99cac83
maximpavliv <maxim.pavliv@gmail.com>
2025-01-27 17:37:01 +0100

    Add axis to confidence average computation
    This prevents having warnings when computing bboxes from keypoints in case provided data contains 0 animals

--- deeplabcut/core/trackingutils.py
+++ deeplabcut/core/trackingutils.py
@@ -770,7 +770,7 @@ def calc_bboxes_from_keypoints(data, slack=0, offset=0):
     bboxes = np.full((data.shape[0], 5), np.nan)
     bboxes[:, :2] = np.nanmin(data[..., :2], axis=1) - slack  # X1, Y1
     bboxes[:, 2:4] = np.nanmax(data[..., :2], axis=1) + slack  # X2, Y2
-    bboxes[:, -1] = np.nanmean(data[..., 2])  # Average confidence
+    bboxes[:, -1] = np.nanmean(data[..., 2], axis=1)  # Average confidence
     bboxes[:, [0, 2]] += offset
     return bboxes
 
