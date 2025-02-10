d2e8cbd844f2c4fdcbda7e627491e94b390f5892
Zelin2001 <lizl2023@ion.ac.cn>
2025-02-10 15:28:10 +0800

    fix - afterwards evaluation for ma models
    This is a supplemental fix for Commit 40465e1
    
    invert

--- deeplabcut/pose_estimation_pytorch/apis/evaluate.py
+++ deeplabcut/pose_estimation_pytorch/apis/evaluate.py
@@ -189,6 +189,7 @@ def evaluate(
         unique_bodypart_poses=unique_poses,
         unique_bodypart_gt=gt_unique_keypoints,
         per_keypoint_rmse=per_keypoint_evaluation,
+        compute_detection_rmse=False,
     )
 
     if loader.model_cfg["metadata"]["with_identity"]:
