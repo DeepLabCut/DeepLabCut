7b532d7b340e027d2e264eaf908eea7da0f6b62e
Niels Poulsen <niels.poulsen@epfl.ch>
2025-02-13 14:12:02 +0100

    first implementation of a dynamic cropper for top-down models

--- deeplabcut/pose_estimation_pytorch/runners/dynamic_cropping.py
+++ deeplabcut/pose_estimation_pytorch/runners/dynamic_cropping.py
@@ -14,6 +14,7 @@ from dataclasses import dataclass, field
 from typing import Optional
 
 import torch
+import torchvision.transforms.functional as F
 
 
 @dataclass
@@ -95,7 +96,7 @@ class DynamicCropper:
         x0, y0, x1, y1 = self._crop
         return image[:, :, y0:y1, x0:x1]
 
-    def update(self, pose: torch.Tensor) -> None:
+    def update(self, pose: torch.Tensor) -> torch.Tensor:
         """Updates the dynamic crop according to the pose model output.
 
         Uses the pose predicted by the model to update the dynamic crop parameters for
@@ -108,6 +109,9 @@ class DynamicCropper:
         Args:
             pose: The pose that was predicted by the pose estimation model in the
                 cropped image coordinate space.
+
+        Returns:
+            The pose, with coordinates updated to the full image space.
         """
         if self._shape is None:
             raise RuntimeError(f"You must call `crop` before calling `update`.")
@@ -124,12 +128,12 @@ class DynamicCropper:
         keypoints = keypoints[~torch.any(torch.isnan(keypoints), dim=1)]
         if len(keypoints) == 0:
             self.reset()
-            return
+            return pose
 
         mask = keypoints[:, 2] >= self.threshold
         if torch.all(~mask):
             self.reset()
-            return
+            return pose
 
         # set the crop coordinates
         x0 = self._min_value(keypoints[:, 0], self._shape[0])
@@ -139,9 +143,10 @@ class DynamicCropper:
         crop_w, crop_h = x1 - x0, y1 - y0
         if crop_w == 0 or crop_h == 0:
             self.reset()
-            return
+        else:
+            self._crop = x0, y0, x1, y1
 
-        self._crop = x0, y0, x1, y1
+        return pose
 
     def reset(self) -> None:
         """Resets the DynamicCropper to not crop the next frame"""
@@ -186,3 +191,323 @@ class DynamicCropper:
     def _clip(self, value: int, maximum: int) -> int:
         """Returns: The value clipped to [0, maximum]"""
         return min(max(value, 0), maximum)
+
+
+class TopDownDynamicCropper(DynamicCropper):
+    """
+    Provides functionality for dynamic, top-down cropping of images to refine
+    input for further processing or pose estimation.
+
+    The `TopDownDynamicCropper` class extends the base functionality of the
+    `DynamicCropper` class, implementing a dynamic image cropping mechanism
+    that adapts to the target region based on the calculated bounding box
+    or predefined patch sets. This dynamic cropping is particularly useful
+    for tasks such as object detection or pose estimation, where focusing
+    on relevant regions significantly improves accuracy. The cropping size
+    is determined dynamically using various parameters including min bounding
+    box size, patch size, overlap, and thresholds for keypoints. It integrates
+    the functionality to handle both patch-based cropping and bounding box-based
+    cropping approaches. The class also supports updating the cropping
+    parameters dynamically based on the predicted pose model output.
+
+    Args:
+        top_down_crop_size:
+
+    Attributes:
+        min_bbox_size (tuple[int, int]): Minimum width and height for a bounding box.
+        min_hq_keypoints (int): Minimum number of high-quality keypoints required
+            to dynamically focus on a region.
+        bbox_from_hq (bool): Indicates whether the bounding box should be created
+            using only high-quality keypoints.
+        _patch_counts (tuple[int, int]): Number of patches to divide the image into
+            on each axis (row, column).
+        _patch_overlap (int): Number of overlapping pixels between adjacent patches.
+        _patches (list): List of precomputed patches for generating crops.
+        _patch_offsets (list): List of offsets corresponding to each patch crop.
+        _td_crop_size (tuple[int, int]): User-specified dimensions of the crop
+            (height, width).
+        _td_ratio (float): Aspect ratio of the target crop size (height/width).
+    """
+
+    def __init__(
+        self,
+        top_down_crop_size: tuple[int, int],
+        patch_counts: tuple[int, int],
+        patch_overlap: int,
+        min_bbox_size: tuple[int, int],
+        threshold: float,
+        margin: int,
+        min_hq_keypoints: int = 2,
+        bbox_from_hq: bool = False,
+        store_crops: bool = False,
+        **kwargs,
+    ) -> None:
+        super().__init__(threshold=threshold, margin=margin, **kwargs)
+        self.min_bbox_size = min_bbox_size
+        self.min_hq_keypoints = min_hq_keypoints
+        self.bbox_from_hq = bbox_from_hq
+
+        self._patch_counts = patch_counts
+        self._patch_overlap = patch_overlap
+        self._patches = []
+        self._patch_offsets = []
+        self._td_crop_size = top_down_crop_size
+        self._td_ratio = self._td_crop_size[0] / self._td_crop_size[1]
+
+        self.crop_history = []
+        self.store_crops = store_crops
+
+    def patch_counts(self) -> tuple[int, int]:
+        """Returns: the number of patches created for an image."""
+        return self._patch_counts
+
+    def num_patches(self) -> int:
+        """Returns: the total number of patches created for an image."""
+        return self._patch_counts[0] * self._patch_counts[1]
+
+    def crop(self, image: torch.Tensor) -> torch.Tensor:
+        """Crops an input image according to the dynamic cropping parameters.
+
+        Args:
+            image: The image to crop, of shape (1, C, H, W).
+
+        Returns:
+            The cropped image of shape (B, C, H', W'), where [H', W'] is the size of
+            the crop.
+
+        Raises:
+            RuntimeError: if there is not exactly one image in the batch to crop, or if
+                `crop` was previously called with an image of a different W or H.
+        """
+        if len(image) != 1:
+            raise RuntimeError(
+                "DynamicCropper can only be used with batch size 1 (found image "
+                f"shape: {image.shape})"
+            )
+
+        if self._shape is None:
+            self._shape = image.shape[3], image.shape[2]
+            self._patches = self.generate_patches()
+
+        if image.shape[3] != self._shape[0] or image.shape[2] != self._shape[1]:
+            raise RuntimeError(
+                "All frames must have the same shape; The first frame had (W, H) "
+                f"{self._shape} but the current frame has shape {image.shape}."
+            )
+
+        if self._crop is None:
+            if self.store_crops:
+                self.crop_history.append([])
+            return self._crop_patches(image)
+
+        self.crop_history.append([self._crop])
+        return self._crop_bounding_box(image, self._crop)
+
+    def update(self, pose: torch.Tensor) -> torch.Tensor:
+        """Updates the dynamic crop according to the pose model output.
+
+        Uses the pose predicted by the model to update the dynamic crop parameters for
+        the next frame. Scales the pose predicted in the cropped image back to the
+        original image space and returns it.
+
+        This method modifies the pose tensor in-place; so pass a copy of the tensor if
+        you need to keep the original values.
+
+        Args:
+            pose: The pose that was predicted by the pose estimation model in the
+                cropped image coordinate space.
+
+        Returns:
+            The pose, with coordinates updated to the full image space.
+        """
+        if self._shape is None:
+            raise RuntimeError(f"You must call `crop` before calling `update`.")
+
+        # check whether this was a patched crop
+        batch_size = pose.shape[0]
+        if batch_size > 1:
+            pose = self._extract_best_patch(pose)
+
+        if self._crop is None:
+            raise RuntimeError(
+                "The _crop should never be `None` when `update` is called. Ensure you "
+                "always alternate between `crop` and `update`."
+            )
+
+        # offset and rescale the pose to the original image space
+        out_w, out_h = self._td_crop_size
+        offset_x, offset_y, w, h = self._crop
+        scale_x, scale_y = w / out_w, h / out_h
+        pose[..., 0] = (pose[..., 0] * scale_x) + offset_x
+        pose[..., 1] = (pose[..., 1] * scale_y) + offset_y
+        pose[..., 0] = torch.clip(pose[..., 0], 0, self._shape[0])
+        pose[..., 1] = torch.clip(pose[..., 1], 0, self._shape[1])
+
+        # check whether keypoints can be used for dynamic cropping
+        keypoints = pose[..., :3].reshape(-1, 3)
+        keypoints = keypoints[~torch.any(torch.isnan(keypoints), dim=1)]
+        if len(keypoints) == 0:
+            self.reset()
+            return pose
+
+        mask = keypoints[:, 2] >= self.threshold
+        if torch.sum(mask) < self.min_hq_keypoints:
+            self.reset()
+            return pose
+
+        if self.bbox_from_hq:
+            keypoints = keypoints[mask]
+
+        # set the crop coordinates
+        x0 = self._min_value(keypoints[:, 0], self._shape[0])
+        x1 = self._max_value(keypoints[:, 0], self._shape[0])
+        y0 = self._min_value(keypoints[:, 1], self._shape[1])
+        y1 = self._max_value(keypoints[:, 1], self._shape[1])
+        crop_w, crop_h = x1 - x0, y1 - y0
+        if crop_w == 0 or crop_h == 0:
+            self.reset()
+        else:
+            self._crop = self._prepare_bounding_box(x0, y0, x1, y1)
+
+        return pose
+
+    def _prepare_bounding_box(
+        self, x1: int, y1: int, x2: int, y2: int
+    ) -> tuple[int, int, int, int]:
+        """
+
+        Args:
+            x1:
+            y1:
+            x2:
+            y2:
+
+        Returns:
+
+        """
+        x1 -= self.margin
+        x2 += self.margin
+        y1 -= self.margin
+        y2 += self.margin
+        w, h = x2 - x1, y2 - y1
+        cx, cy = x1 + w / 2, y1 + h / 2
+
+        input_ratio = w / h
+        if input_ratio > self._td_ratio:  # h/w < h0/w0 => h' = w * h0/w0
+            h = w /  self._td_ratio
+        elif input_ratio < self._td_ratio:  # w/h < w0/h0 => w' = h * w0/h0
+            w = h *  self._td_ratio
+
+        x1, y1 = int(round(cx - (w / 2))), int(round(cy - (h / 2)))
+        w, h = max(int(w), self.min_bbox_size[0]), max(int(h), self.min_bbox_size[1])
+        return x1, y1, w, h
+
+    def _crop_bounding_box(
+        self, image: torch.Tensor, bbox: tuple[int, int, int, int],
+    ) -> torch.Tensor:
+        """
+
+        Args:
+            image:
+            bbox:
+
+        Returns:
+
+        """
+        x1, y1, w, h = bbox
+        out_w, out_h = self._td_crop_size
+        return F.resized_crop(image, y1, x1, h, w, [out_h, out_w])
+
+    def _crop_patches(self, image: torch.Tensor) -> torch.Tensor:
+        """Crops patches from the image.
+
+        Args:
+            image: The image to crop patches from, of shape (1, C, H, W).
+
+        Returns:
+            The patches, of shape (B, C, H', W'), where [H', W'] is the crop size.
+        """
+        patches = [self._crop_bounding_box(image, patch) for patch in self._patches]
+        return torch.cat(patches, dim=0)
+
+    def _extract_best_patch(self, pose: torch.Tensor) -> torch.Tensor:
+        """Extracts the best pose prediction
+
+        Args:
+            pose: The predicted pose, of shape (b, num_idv, num_kpt, 3). The number of
+                individuals must be 1.
+
+        Returns:
+
+        """
+        # check that only 1 prediction was made in each image
+        if pose.shape[1] != 1:
+            raise ValueError(
+                "The TopDownDynamicCropper can only be used with models predicting "
+                f"a single individual per image. Found {pose.shape[0]} "
+                f"predictions."
+            )
+
+        # compute the score for each individual
+        idv_scores = torch.mean(pose[:, 0, :, 2], dim=1)
+
+        # get the index of the best patch
+        best_patch = torch.argmax(idv_scores)
+
+        # set the crop to the one used for the best patch
+        self._crop = self._patches[best_patch]
+
+        return pose[best_patch:best_patch + 1]
+
+    def generate_patches(self) -> list[tuple[int, int, int, int]]:
+        """Generates patch coordinates for splitting an image.
+
+        Returns:
+            A list of patch coordinates as tuples (x0, y0, x1, y1).
+        """
+        patch_xs = self.split_array(
+            self._shape[0], self._patch_counts[0], self._patch_overlap
+        )
+        patch_ys = self.split_array(
+            self._shape[1], self._patch_counts[1], self._patch_overlap
+        )
+
+        patches = []
+        for y0, y1 in patch_ys:
+            for x0, x1 in patch_xs:
+                patches.append(self._prepare_bounding_box(x0, y0, x1, y1))
+
+        return patches
+
+    @staticmethod
+    def split_array(size: int, n: int, overlap: int) -> list[tuple[int, int]]:
+        """
+        Splits an array into n segments of equal size, where the overlap between each
+        segment is at least a given value.
+
+        Args:
+            size: The size of the array.
+            n: The number of segments to split the array into.
+            overlap: The minimum overlap between each segment.
+
+        Returns:
+            (start_index, end_index) pairs for each segment. The end index is exclusive.
+        """
+        if n < 1:
+            raise ValueError(f"Array must be split into at least 1 segment. Found {n}.")
+
+        # FIXME - auto-correct the overlap to spread it out more evenly
+        padded_size = size + (n - 1) * overlap
+        segment_size = (padded_size // n) + (padded_size % n > 0)
+        segments = []
+        end = overlap
+        for i in range(n):
+            start = end - overlap
+            end = start + segment_size
+            if end > size:
+                end = size
+                start = end - segment_size
+
+            segments.append((start, end))
+
+        return segments
