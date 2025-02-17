b3cfc763dc58733f9c0bab3fe037b0a813490af5
maximpavliv <maxim.pavliv@gmail.com>
2025-02-17 08:00:43 +0100

    fix entrypoint

--- deeplabcut/__main__.py
+++ deeplabcut/__main__.py
@@ -9,20 +9,25 @@
 # Licensed under GNU Lesser General Public License v3.0
 #
 
-try:
-    import PySide6
+def main():
+    try:
+        import PySide6
 
-    lite = False
-except ModuleNotFoundError:
-    lite = True
+        lite = False
+    except ModuleNotFoundError:
+        lite = True
 
-# if module is executed directly (i.e. `python -m deeplabcut.__init__`) launch straight into the GUI
-if not lite:
-    print("Starting GUI...")
-    from deeplabcut.gui.launch_script import launch_dlc
+    # if module is executed directly (i.e. `python -m deeplabcut.__init__`) launch straight into the GUI
+    if not lite:
+        print("Starting GUI...")
+        from deeplabcut.gui.launch_script import launch_dlc
 
-    launch_dlc()
-else:
-    print(
-        "You installed DLC lite, thus GUI's cannot be used. If you need GUI support please: pip install 'deeplabcut[gui]''"
-    )
+        launch_dlc()
+    else:
+        print(
+            "You installed DLC lite, thus GUI's cannot be used. If you need GUI support please: pip install 'deeplabcut[gui]''"
+        )
+
+
+if __name__ == "__main__":
+    main()
