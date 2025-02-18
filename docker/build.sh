29a02eb2574c251ac35dc48886b181b14835075b
Niels Poulsen <niels.poulsen@epfl.ch>
2025-02-18 16:33:10 +0100

    update test dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -79,7 +79,7 @@ iterate_build_matrix() {
                 ${pytorch_versions[@]}; do
                 for stage in \
                     ${docker_types[@]}; do
-                    tag=${deeplabcut_version}-${stage}-cuda${cuda_version}-cudnn9
+                    tag=${deeplabcut_version}-${stage}-${cuda_version}
                     case "$mode" in
                     build)
                         echo \
