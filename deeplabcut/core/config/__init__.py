b17d38595e4f61b7db9b1cd88507d556acd4ef62
Jaap de Ruyter <jaap.deruyter@epfl.ch>
2026-01-26 17:17:06 +0100

    add pytorch base config mixin
    This mixin provides methods for:
        - Loading configurations from dictionaries or YAML files
        - Validating configuration data against pydantic models
        - Converting configurations to dictionaries
        - Pretty printing configuration data
    
    add imports from utils

---
+++ deeplabcut/core/config/__init__.py
@@ -0,0 +1,13 @@
+from deeplabcut.core.config.utils import (
+    read_config,
+    read_config_as_dict,
+    write_config,
+    pretty_print,
+    create_config_template,
+    create_config_template_3d,
+    edit_config,
+    write_config_3d,
+    write_config_3d_template,
+    write_project_config,
+)
+from deeplabcut.core.config.config_mixin import ConfigMixin
