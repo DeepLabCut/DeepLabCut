2fbbce0d14b3b8eaf0da5b11c08da91ab572c231
Jaap de Ruyter <deruyter92@gmail.com>
2026-02-05 16:01:57 +0100

    fix intermediate head_cfg container in build PoseModel
    The head_cfg (a part of PoseConfig) was used for dumping modules. This is not supported for typed configs.
    
    The container is now replaced with a plain dict type instead of config.

--- deeplabcut/pose_estimation_pytorch/models/model.py
+++ deeplabcut/pose_estimation_pytorch/models/model.py
@@ -184,7 +184,11 @@ class PoseModel(nn.Module):
 
         heads = {}
         for name, head_cfg in cfg["heads"].items():
-            head_cfg = copy.deepcopy(head_cfg)
+            # NOTE @deruyter92 2026-02-05: currently modules are added inside the config
+            # dictionary. This seems like a bad practice and is unsupported for typed configs or
+            # OmegaConf DictConfig. The intermediate head_cfg container is now converted to a dictionary
+            # to avoid this issue.
+            head_cfg = dict(copy.deepcopy(head_cfg))
             if "type" in head_cfg["criterion"]:
                 head_cfg["criterion"] = CRITERIONS.build(head_cfg["criterion"])
             else:
