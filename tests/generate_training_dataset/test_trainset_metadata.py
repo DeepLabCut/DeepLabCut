9f19f20183920555a88e238ebd720365beb8ac00
Jaap de Ruyter <deruyter92@gmail.com>
2026-02-05 18:45:17 +0100

    YAML I/O - add centralized, safe ruamel YAML loader and dumper with Path and Enum serialization

--- tests/generate_training_dataset/test_trainset_metadata.py
+++ tests/generate_training_dataset/test_trainset_metadata.py
@@ -17,8 +17,8 @@ import pickle
 from unittest.mock import MagicMock, patch
 
 import pytest
-from ruamel.yaml import YAML
 
+from deeplabcut.core.config.utils import get_yaml_loader, get_yaml_dumper
 import deeplabcut.generate_training_dataset.metadata as metadata
 from deeplabcut.core.engine import Engine
 from deeplabcut.utils import auxiliaryfunctions
@@ -72,7 +72,7 @@ def test_load_metadata(tmpdir, data: dict, load_splits: bool):
     # write data to tmp file
     cfg, cfg_path, trainset_dir, meta_path = _create_project_with_config(tmpdir)
     with open(meta_path, "w") as f:
-        YAML().dump(data, f)
+        get_yaml_dumper().dump(data, f)
 
     print(cfg_path)
     print(meta_path)
@@ -169,8 +169,8 @@ def test_save_metadata_simple(tmpdir, data):
     print(trainset_meta)
 
     trainset_meta.save()
-    with open(meta_path) as f:
-        meta = YAML().load(f)
+    with open(meta_path, "r") as f:
+        meta = get_yaml_loader().load(f)
     print(data)
     print(meta)
     assert data["expected"] == meta
@@ -558,7 +558,7 @@ def _create_project_with_config(
 
     cfg_path = project_dir.join("config.yaml")
     with open(cfg_path, "w") as file:
-        YAML().dump(cfg, file)
+        get_yaml_dumper().dump(cfg, file)
 
     it = f"iteration-{iteration}"
     dir_name = "UnaugmentedDataSet_" + task + date
