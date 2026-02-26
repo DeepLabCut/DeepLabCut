45f91b2bb31c5a3c0b8ce5183644da8f80763543
Jaap de Ruyter <deruyter92@gmail.com>
2026-02-26 13:40:26 +0100

    add commment legenda to __init__ for keypoint refactoring

--- deeplabcut/pose_estimation_pytorch/modelzoo/fmpose_3d/__init__.py
+++ deeplabcut/pose_estimation_pytorch/modelzoo/fmpose_3d/__init__.py
@@ -6,3 +6,10 @@ Please see AUTHORS for contributors.
 https://github.com/DeepLabCut/DeepLabCut/blob/main/AUTHORS
 Licensed under GNU Lesser General Public License v3.0
 """
+
+# NOTE: this module may contain items that need refactoring during 
+# the keypoint migration.
+
+# kpt_refactor - Needs attention when refactoring keypoints
+# i_o - This module writes keypoints to disk
+# pandas - This module relies on pandas (might be moved to polars)
