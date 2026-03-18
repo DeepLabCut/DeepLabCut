70ff090536bb5c4404afd2c1e1713adf4dbd681a
Cyril Achard <cyril.achard@epfl.ch>
2026-03-18 14:28:27 +0100

    Remove outdated test code
    build_training_runner already does what was being tested manually

--- tests/pose_estimation_pytorch/runners/test_bottom_up.py
+++ tests/pose_estimation_pytorch/runners/test_bottom_up.py
@@ -14,12 +14,9 @@ from pathlib import Path
 from typing import Any
 
 import pytest
-import torch
 
 from deeplabcut.pose_estimation_pytorch.config import make_pytorch_pose_config
-from deeplabcut.pose_estimation_pytorch.models import LOSS_AGGREGATORS, PREDICTORS, PoseModel
-from deeplabcut.pose_estimation_pytorch.models.criterions.aggregators import WeightedLossAggregator
-from deeplabcut.pose_estimation_pytorch.runners.schedulers import LRListScheduler
+from deeplabcut.pose_estimation_pytorch.models import PoseModel
 from deeplabcut.pose_estimation_pytorch.runners.train import build_training_runner
 from deeplabcut.utils import auxiliaryfunctions
 
@@ -40,8 +37,9 @@ def print_dict(data: dict, indent: int = 0):
 def test_build_bottom_up_runner(
     net_type: str,
     multianimal: bool,
+    tmp_path: Path,
 ) -> None:
-    project_cfg: dict[str, Any] = {"multianimalproject": multianimal}
+    project_cfg: dict[str, Any] = {"multianimalproject": multianimal, "project_path": str(tmp_path)}
     if multianimal:
         project_cfg["bodyparts"] = "MULTI!"
         project_cfg["multianimalbodyparts"] = ["head", "shoulder", "knee", "toe"]
@@ -54,35 +52,13 @@ def test_build_bottom_up_runner(
 
     root_path = Path(__file__).parent.parent
     template_path = root_path / "other/test_configs/pytorch_config.yaml"
-    template_path.resolve()
+    template_path = template_path.resolve()
     assert template_path.is_file(), f"Template config not found at {template_path}"
     auxiliaryfunctions.read_plainconfig(str(template_path))
     pytorch_cfg = make_pytorch_pose_config(project_cfg, str(template_path), net_type)
     print_dict(pytorch_cfg)
 
     pose_model = PoseModel.build(pytorch_cfg["model"])
-
-    head_criterions = []
-    for head_cfg in pytorch_cfg["model"]["heads"]:
-        crit_cfg = head_cfg["criterion"]
-        criterion_weight = crit_cfg.get("weight", 1)
-        criterion = LOSS_AGGREGATORS.build({k: v for k, v in crit_cfg.items() if k != "weight"})
-        head_criterions.append((criterion_weight, criterion))
-    criterion = WeightedLossAggregator(head_criterions)
-
-    get_optimizer = getattr(torch.optim, pytorch_cfg["optimizer"]["type"])
-    optimizer = get_optimizer(params=pose_model.parameters(), **pytorch_cfg["optimizer"]["params"])
-
-    PREDICTORS.build(dict(pytorch_cfg["model"]["predictor"]))
-
-    if pytorch_cfg.get("scheduler"):
-        if pytorch_cfg["scheduler"]["type"] == "LRListScheduler":
-            _scheduler = LRListScheduler
-        else:
-            _scheduler = getattr(torch.optim.lr_scheduler, pytorch_cfg["scheduler"]["type"])
-        _scheduler(optimizer=optimizer, **pytorch_cfg["scheduler"]["params"])
-    else:
-        pass
 
     # NOTE: @C-Achard 2026-03-18 This file was not named with test_* as a prefix,
     #  so it never ran in CI. A lot of imports are outdated and non-existent
