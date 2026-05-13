1efb65af856b3961808001455716b93c882b7173
C-Achard <cyril.achard@epfl.ch>
2026-05-13 10:37:16 +0200

    Add license headers and tidy debug exports
    Add project/license header comments to deeplabcut/core/debug/__init__.py and deeplabcut/gui/dialogs/__init__.py. In core/debug/__init__.py, clean up the public API exports: remove internal symbols DLC_LOG_TIMING and LOG_QUEUE_MAXLEN from __all__, reorder entries, and ensure collect_executable_summary is exported. These changes clarify public exports and add attribution/licensing headers.

--- deeplabcut/core/debug/__init__.py
+++ deeplabcut/core/debug/__init__.py
@@ -1,9 +1,18 @@
+#
+# DeepLabCut Toolbox (deeplabcut.org)
+# © A. & M.W. Mathis Labs
+# https://github.com/DeepLabCut/DeepLabCut
+#
+# Please see AUTHORS for contributors.
+# https://github.com/DeepLabCut/DeepLabCut/blob/main/AUTHORS
+#
+# Licensed under GNU Lesser General Public License v3.0
+#
+
 from collections.abc import Sequence
 
 from .debug_logger import (
     DLC_ALL_LIBS_SPECS,
-    DLC_LOG_TIMING,
-    LOG_QUEUE_MAXLEN,
     ExecutableSpec,
     InMemoryDebugRecorder,
     LibrarySpec,
@@ -19,19 +28,17 @@ from .debug_logger import (
 )
 
 __all__: Sequence[str] = (
-    "DLC_LOG_TIMING",
     "DLC_ALL_LIBS_SPECS",
+    "ExecutableSpec",
     "InMemoryDebugRecorder",
     "LibrarySpec",
-    "ExecutableSpec",
-    "LOG_QUEUE_MAXLEN",
     "RecordedLog",
     "build_debug_report",
     "collect_debug_sections",
+    "collect_executable_summary",
     "collect_version_summary",
     "format_debug_report",
     "get_debug_recorder",
     "install_debug_recorder",
     "log_timing",
-    "collect_executable_summary",
 )
