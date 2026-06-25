474f2df68facd5b127c9a547215ea32a5a17277e
Jaap de Ruyter van Steveninck <32810691+deruyter92@users.noreply.github.com>
2026-06-25 11:39:14 +0200

    add modules deeplabcut.api and deeplabcu.tensorflow_compat (init only)

---
+++ deeplabcut/api/__init__.py
@@ -0,0 +1,13 @@
+#
+# DeepLabCut Toolbox (deeplabcut.org)
+# © A. & M.W. Mathis Labs
+# https://github.com/DeepLabCut/DeepLabCut
+#
+# Please see AUTHORS for contributors.
+# https://github.com/DeepLabCut/DeepLabCut/blob/master/AUTHORS
+#
+# Licensed under GNU Lesser General Public License v3.0
+#
+"""
+Public API for DeepLabCut.
+"""
