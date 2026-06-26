b9afbe3186075fc2dd092c91b65dd1b7ded61b36
Jaap de Ruyter van Steveninck <32810691+deruyter92@users.noreply.github.com>
2026-06-26 13:00:46 +0200

    adjust tf_routing: import top-level tensorflow_compat

--- deeplabcut/tensorflow_compat/__init__.py
+++ deeplabcut/tensorflow_compat/__init__.py
@@ -9,5 +9,61 @@
 # Licensed under GNU Lesser General Public License v3.0
 #
 """
-Compatibility layer for the legacy TensorFlow API. Remove this module when TF support is dropped.
+Compatibility layer for the legacy TensorFlow API. Remove this package when TF support is dropped.
 """
+
+from deeplabcut.tensorflow_compat.dataset_management import (
+    MakeInference_yaml,
+    MakeTest_pose_yaml,
+    MakeTrain_pose_yaml,
+    ParseYaml,
+    create_multianimaltraining_dataset,
+    create_training_dataset,
+    create_training_dataset_from_existing_split,
+    create_training_model_comparison,
+)
+from deeplabcut.tensorflow_compat.tensorflow_api import (
+    analyze_images,
+    analyze_time_lapse_frames,
+    analyze_videos,
+    convert_detections2tracklets,
+    create_tracking_dataset,
+    evaluate_network,
+    export_model,
+    extract_maps,
+    extract_save_all_maps,
+    get_available_aug_methods,
+    return_evaluate_network_data,
+    return_train_network_path,
+    train_network,
+    visualize_locrefs,
+    visualize_paf,
+    visualize_scoremaps,
+)
+
+__all__ = [
+    "MakeInference_yaml",
+    "MakeTest_pose_yaml",
+    "MakeTrain_pose_yaml",
+    "ParseYaml",
+    "analyze_images",
+    "analyze_time_lapse_frames",
+    "analyze_videos",
+    "convert_detections2tracklets",
+    "create_multianimaltraining_dataset",
+    "create_tracking_dataset",
+    "create_training_dataset",
+    "create_training_dataset_from_existing_split",
+    "create_training_model_comparison",
+    "evaluate_network",
+    "export_model",
+    "extract_maps",
+    "extract_save_all_maps",
+    "get_available_aug_methods",
+    "return_evaluate_network_data",
+    "return_train_network_path",
+    "train_network",
+    "visualize_locrefs",
+    "visualize_paf",
+    "visualize_scoremaps",
+]
