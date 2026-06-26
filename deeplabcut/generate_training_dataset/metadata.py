58a1af79d41a910aee6072d7a7dbed49c1b73edd
Jaap de Ruyter van Steveninck <32810691+deruyter92@users.noreply.github.com>
2026-06-26 11:15:11 +0200

    fix Engine imports across repo (core, not compat)

--- deeplabcut/generate_training_dataset/metadata.py
+++ deeplabcut/generate_training_dataset/metadata.py
@@ -121,7 +121,7 @@ class TrainingDatasetMetadata:
             name="my-dlc-projectJun17-trainset60shuffle5",
             train_fraction=0.6,
             index=5,
-            engine=compat.Engine.PYTORCH,
+            engine=Engine.PYTORCH,
             split=DataSplit(train_indices=(1, 3, 4), test_indices=(0, 2)),
         )
         trainset_metadata = trainset_metadata.add(new_shuffle)
