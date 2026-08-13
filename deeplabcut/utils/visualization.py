fb4b16d25bb7a7fc57b242ec6557f0dc70f21185
Cyril Achard <cyril.achard@epfl.ch>
2026-08-13 10:59:35 +0200

    Normalize visualization file headers
    Removes the duplicated legacy license/docstring block from `deeplabcut/utils/visualization.py` and adds the standard DeepLabCut license header comments to `tests/utils/test_visualization.py` for consistent file metadata across the codebase.

--- deeplabcut/utils/visualization.py
+++ deeplabcut/utils/visualization.py
@@ -8,15 +8,6 @@
 #
 # Licensed under GNU Lesser General Public License v3.0
 #
-"""
-DeepLabCut2.0 Toolbox (deeplabcut.org)
-© A. & M. Mathis Labs
-https://github.com/DeepLabCut/DeepLabCut
-Please see AUTHORS for contributors.
-
-https://github.com/DeepLabCut/DeepLabCut/blob/master/AUTHORS
-Licensed under GNU Lesser General Public License v3.0
-"""
 
 from __future__ import annotations
 
